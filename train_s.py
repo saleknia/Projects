b1fa04c3006bbcae532aeda86e3d297eb0c88012
saleknia <salekniaamirhossein@gmail.com>
2022-05-31 13:49:04 +0000

    Commit Process

---
+++ train_s.py
@@ -0,0 +1,478 @@
+# Instaling Libraries
+import os
+import copy
+import torch
+import torchvision
+import torch.nn as nn
+from PIL import Image
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import numpy as np
+import random
+import pickle
+import argparse
+from torch.backends import cudnn
+# from albumentations.pytorch.transforms import ToTensorV2
+# import albumentations as A
+import matplotlib.pyplot as plt
+from torchvision import transforms
+import torchvision.transforms.functional as TF
+from torch.utils.data import random_split
+from tqdm.notebook import tqdm
+import torch.optim as optim
+from models.UNet import UNet
+from models.UNet_loss import UNet_loss
+from models.UNet_plus import NestedUNet
+from models.UNet_plus_loss import NestedUNet_loss
+from models.att_unet import AttentionUNet
+from models.att_unet_loss import AttentionUNet_loss
+from models.multi_res_unet import MultiResUnet
+from models.U import U
+from models.U_loss import U_loss
+from models.multi_res_unet_loss import MultiResUnet_loss
+from models.UCTransNet import UCTransNet
+from models.GT_UNet import GT_U_Net
+from models.ENet import ENet
+from models.DABNet import DABNet
+from models.DABNet_loss import DABNet_loss
+from models.ENet_loss import ENet_loss
+from models.UCTransNet_GT import UCTransNet_GT
+from models.GT_CTrans import GT_CTrans
+# from models.original_UNet import original_UNet
+import utils
+from utils import color
+from utils import Save_Checkpoint
+from trainer import trainer
+from tester import tester
+from dataset import COVID_19,Synapse_dataset,RandomGenerator,ValGenerator,ACDC,CT_1K
+from utils import DiceLoss,atten_loss,prototype_loss,prototype_loss_kd
+from config import *
+from tabulate import tabulate
+from tensorboardX import SummaryWriter
+# from testing import inference
+# from testingV2 import inferenceV2
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def main(args):
+
+    if tensorboard:
+        tensorboard_log = tensorboard_folder
+        logger.info(f'Tensorboard Directory: {tensorboard_log}')
+        if not os.path.isdir(tensorboard_log):
+            os.makedirs(tensorboard_log)
+        writer = SummaryWriter(tensorboard_log)
+    else:
+        writer = None
+
+    train_tf = transforms.Compose([RandomGenerator(output_size=[IMAGE_HEIGHT, IMAGE_WIDTH])])
+    # train_tf = ValGenerator(output_size=[IMAGE_HEIGHT, IMAGE_WIDTH])
+    val_tf = ValGenerator(output_size=[IMAGE_HEIGHT, IMAGE_WIDTH])
+
+    # transform = A.Compose(
+    #     [
+    #         A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+    #         ToTensorV2()
+    #     ]
+    # )
+
+# LOAD_MODEL
+
+    if MODEL_NAME=='UNet':
+        model = UNet(n_channels=1, n_classes=NUM_CLASS).to(DEVICE)
+        # model = original_UNet().to(DEVICE)
+
+    elif MODEL_NAME=='UNet_loss':
+        model = UNet_loss(n_channels=1, n_classes=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME=='U':
+        model = U(bilinear=False).to(DEVICE)
+
+    elif MODEL_NAME=='U_loss':
+        model = U_loss(bilinear=False).to(DEVICE)
+
+    elif MODEL_NAME=='UCTransNet':
+        config_vit = get_CTranS_config()
+        model = UCTransNet(config_vit,n_channels=n_channels,n_classes=n_labels,img_size=IMAGE_HEIGHT).to(DEVICE)
+
+    elif MODEL_NAME=='UCTransNet_GT':
+        config_vit = get_CTranS_config()
+        model = UCTransNet_GT(config_vit,n_channels=n_channels,n_classes=n_labels,img_size=IMAGE_HEIGHT).to(DEVICE)
+
+    elif MODEL_NAME=='GT_UNet':
+        model = GT_U_Net(img_ch=1,output_ch=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME=='GT_CTrans':
+        config_vit = get_CTranS_config()
+        model = GT_CTrans(config_vit,img_ch=1,output_ch=NUM_CLASS,img_size=256).to(DEVICE)
+
+    elif MODEL_NAME == 'AttUNet':
+        model = AttentionUNet(img_ch=1, output_ch=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME == 'AttUNet_loss':
+        model = AttentionUNet_loss(img_ch=1, output_ch=NUM_CLASS).to(DEVICE) 
+
+    elif MODEL_NAME == 'MultiResUnet':
+        model = MultiResUnet().to(DEVICE)
+
+    elif MODEL_NAME == 'MultiResUnet_loss':
+        model = MultiResUnet_loss().to(DEVICE)
+
+    elif MODEL_NAME == 'UNet++':
+        model = NestedUNet().to(DEVICE)
+
+    elif MODEL_NAME == 'UNet++_loss':
+        model = NestedUNet_loss().to(DEVICE)
+
+    elif MODEL_NAME == 'ENet':
+        model = ENet(nclass=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME == 'ENet_loss':
+        model = ENet_loss(nclass=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME == 'DABNet':
+        model = DABNet(classes=NUM_CLASS).to(DEVICE)
+
+    elif MODEL_NAME == 'DABNet_loss':
+        model = DABNet_loss(classes=NUM_CLASS).to(DEVICE)
+        
+    else: 
+        raise TypeError('Please enter a valid name for the model type')
+
+
+    num_parameters = utils.count_parameters(model)
+
+    model_table = tabulate(
+        tabular_data=[[MODEL_NAME, f'{num_parameters:.2f} M', DEVICE]],
+        headers=['Builded Model', '#Parameters', 'Device'],
+        tablefmt="fancy_grid"
+        )
+    logger.info(model_table)
+
+    # loss_fn = utils.DiceLoss(n_classes=NUM_CLASS)
+    # optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    # optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=LEARNING_RATE)
+
+    optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=LEARNING_RATE, momentum=0.9, weight_decay=0.0001)
+ 
+    if COSINE_LR is True:
+        lr_scheduler = utils.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=1e-4)
+    else:
+        lr_scheduler =  None     
+
+
+    initial_best_acc = 0
+    initial_best_epoch = 1
+    current_num_epoch = 0
+    last_num_epoch = 0
+    if LOAD_MODEL:
+        checkpoint_path = '/content/drive/MyDrive/checkpoint/'+CKPT_NAME+'_last.pth'
+        logger.info('Loading Checkpoint...')
+        if os.path.isfile(checkpoint_path):
+
+            pretrained_model_path = checkpoint_path
+            loaded_data = torch.load(pretrained_model_path, map_location='cuda')
+            pretrained = loaded_data['net']
+            model2_dict = model.state_dict()
+            state_dict = {k:v for k,v in pretrained.items() if ((k in model2_dict.keys()) and (v.shape==model2_dict[k].shape))}
+            # logger.info(state_dict.keys())
+            model2_dict.update(state_dict)
+            model.load_state_dict(model2_dict)
+
+            initial_best_acc=loaded_data['best_acc']
+            loaded_acc=loaded_data['acc']
+            initial_best_epoch=loaded_data['best_epoch']
+            last_num_epoch=loaded_data['num_epoch']
+            current_num_epoch=last_num_epoch+current_num_epoch
+
+            optimizer.load_state_dict(loaded_data['optimizer'])
+            # lr_scheduler.load_state_dict(loaded_data['lr_scheduler'])
+
+            # if last_num_epoch-initial_best_epoch < early_stopping:
+            #     count_es = last_num_epoch-initial_best_epoch
+            #     es = False
+            # elif early_stopping < last_num_epoch-initial_best_epoch:
+            #     count_es = early_stopping + 1
+            #     es = True
+            table = tabulate(
+                            tabular_data=[[loaded_acc, initial_best_acc, initial_best_epoch, last_num_epoch]],
+                            headers=['Loaded Model Acc', 'Initial Best Acc', 'Best Epoch Number', 'Num Epochs'],
+                            tablefmt="fancy_grid"
+                            )
+            logger.info(table)
+        else:
+            logger.info(f'No Such file : {checkpoint_path}')
+        logger.info('\n')
+
+    start_epoch = last_num_epoch + 1
+    end_epoch =  NUM_EPOCHS
+
+    if TASK_NAME=='COVID-19':
+
+        train_dataset=COVID_19(split='train', joint_transform=train_tf)
+        valid_dataset=COVID_19(split='test', joint_transform=val_tf)
+
+        train_loader = DataLoader(train_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+        valid_loader = DataLoader(valid_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+
+        data_loader={'train':train_loader,'valid':valid_loader}
+
+    elif TASK_NAME=='Synapse':
+        # index = np.load(file='index.npy')
+        # train_dataset=Synapse_dataset(split='train',index=index[0:2000],joint_transform=train_tf)
+        # valid_dataset=Synapse_dataset(split='val',index=index[2000:],joint_transform=val_tf)
+
+        train_dataset=Synapse_dataset(split='train', joint_transform=train_tf)
+        valid_dataset=Synapse_dataset(split='val', joint_transform=val_tf)
+
+        # g = torch.Generator()
+        # g.manual_seed(0)
+
+        train_loader = DataLoader(train_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                # generator=g
+                                )
+        valid_loader = DataLoader(valid_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                # generator=g
+                                )
+
+        data_loader={'train':train_loader,'valid':valid_loader}
+
+    elif TASK_NAME=='ACDC':
+        # index = np.load(file='index.npy')
+        # train_dataset=Synapse_dataset(split='train',index=index[0:2000],joint_transform=train_tf)
+        # valid_dataset=Synapse_dataset(split='val',index=index[2000:],joint_transform=val_tf)
+
+        train_dataset=ACDC(split='train', joint_transform=train_tf)
+        valid_dataset=ACDC(split='test', joint_transform=val_tf)
+
+        # g = torch.Generator()
+        # g.manual_seed(0)
+
+        train_loader = DataLoader(train_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+        valid_loader = DataLoader(valid_dataset,
+                                batch_size=1,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+
+        data_loader={'train':train_loader,'valid':valid_loader}
+
+    elif TASK_NAME=='CT-1K':
+
+        train_dataset=CT_1K(split='train', joint_transform=train_tf)
+        valid_dataset=CT_1K(split='test', joint_transform=val_tf)
+
+        # g = torch.Generator()
+        # g.manual_seed(0)
+
+        train_loader = DataLoader(train_dataset,
+                                batch_size=BATCH_SIZE,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+        valid_loader = DataLoader(valid_dataset,
+                                batch_size=1,
+                                shuffle=True,
+                                worker_init_fn=worker_init,
+                                num_workers=NUM_WORKERS,
+                                pin_memory=PIN_MEMORY,
+                                drop_last=True,
+                                )
+
+        data_loader={'train':train_loader,'valid':valid_loader}
+
+    if SAVE_MODEL:
+        checkpoint = Save_Checkpoint(CKPT_NAME,current_num_epoch,last_num_epoch,initial_best_acc,initial_best_epoch)
+    else:
+        checkpoint = None
+
+    if args.train=='True':
+        logger.info(50*'*')
+        logger.info('Training Phase')
+        logger.info(50*'*')
+        loss_function = prototype_loss()
+        # loss_function = prototype_loss_kd()
+        for epoch in range(start_epoch,end_epoch+1):
+            # set_epoch(epoch,g)
+            trainer(
+                end_epoch=end_epoch,
+                epoch_num=epoch,
+                model=model,
+                dataloader=data_loader['train'],
+                optimizer=optimizer,
+                device=DEVICE,
+                ckpt=checkpoint,                
+                num_class=NUM_CLASS,
+                lr_scheduler=lr_scheduler,
+                writer=writer,
+                logger=logger,
+                loss_function=loss_function)
+            # tester(
+            #     end_epoch=end_epoch,
+            #     epoch_num=epoch,
+            #     model=model,
+            #     dataloader=data_loader['valid'],
+            #     device=DEVICE,
+            #     ckpt=checkpoint,
+            #     num_class=NUM_CLASS,
+            #     writer=writer,
+            #     logger=logger,
+            #     optimizer=optimizer,
+            #     lr_scheduler=lr_scheduler,
+            #     early_stopping=early_stopping)
+
+            if True:#epoch==end_epoch:
+                if SAVE_MODEL and 0 < checkpoint.best_accuracy():
+                    # pretrained_model_path = os.path.join(os.path.abspath('checkpoint'), CKPT_NAME + '_best.pth')
+                    # pretrained_model_path = '/content/drive/MyDrive/checkpoint/' + CKPT_NAME + '_best.pth'
+                    pretrained_model_path = '/content/drive/MyDrive/checkpoint/' + CKPT_NAME + '_best.pth'
+                    loaded_data = torch.load(pretrained_model_path, map_location='cuda')
+                    pretrained = loaded_data['net']
+                    model2_dict = model.state_dict()
+                    state_dict = {k:v for k,v in pretrained.items() if ((k in model2_dict.keys()) and (v.shape==model2_dict[k].shape))}
+                    model2_dict.update(state_dict)
+                    model.load_state_dict(model2_dict)
+
+                    acc=loaded_data['acc']
+                    acc_per_class=loaded_data['acc_per_class'].tolist()
+                    acc_per_class=[round(x,2) for x in acc_per_class]
+                    best_epoch=loaded_data['best_epoch']
+
+                    logger.info(50*'*')
+                    logger.info(f'Best Accuracy over training: {acc:.2f}')
+                    logger.info(f'Best Accuracy Per Class over training: {acc_per_class}')
+                    logger.info(f'Epoch Number: {best_epoch}')
+
+                    if args.inference=='True':
+                        logger.info(50*'*')
+                        logger.info('Inference Phase')
+                        logger.info(50*'*')
+                        inference(model=model,logger=logger)
+                        tester(
+                            end_epoch=1,
+                            epoch_num=1,
+                            model=copy.deepcopy(model),
+                            dataloader=data_loader['valid'],
+                            device=DEVICE,
+                            ckpt=None,
+                            num_class=NUM_CLASS,
+                            writer=writer,
+                            logger=logger,
+                            optimizer=None,
+                            lr_scheduler=None,
+                            early_stopping=None)
+                
+                    logger.info(50*'*')
+                    logger.info(50*'*')
+                    logger.info('\n')
+    if tensorboard:
+        writer.close()
+
+    # numpy_state = np.random.get_state()
+    # with open('./checkpoint/numpy_state.pickle', 'wb') as f:
+    #     pickle.dump(numpy_state, f)
+
+    # random_state = random.getstate()
+    # with open('./checkpoint/random_state.pickle', 'wb') as f:
+    #     pickle.dump(random_state, f)
+
+    # torch_state = torch.get_rng_state()
+    # torch.save(torch_state, './checkpoint/torch_state.pth')
+
+    # cuda_state = torch.cuda.get_rng_state()
+    # torch.save(cuda_state, './checkpoint/cuda_state.pth')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--inference', type=str,default='False')
+parser.add_argument('--train', type=str,default='True')
+args = parser.parse_args()
+
+def worker_init(worker_id):
+    random.seed(SEED + worker_id)
+
+# def set_epoch(epoch,g):
+#     g.manual_seed(5728479885 + epoch)   
+
+# def worker_init(worker_id):
+#     worker_seed = torch.initial_seed() % 2**32
+#     np.random.seed(worker_seed)
+#     random.seed(worker_seed)
+
+if __name__ == "__main__":
+    
+    deterministic = True
+    if not deterministic:
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+    else:
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+    
+    random.seed(SEED)    
+    np.random.seed(SEED)  
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED) 
+
+    if os.path.isfile('/content/drive/MyDrive/checkpoint/numpy_state.pickle'):
+        with open('/content/drive/MyDrive/checkpoint/numpy_state.pickle', 'rb') as f:
+            numpy_state = pickle.load(f) 
+        np.random.set_state(numpy_state)
+   
+    if os.path.isfile('/content/drive/MyDrive/checkpoint/random_state.pickle'):
+        with open('/content/drive/MyDrive/checkpoint/random_state.pickle', 'rb') as f:
+            random_state = pickle.load(f) 
+        random.setstate(random_state)
+    
+
+    if os.path.isfile('/content/drive/MyDrive/checkpoint/torch_state.pth'):
+        torch_state = torch.load('/content/drive/MyDrive/checkpoint/torch_state.pth')
+        torch.set_rng_state(torch_state)
+
+    if os.path.isfile('/content/drive/MyDrive/checkpoint/cuda_state.pth'):
+        cuda_state = torch.load('/content/drive/MyDrive/checkpoint/cuda_state.pth')
+        torch.cuda.set_rng_state(cuda_state)
+
+    main(args)
+    
---
+++ trainer_s.py
@@ -0,0 +1,196 @@
+import utils
+from utils import cosine_scheduler
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.modules.loss import CrossEntropyLoss
+from utils import DiceLoss,atten_loss,prototype_loss,IM_loss
+from tqdm import tqdm
+from utils import print_progress
+import torch.nn.functional as F
+import warnings
+warnings.filterwarnings("ignore")
+
+def loss_kd_regularization(outputs, masks):
+    """
+    loss function for mannually-designed regularization: Tf-KD_{reg}
+    """
+    correct_prob = 0.9    # the probability for correct class in u(k)
+    K = outputs.size(1)
+
+    teacher_scores = torch.ones_like(outputs).cuda()
+    teacher_scores = teacher_scores*(1-correct_prob)/(K-1)  # p^d(k)
+
+    teacher_scores[masks] = correct_prob
+
+    return teacher_scores
+
+def prediction_map_distillation(y, masks, T=2.0) :
+    """
+    basic KD loss function based on "Distilling the Knowledge in a Neural Network"
+    https://arxiv.org/abs/1503.02531
+    :param y: student score map
+    :param teacher_scores: teacher score map
+    :param T:  for softmax
+    :return: loss value
+    """
+    y = y.cuda()
+    masks = masks.long()
+    masks = masks.cuda()
+
+    bin_masks = masks
+    bin_masks[bin_masks!=0] = 1.0 
+
+    masks_temp = F.one_hot(masks, num_classes=9)
+    masks_temp = torch.permute(masks_temp, (0, 3, 1, 2))
+    masks_temp = masks_temp.bool()
+
+    teacher_scores = loss_kd_regularization(outputs=y, masks=masks_temp)
+
+    y_prime = y * bin_masks.unsqueeze(dim=1).expand_as(y)
+    teacher_scores_prime = teacher_scores * bin_masks.unsqueeze(dim=1).expand_as(teacher_scores)
+
+    p = F.log_softmax(y_prime / T , dim=1)
+    q = F.softmax(teacher_scores_prime / T, dim=1)
+
+    p = p.view(-1, 2)
+    q = q.view(-1, 2)
+
+    l_kl = F.kl_div(p, q, reduction='batchmean') * (T ** 2)
+    return l_kl
+
+
+def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class,lr_scheduler,writer,logger,loss_function):
+    torch.autograd.set_detect_anomaly(True)
+    print(f'Epoch: {epoch_num} ---> Train , lr: {optimizer.param_groups[0]["lr"]}')
+    model.train()
+
+    loss_total = utils.AverageMeter()
+    loss_dice_total = utils.AverageMeter()
+    loss_ce_total = utils.AverageMeter()
+    loss_proto_total = utils.AverageMeter()
+    loss_kd_total = utils.AverageMeter()
+
+    Eval = utils.Evaluator(num_class=num_class)
+
+    mIOU = 0.0
+    Dice = 0.0
+
+    accuracy = utils.AverageMeter()
+
+    ce_loss = CrossEntropyLoss()
+    dice_loss = DiceLoss(num_class)
+    ##################################################################
+    # IMD_loss = IM_loss()
+    proto_loss = loss_function
+    ##################################################################
+    ##################################################################
+    # kd_loss = loss_function
+    ##################################################################
+    total_batchs = len(dataloader)
+    loader = dataloader 
+
+    base_iter = (epoch_num-1) * total_batchs
+    iter_num = base_iter
+    max_iterations = end_epoch * total_batchs
+    # max_iterations = 50 * total_batchs
+
+    for batch_idx, (inputs, targets) in enumerate(loader):
+
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        targets = targets.float()
+
+
+        # outputs = model(inputs)
+
+        outputs, up4, up3, up2, up1 = model(inputs)
+
+
+        loss_ce = ce_loss(outputs, targets[:].long())
+        loss_dice = dice_loss(outputs, targets, softmax=True)
+        # loss_proto = proto_loss(masks=targets, outputs=proto)
+        # loss_proto = proto_loss(masks=targets, up4=up4, up3=up3, up2=up2, up1=up1)
+        loss_proto = 0.0
+        # loss_proto = proto_loss(masks=targets.clone(), up3=up3, up2=up2, up1=up1)
+        # loss_kd = prediction_map_distillation(y=outputs, masks=targets)
+        # loss_kd = IMD_loss(masks=targets.clone(), up3=up3, up2=up2, up1=up1)
+        ###############################################
+        alpha = 0.01
+        beta = 0.0
+        # loss = 0.5 * loss_ce + 0.5 * loss_dice + beta * loss_kd 
+        loss = 0.5 * loss_ce + 0.5 * loss_dice + alpha * loss_proto
+        # loss = 0.5 * loss_ce + 0.5 * loss_dice 
+        ###############################################
+
+        lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9
+
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr_
+
+        iter_num = iter_num + 1        
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9
+        # for param_group in optimizer.param_groups:
+        #     param_group['lr'] = lr_
+
+        # iter_num = iter_num + 1
+
+        loss_total.update(loss)
+        loss_dice_total.update(loss_dice)
+        loss_ce_total.update(loss_ce)
+        loss_proto_total.update(loss_proto)
+        # loss_kd_total.update(loss_kd)
+        ###############################################
+        targets = targets.long()
+        predictions = torch.argmax(input=outputs,dim=1).long()
+        Eval.add_batch(gt_image=targets,pre_image=predictions)
+
+        accuracy.update(Eval.Pixel_Accuracy())
+
+        print_progress(
+            iteration=batch_idx+1,
+            total=total_batchs,
+            prefix=f'Train {epoch_num} Batch {batch_idx+1}/{total_batchs} ',
+            # suffix=f'Dice_loss = {loss_dice_total.avg:.4f} , CE_loss={loss_ce_total.avg:.4f} , Att_loss = {loss_att_total.avg:.6f} , mIoU = {Eval.Mean_Intersection_over_Union()*100:.2f} , Dice = {Eval.Dice()*100:.2f}',
+            # suffix=f'Dice_loss = {loss_dice_total.avg:.4f} , CE_loss={loss_ce_total.avg:.4f} , mIoU = {Eval.Mean_Intersection_over_Union()*100:.2f} , Dice = {Eval.Dice()*100:.2f}',          
+            suffix=f'Dice_loss = {0.5*loss_dice_total.avg:.4f} , CE_loss = {0.5*loss_ce_total.avg:.4f} , proto_loss = {alpha*loss_proto_total.avg:.8f} , Dice = {Eval.Dice()*100:.2f}',         
+            # suffix=f'Dice_loss = {0.5*loss_dice_total.avg:.4f} , CE_loss = {0.5*loss_ce_total.avg:.4f} , loss_kd = {beta*loss_kd_total.avg:.8f} , proto_loss = {alpha*loss_proto_total.avg:.8f} , Dice = {Eval.Dice()*100:.2f}',          
+            # suffix=f'Dice_loss = {0.5*loss_dice_total.avg:.4f} , CE_loss = {0.5*loss_ce_total.avg:.4f} , loss_kd = {beta*loss_kd_total.avg:.8f} , Dice = {Eval.Dice()*100:.2f}',          
+            bar_length=45
+        )  
+  
+    # acc = 100*accuracy.avg
+    # mIOU = 100*Eval.Mean_Intersection_over_Union()
+    # Dice = 100*Eval.Dice()
+    acc = 100*accuracy.avg
+    mIOU = 100*Eval.Mean_Intersection_over_Union()
+    Dice,Dice_per_class = Eval.Dice(per_class=True)
+    Dice,Dice_per_class = 100*Dice,100*Dice_per_class
+
+    if writer is not None:
+        writer.add_scalar('Loss/train', loss_total.avg.item(), epoch_num)
+        writer.add_scalar('Acc/train', acc.item(), epoch_num)
+        writer.add_scalar('Dice/train', Dice.item(), epoch_num)
+        writer.add_scalar('MIoU/train', mIOU.item(), epoch_num)
+
+    if lr_scheduler is not None:
+        lr_scheduler.step()        
+        
+    logger.info(f'Epoch: {epoch_num} ---> Train , Loss: {loss_total.avg:.4f} , mIoU: {mIOU:.2f} , Dice: {Dice:.2f} , Pixel Accuracy: {acc:.2f}, lr: {optimizer.param_groups[0]["lr"]}')
+
+    # Save checkpoint
+    if ckpt is not None:
+        ckpt.save_best(acc=Dice, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=lr_scheduler)
+    if ckpt is not None:
+        ckpt.save_last(acc=Dice, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=lr_scheduler)
+    # if ckpt is not None and (epoch_num==end_epoch):
+    #     ckpt.save_last(acc=Dice, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=lr_scheduler)
+    # if ckpt is not None and (early_stopping < ckpt.early_stopping(epoch_num)):
+    #     ckpt.save_last(acc=Dice, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=lr_scheduler)  
+
+
