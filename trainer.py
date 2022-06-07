7c565d536259c7b0dfe8e48856340af007f9c0b5
saleknia <salekniaamirhossein@gmail.com>
2022-06-07 10:08:24 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -141,7 +141,7 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
         # loss_kd = prediction_map_distillation(y=outputs, masks=targets)
         # loss_kd = IMD_loss(masks=targets.clone(), up3=up3, up2=up2, up1=up1)
         ###############################################
-        alpha = 0.01
+        alpha = 0.1
         beta = 0.0
         loss = 0.5 * loss_ce + 0.5 * loss_dice + alpha * loss_proto 
         # loss = 0.5 * loss_ce + 0.5 * loss_dice + alpha * loss_proto + beta * loss_kd
