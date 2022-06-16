839e9a19813b518b612e1503175b649ec24ebcb3
saleknia <salekniaamirhossein@gmail.com>
2022-06-16 14:18:58 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -136,21 +136,19 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
         loss_ce = ce_loss(outputs, targets[:].long())
         loss_dice = dice_loss(outputs, targets, softmax=True)
 
-        # loss_proto = proto_loss(masks=targets.clone(), up4=up4, up3=up3, up2=up2, up1=up1)
-        # loss_kd = kd_loss(e5=e5)
+        loss_proto = proto_loss(masks=targets.clone(), up4=up4, up3=up3, up2=up2, up1=up1)
+        loss_kd = kd_loss(e5=e5)
 
-        # loss_MM = MM_loss(probs1=probs1, probs2=probs2, probs3=probs3, probs4=probs4)
-
-        loss_proto = 0.0
-        loss_kd = 0.0
+        # loss_proto = 0.0
+        # loss_kd = 0.0
 
         # loss_kd = prediction_map_distillation(y=outputs, masks=targets)
         # loss_kd = IMD_loss(masks=targets.clone(), up3=up3, up2=up2, up1=up1)
         ###############################################
         alpha = 0.01
         beta = 0.01
-        loss = 0.4 * loss_ce + 0.6 * loss_dice 
-        # loss = 0.4 * loss_ce + 0.6 * loss_dice + alpha * loss_proto + beta * loss_kd 
+        # loss = 0.4 * loss_ce + 0.6 * loss_dice 
+        loss = 0.4 * loss_ce + 0.6 * loss_dice + alpha * loss_proto + beta * loss_kd 
         ###############################################
 
         lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9
