0db89e2cf6a68c2665aa0f065b28a1182b4cec2c
saleknia <salekniaamirhossein@gmail.com>
2022-06-22 14:32:48 +0000

    Commit Process

--- models/att_unet_loss.py
+++ models/att_unet_loss.py
@@ -97,20 +97,20 @@ class seg_head(nn.Module):
         self.RELU6 = nn.ReLU6()
         self.out = nn.Conv2d(64, 9, kernel_size=(1,1), stride=(1,1))
     def forward(self, up4, up3, up2, up1):
-        up1 = torchvision.ops.stochastic_depth(input=up1, p=0.5, mode='batch')
-        up2 = torchvision.ops.stochastic_depth(input=up2, p=0.5, mode='batch')
-        up3 = torchvision.ops.stochastic_depth(input=up3, p=0.5, mode='batch')
-        up4 = self.scale_4(self.conv_4(up4))
-        up3 = up3 + up4
+        up22 = torchvision.ops.stochastic_depth(input=up2, p=0.5, mode='batch')
+        up33 = torchvision.ops.stochastic_depth(input=up3, p=0.5, mode='batch')
+        up44 = torchvision.ops.stochastic_depth(input=up4, p=0.5, mode='batch')
+        up4 = self.scale_4(self.conv_4(up44))
+        up3 = up33 + up4
         up3 = self.scale_3(self.conv_3(up3))
-        up2 = up3 + up2
+        up2 = up3 + up22
         up2 = self.scale_2(self.conv_2(up2))
         up = up2 + up1
         up = self.conv(up)
         up = self.BN(up)
         up = self.RELU6(up)
         up = self.out(up)
-        return up
+        return up, up4, up3, up2, up1 
 
 
 class AttentionUNet_loss(nn.Module):
@@ -187,7 +187,7 @@ class AttentionUNet_loss(nn.Module):
         d2 = self.UpConv2(d2)
 
         # out = self.Conv(d2)
-        out = self.head(up4=d5, up3=d4, up2=d3, up1=d2)
+        out, d5, d4, d3, d2 = self.head(up4=d5, up3=d4, up2=d3, up1=d2)
         
         if self.training:
             return out, d5, d4, d3, d2, e5, e4, e3, e2, e1
