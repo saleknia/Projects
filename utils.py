2611fc4d11f2109a4ff38bd9e0266635514e09d9
saleknia <salekniaamirhossein@gmail.com>
2022-07-06 05:57:31 +0000

    Commit Process

--- utils.py
+++ utils.py
@@ -909,15 +909,15 @@ class prototype_loss(nn.Module):
             # weights = 1.0 - weights 
             # l = l + (torch.mean(weights * distances)) + (1.0 / torch.mean(distances))
             
-            x = self.protos[k][batch_indexs].unsqueeze(dim=0).clone().detach()
+            x = self.protos[k][indexs].unsqueeze(dim=0).clone().detach()
             weights = torch.cdist(x, x, p=2.0)[0]
             weights = weights - weights.min()
             if weights.max()!=0:
                 weights = weights / weights.max()
             weights = 1.0 - weights 
-            weights = weights[:,0:length]
+            # weights = weights[:,0:length]
 
-            l = l + (1.0 / torch.mean(distances_c[0]-diagonal)) + (torch.mean(weights * distances_c[0]))
+            l = l + (1.0 / torch.mean(distances_c[0]-diagonal)) + (torch.mean(weights * (distances_c[0]-diagonal)))
             l = l + (1.0 * (torch.mean(diagonal)))
             # l = l + cosine_loss_s
             loss = loss + l
