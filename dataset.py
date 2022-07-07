baca756ca7fdce6dc5ef4e8fb8d5dd1ba9c1b5ad
saleknia <salekniaamirhossein@gmail.com>
2022-07-07 15:24:00 +0000

    Commit Process

--- dataset.py
+++ dataset.py
@@ -404,6 +404,7 @@ class CT_1K(Dataset):
         data_path = os.path.join(self.data_dir, slice_name)
         data = np.load(data_path)
         image, mask = data['image'], data['label']
+        mask[mask==12] = 0
 
 
         sample = {'image': image, 'label': mask}
