b89de7bb51287aa193cede86ebd113272060a374
saleknia <salekniaamirhossein@gmail.com>
2022-07-09 21:02:15 +0000

    Commit Process

--- models/ESPNet_loss.py
+++ models/ESPNet_loss.py
@@ -404,6 +404,6 @@ class ESPNet_loss(nn.Module):
         classifier = self.classifier(concat_features)
 
         if self.training:
-            return classifier, output0, concat_features, output1, output2,         
+            return classifier, output2, output1, concat_features, output0
         else:
             return classifier
