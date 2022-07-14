053e41307280cd2c1559d16933a032f279a4bfc4
saleknia <salekniaamirhossein@gmail.com>
2022-07-14 12:27:53 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -1,193 +1,238 @@
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+
+
+# class Mobile_netV2_loss(nn.Module):
+#     def __init__(self,
+#                  num_classes=1000,
+#                  width_mult=1.0,
+#                  inverted_residual_setting=None,
+#                  round_nearest=8,
+#                  block=None,
+#                  norm_layer=None):
+#         """
+#         MobileNet V2 main class
+
+#         Args:
+#             num_classes (int): Number of classes
+#             width_mult (float): Width multiplier - adjusts number of channels in each layer by this amount
+#             inverted_residual_setting: Network structure
+#             round_nearest (int): Round the number of channels in each layer to be a multiple of this number
+#             Set to 1 to turn off rounding
+#             block: Module specifying inverted residual building block for mobilenet
+#             norm_layer: Module specifying the normalization layer to use
+
+#         """
+#         super(Mobile_netV2_loss, self).__init__()
+
+#         if block is None:
+#             block = InvertedResidual
+
+#         if norm_layer is None:
+#             norm_layer = nn.BatchNorm2d
+
+#         input_channel = 32
+#         last_channel = 1280
+
+#         if inverted_residual_setting is None:
+#             inverted_residual_setting = [
+#                 # t, c, n, s
+#                 [1, 16, 1, 1],
+#                 [6, 24, 2, 1],
+#                 [6, 32, 3, 2],
+#                 [6, 64, 4, 1],
+#                 [6, 96, 3, 1],
+#                 [6, 160, 3, 2],
+#                 [6, 320, 1, 1],
+#             ]
+
+#         # only check the first element, assuming user knows t,c,n,s are required
+#         if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
+#             raise ValueError("inverted_residual_setting should be non-empty "
+#                              "or a 4-element list, got {}".format(inverted_residual_setting))
+
+#         # building first layer
+#         input_channel = _make_divisible(input_channel * width_mult, round_nearest)
+#         self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
+#         features = [ConvBNReLU(3, input_channel, stride=2, norm_layer=norm_layer)]
+#         # building inverted residual blocks
+#         for t, c, n, s in inverted_residual_setting:
+#             output_channel = _make_divisible(c * width_mult, round_nearest)
+#             for i in range(n):
+#                 stride = s if i == 0 else 1
+#                 features.append(block(input_channel, output_channel, stride, expand_ratio=t, norm_layer=norm_layer))
+#                 input_channel = output_channel
+#         # building last several layers
+#         features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1, norm_layer=norm_layer))
+#         # make it nn.Sequential
+#         self.features = nn.Sequential(*features)
+
+#         # building classifier
+#         self.classifier = nn.Sequential(
+#             nn.Dropout(0.2),
+#             nn.Linear(self.last_channel, num_classes),
+#         )
+
+#         # self.last_conv = nn.Sequential(
+#         #     nn.Conv2d(self.last_channel, num_classes, kernel_size=1),
+#         #     nn.BatchNorm2d(num_classes),
+#         #     nn.ReLU(inplace=True)
+#         # )
+#         self.last_conv = nn.Conv2d(self.last_channel, num_classes, kernel_size=1)
+
+#         # weight initialization
+#         for m in self.modules():
+#             if isinstance(m, nn.Conv2d):
+#                 nn.init.kaiming_normal_(m.weight, mode='fan_out')
+#                 if m.bias is not None:
+#                     nn.init.zeros_(m.bias)
+#             elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+#                 nn.init.ones_(m.weight)
+#                 nn.init.zeros_(m.bias)
+#             elif isinstance(m, nn.Linear):
+#                 nn.init.normal_(m.weight, 0, 0.01)
+#                 nn.init.zeros_(m.bias)
+
+#     def _forward_impl(self, x):
+#         input_size = x.shape[-2:]
+#         # This exists since TorchScript doesn't support inheritance, so the superclass method
+#         # (this one) needs to have a name other than `forward` that can be accessed in a subclass
+#         x = self.features[0](x)
+#         x = self.features[1](x)
+#         x = self.features[2](x)
+#         x = self.features[3](x)
+#         x = self.features[4](x)
+#         x = self.features[5](x)
+#         x = self.features[6](x)
+#         x = self.features[7](x)
+#         x = self.features[8](x)
+#         x = self.features[9](x)
+#         x = self.features[10](x)
+#         x = self.features[11](x)
+#         x = self.features[12](x)
+#         up4 = self.features[13](x)
+#         x = self.features[14](up4)
+#         x = self.features[15](x)
+#         x = self.features[16](x)
+#         up3 = self.features[17](x)
+#         x = self.features[18](up3)
+#         # Cannot use "squeeze" as batch-size can be 1 => must use reshape with x.shape[0]
+#         up2 = self.last_conv(x)
+#         up1 = F.interpolate(up2, size=input_size, mode='bilinear', align_corners=True)
+#         if self.training:
+#             return up1, up4, up3, up2, up1
+#         else:
+#             return up1
+#     def forward(self, x):
+#         x = torch.cat([x, x, x], dim=1)  # 扩充为3通道
+#         return self._forward_impl(x)
+
+
+# def _make_divisible(v, divisor, min_value=None):
+#     """
+#     This function is taken from the original tf repo.
+#     It ensures that all layers have a channel number that is divisible by 8
+#     It can be seen here:
+#     https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+#     :param v:
+#     :param divisor:
+#     :param min_value:
+#     :return:
+#     """
+#     if min_value is None:
+#         min_value = divisor
+#     new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+#     # Make sure that round down does not go down by more than 10%.
+#     if new_v < 0.9 * v:
+#         new_v += divisor
+#     return new_v
+
+
+# class ConvBNReLU(nn.Sequential):
+#     def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1, norm_layer=None):
+#         padding = (kernel_size - 1) // 2
+#         if norm_layer is None:
+#             norm_layer = nn.BatchNorm2d
+#         super(ConvBNReLU, self).__init__(
+#             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+#             norm_layer(out_planes),
+#             nn.ReLU6(inplace=True)
+#         )
+
+
+# class InvertedResidual(nn.Module):
+#     def __init__(self, inp, oup, stride, expand_ratio, norm_layer=None):
+#         super(InvertedResidual, self).__init__()
+#         self.stride = stride
+#         assert stride in [1, 2]
+
+#         if norm_layer is None:
+#             norm_layer = nn.BatchNorm2d
+
+#         hidden_dim = int(round(inp * expand_ratio))
+#         self.use_res_connect = self.stride == 1 and inp == oup
+
+#         layers = []
+#         if expand_ratio != 1:
+#             # pw
+#             layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1, norm_layer=norm_layer))
+#         layers.extend([
+#             # dw
+#             ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, norm_layer=norm_layer),
+#             # pw-linear
+#             nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+#             norm_layer(oup),
+#         ])
+#         self.conv = nn.Sequential(*layers)
+
+#     def forward(self, x):
+#         if self.use_res_connect:
+#             return x + self.conv(x)
+#         else:
+#             return self.conv(x)
+
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models import resnet18
 
 
 class Mobile_netV2_loss(nn.Module):
-    def __init__(self,
-                 num_classes=1000,
-                 width_mult=1.0,
-                 inverted_residual_setting=None,
-                 round_nearest=8,
-                 block=None,
-                 norm_layer=None):
-        """
-        MobileNet V2 main class
-
-        Args:
-            num_classes (int): Number of classes
-            width_mult (float): Width multiplier - adjusts number of channels in each layer by this amount
-            inverted_residual_setting: Network structure
-            round_nearest (int): Round the number of channels in each layer to be a multiple of this number
-            Set to 1 to turn off rounding
-            block: Module specifying inverted residual building block for mobilenet
-            norm_layer: Module specifying the normalization layer to use
-
-        """
+    def __init__(self, num_classes=2, pretrained=False):
         super(Mobile_netV2_loss, self).__init__()
+        model = resnet18(pretrained)
 
-        if block is None:
-            block = InvertedResidual
+        # take pretrained resnet, except AvgPool and FC
+        self.conv1 = model.conv1
+        self.bn1 = model.bn1
+        self.relu = model.relu
+        self.maxpool = model.maxpool
+        self.layer1 = model.layer1
+        self.layer2 = model.layer2
+        self.layer3 = model.layer3
+        self.layer4 = model.layer4
+        self.last_conv = nn.Conv2d(512, num_classes, kernel_size=1)
 
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-
-        input_channel = 32
-        last_channel = 1280
-
-        if inverted_residual_setting is None:
-            inverted_residual_setting = [
-                # t, c, n, s
-                [1, 16, 1, 1],
-                [6, 24, 2, 1],
-                [6, 32, 3, 2],
-                [6, 64, 4, 1],
-                [6, 96, 3, 1],
-                [6, 160, 3, 2],
-                [6, 320, 1, 1],
-            ]
-
-        # only check the first element, assuming user knows t,c,n,s are required
-        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
-            raise ValueError("inverted_residual_setting should be non-empty "
-                             "or a 4-element list, got {}".format(inverted_residual_setting))
-
-        # building first layer
-        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
-        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
-        features = [ConvBNReLU(3, input_channel, stride=2, norm_layer=norm_layer)]
-        # building inverted residual blocks
-        for t, c, n, s in inverted_residual_setting:
-            output_channel = _make_divisible(c * width_mult, round_nearest)
-            for i in range(n):
-                stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t, norm_layer=norm_layer))
-                input_channel = output_channel
-        # building last several layers
-        features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1, norm_layer=norm_layer))
-        # make it nn.Sequential
-        self.features = nn.Sequential(*features)
-
-        # building classifier
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.2),
-            nn.Linear(self.last_channel, num_classes),
-        )
-
-        # self.last_conv = nn.Sequential(
-        #     nn.Conv2d(self.last_channel, num_classes, kernel_size=1),
-        #     nn.BatchNorm2d(num_classes),
-        #     nn.ReLU(inplace=True)
-        # )
-        self.last_conv = nn.Conv2d(self.last_channel, num_classes, kernel_size=1)
-
-        # weight initialization
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.zeros_(m.bias)
-
-    def _forward_impl(self, x):
+    def forward(self, x):
         input_size = x.shape[-2:]
-        # This exists since TorchScript doesn't support inheritance, so the superclass method
-        # (this one) needs to have a name other than `forward` that can be accessed in a subclass
-        x = self.features[0](x)
-        x = self.features[1](x)
-        x = self.features[2](x)
-        x = self.features[3](x)
-        x = self.features[4](x)
-        x = self.features[5](x)
-        x = self.features[6](x)
-        x = self.features[7](x)
-        x = self.features[8](x)
-        x = self.features[9](x)
-        x = self.features[10](x)
-        x = self.features[11](x)
-        x = self.features[12](x)
-        up4 = self.features[13](x)
-        x = self.features[14](up4)
-        x = self.features[15](x)
-        x = self.features[16](x)
-        up3 = self.features[17](x)
-        x = self.features[18](up3)
-        # Cannot use "squeeze" as batch-size can be 1 => must use reshape with x.shape[0]
-        up2 = self.last_conv(x)
-        up1 = F.interpolate(up2, size=input_size, mode='bilinear', align_corners=True)
-        if self.training:
-            return up1, up4, up3, up2, up1
-        else:
-            return up1
-    def forward(self, x):
         x = torch.cat([x, x, x], dim=1)  # 扩充为3通道
-        return self._forward_impl(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        low = x
+        x = self.maxpool(x)
 
-
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
-
-
-class ConvBNReLU(nn.Sequential):
-    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1, norm_layer=None):
-        padding = (kernel_size - 1) // 2
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-        super(ConvBNReLU, self).__init__(
-            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
-            norm_layer(out_planes),
-            nn.ReLU6(inplace=True)
-        )
-
-
-class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio, norm_layer=None):
-        super(InvertedResidual, self).__init__()
-        self.stride = stride
-        assert stride in [1, 2]
-
-        if norm_layer is None:
-            norm_layer = nn.BatchNorm2d
-
-        hidden_dim = int(round(inp * expand_ratio))
-        self.use_res_connect = self.stride == 1 and inp == oup
-
-        layers = []
-        if expand_ratio != 1:
-            # pw
-            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1, norm_layer=norm_layer))
-        layers.extend([
-            # dw
-            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim, norm_layer=norm_layer),
-            # pw-linear
-            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-            norm_layer(oup),
-        ])
-        self.conv = nn.Sequential(*layers)
-
-    def forward(self, x):
-        if self.use_res_connect:
-            return x + self.conv(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        high = x
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.last_conv(x)
+        x = F.interpolate(x, size=input_size, mode='bilinear', align_corners=True)
+        if self.training:
+            return x, None, None, None, None
         else:
-            return self.conv(x)
+            return x
