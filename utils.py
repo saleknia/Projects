8a2b339fc8d7bc986071c3785491d78fc1f23c65
saleknia <salekniaamirhossein@gmail.com>
2022-07-23 20:40:18 +0000

    Commit Process

--- utils.py
+++ utils.py
@@ -971,7 +971,7 @@ class prototype_loss(nn.Module):
         # self.down_scales = [1.0,1.0,0.5,0.25,0.125]
 
         # ENet
-        # self.down_scales = [0.5,0.25,0.125,0.125]
+        # self.down_scales = [1.0,0.5,0.25,0.125,0.125]
 
         # ESPNet
         self.down_scales = [1.0,0.5,0.5,0.25,0.125]
@@ -1000,18 +1000,18 @@ class prototype_loss(nn.Module):
         # self.proto_4 = torch.zeros(num_class, 512)
 
         # ENet
-        self.proto_0 = torch.zeros(num_class, 9  )
-        self.proto_1 = torch.zeros(num_class, 16 )
-        self.proto_2 = torch.zeros(num_class, 64 )
-        self.proto_3 = torch.zeros(num_class, 128)
-        self.proto_4 = torch.zeros(num_class, 128)
-
-        # ESPNet
         # self.proto_0 = torch.zeros(num_class, 9  )
         # self.proto_1 = torch.zeros(num_class, 16 )
-        # self.proto_2 = torch.zeros(num_class, 9  )
-        # self.proto_3 = torch.zeros(num_class, 64 )
+        # self.proto_2 = torch.zeros(num_class, 64 )
+        # self.proto_3 = torch.zeros(num_class, 128)
         # self.proto_4 = torch.zeros(num_class, 128)
+
+        # ESPNet
+        self.proto_0 = torch.zeros(num_class, 9  )
+        self.proto_1 = torch.zeros(num_class, 16 )
+        self.proto_2 = torch.zeros(num_class, 9  )
+        self.proto_3 = torch.zeros(num_class, 64 )
+        self.proto_4 = torch.zeros(num_class, 128)
 
         # SUNet
         # self.proto_0 = torch.zeros(num_class, self.num_class +1)
@@ -1152,6 +1152,16 @@ class prototype_loss(nn.Module):
             p = p.long().item()
             self.momentum = self.momentum_schedule[self.iteration] 
             self.protos[k][p-1] = self.protos[k][p-1] * self.momentum + prototypes[count] * (1 - self.momentum)
+        
+        # if self.iteration % 689==0:
+        #     self.iteration = 0
+        #     self.momentum_schedule = cosine_scheduler(0.85, 1.0, 5.0, 138)
+        #     self.proto_0 = torch.zeros(self.num_class, 9  )
+        #     self.proto_1 = torch.zeros(self.num_class, 16 )
+        #     self.proto_2 = torch.zeros(self.num_class, 64 )
+        #     self.proto_3 = torch.zeros(self.num_class, 128)
+        #     self.proto_4 = torch.zeros(self.num_class, 128)
+        #     self.protos = [self.proto_0,self.proto_1, self.proto_2, self.proto_3, self.proto_4]
 
 
 class CriterionPixelWise(nn.Module):
