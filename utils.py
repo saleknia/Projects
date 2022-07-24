1aa9bb680b141036bde1f0ab244027f3632b3e98
saleknia <salekniaamirhossein@gmail.com>
2022-07-24 17:50:16 +0000

    Commit Process

--- utils.py
+++ utils.py
@@ -974,10 +974,13 @@ class prototype_loss(nn.Module):
         # self.down_scales = [1.0,0.5,0.25,0.125,0.125]
 
         # ESPNet
-        self.down_scales = [1.0,0.5,0.5,0.25,0.125]
+        # self.down_scales = [1.0,0.5,0.5,0.25,0.125]
 
         # Mobile_NetV2
         # self.down_scales = [1.0,0.125,0.125,0.25,0.25]
+
+        # SUNet
+        self.down_scales = [1.0,1.0,0.5,0.25,0.125]
 
         # ResNet_18
         # self.down_scales = [1.0, 0.25, 0.125, 0.0625, 0.03125]
