a8b694e72d12d60731cf61d4c2478d8e50e8dbd0
saleknia <salekniaamirhossein@gmail.com>
2022-08-01 20:01:00 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -11,207 +11,8 @@ import torch.nn.functional as F
 import warnings
 from utils import focal_loss
 from torch.autograd import Variable
+import pickle
 warnings.filterwarnings("ignore")
-
-def one_hot_loss(exist_pred, targets):
-    targets=targets.cpu()
-    exist_pred=exist_pred.cpu()
-    yp = torch.zeros(6,9)
-    for i in range(6):
-        unique_num = torch.unique(targets[i])
-        for j in range(9):
-            if j in unique_num:
-                yp[i,j] = 1.0
-            else:
-                yp[i,j] = 0.0
-    loss = torch.nn.functional.binary_cross_entropy(input=exist_pred, target=yp)
-    return loss
-
-def loss_kd_regularization(outputs, masks):
-    """
-    loss function for mannually-designed regularization: Tf-KD_{reg}
-    """
-    correct_prob = 0.95    # the probability for correct class in u(k)
-    K = outputs.size(1)
-
-    teacher_scores = torch.ones_like(outputs).cuda()
-    teacher_scores = teacher_scores*(1-correct_prob)/(K-1)  # p^d(k)
-
-    teacher_scores[masks] = correct_prob
-
-    return teacher_scores
-
-def prediction_map_distillation(y, masks) :
-    """
-    basic KD loss function based on "Distilling the Knowledge in a Neural Network"
-    https://arxiv.org/abs/1503.02531
-    :param y: student score map
-    :param teacher_scores: teacher score map
-    :param T:  for softmax
-    :return: loss value
-    """
-    y = y.cuda()
-    masks = masks.long()
-    masks = masks.cuda()
-
-    masks_temp = F.one_hot(masks, num_classes=9)
-    masks_temp = torch.permute(masks_temp, (0, 3, 1, 2))
-    masks_temp = masks_temp.bool()
-
-    teacher_scores = loss_kd_regularization(outputs=y, masks=masks_temp)
-
-    return teacher_scores
-
-def flatten(tensor):
-    """Flattens a given tensor such that the channel axis is first.
-    The shapes are transformed as follows:
-       (N, C, D, H, W) -> (C, N * D * H * W)
-    """
-    # number of channels
-    C = tensor.size(1)
-    # new axis order
-    axis_order = (1, 0) + tuple(range(2, tensor.dim()))
-    # Transpose: (N, C, D, H, W) -> (C, N, D, H, W)
-    transposed = tensor.permute(axis_order)
-    # Flatten: (C, N, D, H, W) -> (C, N * D * H * W)
-    return transposed.contiguous().view(C, -1)
-
-class _AbstractDiceLoss(nn.Module):
-    """
-    Base class for different implementations of Dice loss.
-    """
-
-    def __init__(self, weight=None, normalization='sigmoid'):
-        super(_AbstractDiceLoss, self).__init__()
-        self.register_buffer('weight', weight)
-        # The output from the network during training is assumed to be un-normalized probabilities and we would
-        # like to normalize the logits. Since Dice (or soft Dice in this case) is usually used for binary data,
-        # normalizing the channels with Sigmoid is the default choice even for multi-class segmentation problems.
-        # However if one would like to apply Softmax in order to get the proper probability distribution from the
-        # output, just specify `normalization=Softmax`
-        assert normalization in ['sigmoid', 'softmax', 'none']
-        if normalization == 'sigmoid':
-            self.normalization = nn.Sigmoid()
-        elif normalization == 'softmax':
-            self.normalization = nn.Softmax(dim=1)
-        else:
-            self.normalization = lambda x: x
-
-    def dice(self, input, target, weight):
-        # actual Dice score computation; to be implemented by the subclass
-        raise NotImplementedError
-
-    def forward(self, input, target):
-        # get probabilities from logits
-        input = self.normalization(input)
-
-        # compute per channel Dice coefficient
-        per_channel_dice = self.dice(input, target, weight=self.weight)
-
-        # average Dice score across all channels/classes
-        return 1. - torch.mean(per_channel_dice)
-
-class GeneralizedDiceLoss(_AbstractDiceLoss):
-    """Computes Generalized Dice Loss (GDL) as described in https://arxiv.org/pdf/1707.03237.pdf.
-    """
-
-    def __init__(self, num_classes, normalization='softmax', epsilon=1e-6):
-        super().__init__(weight=None, normalization=normalization)
-        self.epsilon = epsilon
-        self.num_classes = num_classes
-    def _one_hot_encoder(self, input_tensor):
-        tensor_list = []
-        for i in range(self.num_classes):
-            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
-            tensor_list.append(temp_prob.unsqueeze(1))
-        output_tensor = torch.cat(tensor_list, dim=1)
-        return output_tensor.float()
-
-    def dice(self, input, target, weight):
-        target = self._one_hot_encoder(target)
-        assert input.size() == target.size(), "'input' and 'target' must have the same shape"
-
-        input = flatten(input)
-        target = flatten(target)
-        target = target.float()
-
-        if input.size(0) == 1:
-            # for GDL to make sense we need at least 2 channels (see https://arxiv.org/pdf/1707.03237.pdf)
-            # put foreground and background voxels in separate channels
-            input = torch.cat((input, 1 - input), dim=0)
-            target = torch.cat((target, 1 - target), dim=0)
-
-        # GDL weighting: the contribution of each label is corrected by the inverse of its volume
-        w_l = target.sum(-1)
-        w_l = 1 / (w_l * w_l).clamp(min=self.epsilon)
-        w_l.requires_grad = False
-
-        intersect = (input * target).sum(-1)
-        intersect = intersect * w_l
-
-        denominator = (input + target).sum(-1)
-        denominator = (denominator * w_l).clamp(min=self.epsilon)
-
-        return 2 * (intersect.sum() / denominator.sum())
-
-class WeightedCrossEntropyLoss(nn.Module):
-    """WeightedCrossEntropyLoss (WCE) as described in https://arxiv.org/pdf/1707.03237.pdf
-    """
-
-    def __init__(self, ignore_index=-1):
-        super(WeightedCrossEntropyLoss, self).__init__()
-        self.ignore_index = ignore_index
-
-    def forward(self, input, target):
-        weight = self._class_weights(input)
-        return F.cross_entropy(input, target, weight=weight, ignore_index=self.ignore_index)
-
-    @staticmethod
-    def _class_weights(input):
-        # normalize the input first
-        input = F.softmax(input, dim=1)
-        flattened = flatten(input)
-        nominator = (1. - flattened).sum(-1)
-        denominator = flattened.sum(-1)
-        class_weights = Variable(nominator / denominator, requires_grad=False)
-        return class_weights
-
-# def prediction_map_distillation(y, masks, T=4.0) :
-#     """
-#     basic KD loss function based on "Distilling the Knowledge in a Neural Network"
-#     https://arxiv.org/abs/1503.02531
-#     :param y: student score map
-#     :param teacher_scores: teacher score map
-#     :param T:  for softmax
-#     :return: loss value
-#     """
-#     y = y.cuda()
-#     masks = masks.long()
-#     masks = masks.cuda()
-
-#     bin_masks = masks
-#     bin_masks[bin_masks!=0] = 1.0 
-
-#     masks_temp = F.one_hot(masks, num_classes=9)
-#     masks_temp = torch.permute(masks_temp, (0, 3, 1, 2))
-#     masks_temp = masks_temp.bool()
-
-#     teacher_scores = loss_kd_regularization(outputs=y, masks=masks_temp)
-
-#     # y_prime = y * bin_masks.unsqueeze(dim=1).expand_as(y)
-#     # teacher_scores_prime = teacher_scores * bin_masks.unsqueeze(dim=1).expand_as(teacher_scores)
-
-#     y_prime = y
-#     teacher_scores_prime = teacher_scores 
-
-#     p = F.log_softmax(y_prime / T , dim=1)
-#     q = F.softmax(teacher_scores_prime / T, dim=1)
-
-#     p = p.view(-1, 2)
-#     q = q.view(-1, 2)
-
-#     l_kl = F.kl_div(p, q, reduction='batchmean') * (T ** 2)
-#     return l_kl
 
 
 def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class,lr_scheduler,writer,logger,loss_function):
@@ -224,8 +25,6 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
     loss_total = utils.AverageMeter()
     loss_dice_total = utils.AverageMeter()
     loss_ce_total = utils.AverageMeter()
-    loss_proto_total = utils.AverageMeter()
-    loss_kd_total = utils.AverageMeter()
 
     Eval = utils.Evaluator(num_class=num_class)
 
@@ -234,16 +33,10 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
 
     accuracy = utils.AverageMeter()
 
-    ce_loss = WeightedCrossEntropyLoss()
-    dice_loss = GeneralizedDiceLoss(num_classes=num_class)
-
     ce_loss = CrossEntropyLoss()
     dice_loss = DiceLoss(num_class)
     ##################################################################
-    # kd_out_loss = IM_loss()
-    # kd_out_loss = CriterionPixelWise()
-    kd_loss = M_loss()    
-    proto_loss = loss_function
+    proto = loss_function
     ##################################################################
     total_batchs = len(dataloader)
     loader = dataloader 
@@ -257,44 +50,16 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
         inputs, targets = inputs.to(device), targets.to(device)
 
         targets = targets.float()
-
         
         outputs, up4, up3, up2, up1 = model(inputs)
-        # e5, e4, e3, e2 = model(inputs, pretrain=True)
-        # outputs = torch.zeros(inputs.shape,device='cuda')
-     
-        targets = targets.long()
-        predictions = torch.argmax(input=outputs,dim=1).long()
-        overlap = (predictions==targets).float()
-        t_masks = targets * overlap
-        targets = targets.float()
-
-        # loss_ce = ce_loss(input=outputs, target=targets.long())
-        loss_dice = dice_loss(input=outputs, target=targets)
+        proto(masks=targets, outputs=outputs)
 
         loss_ce = ce_loss(outputs, targets[:].long())
-        # loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
+        loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
 
-        loss_proto = proto_loss(masks=targets.clone(), t_masks=t_masks, up4=up4, up3=up3, up2=up2, up1=up1, outputs=outputs)
-        # loss_kd = kd_loss(e5=e5, e4=e4, e3=e3, e2=e2)
-
-        # loss_proto = 0.0
-        loss_kd = 0.0
-        # loss_ce = 0
-        # loss_dice = 0
-
-        # loss_kd_out = prediction_map_distillation(y=outputs, masks=targets)
-        # loss_kd_out = kd_out_loss(masks=targets.clone(), x4=up4, x3=up3, x2=up2, x1=up1)
-        # loss_kd = kd_loss(e5)
-        ###############################################
-        alpha = 1.0
-        beta = 1.0
-        gamma = 0.01
-        # loss = alpha * loss_dice + beta * loss_ce 
-        loss = alpha * loss_dice + beta * loss_ce + gamma * loss_proto         
-        # loss = 0.5 * loss_ce + 0.5 * loss_dice + beta * loss_kd
-        # loss = loss_kd 
-        ###############################################
+        alpha = 0.5
+        beta = 0.5
+        loss = alpha * loss_dice + beta * loss_ce 
 
         lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9
 
@@ -306,15 +71,12 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        # lr_scheduler.step()
 
 
         loss_total.update(loss)
         loss_dice_total.update(loss_dice)
         loss_ce_total.update(loss_ce)
-        loss_proto_total.update(loss_proto)
-        loss_kd_total.update(loss_kd)
-        ###############################################
+
         targets = targets.long()
 
         predictions = torch.argmax(input=outputs,dim=1).long()
@@ -326,11 +88,7 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
             iteration=batch_idx+1,
             total=total_batchs,
             prefix=f'Train {epoch_num} Batch {batch_idx+1}/{total_batchs} ',
-            # suffix=f'Dice_loss = {loss_dice_total.avg:.4f} , CE_loss={loss_ce_total.avg:.4f} , Att_loss = {loss_att_total.avg:.6f} , mIoU = {Eval.Mean_Intersection_over_Union()*100:.2f} , Dice = {Eval.Dice()*100:.2f}',
-            # suffix=f'Dice_loss = {loss_dice_total.avg:.4f} , CE_loss={loss_ce_total.avg:.4f} , mIoU = {Eval.Mean_Intersection_over_Union()*100:.2f} , Dice = {Eval.Dice()*100:.2f}',          
-            # suffix=f'Dice_loss = {0.5*loss_dice_total.avg:.4f} , CE_loss = {0.5*loss_ce_total.avg:.4f} , proto_loss = {alpha*loss_proto_total.avg:.8f} , Dice = {Eval.Dice()*100:.2f}',         
-            suffix=f'Dice_loss = {alpha*loss_dice_total.avg:.4f} , CE_loss = {beta*loss_ce_total.avg:.4f} , proto_loss = {gamma*loss_proto_total.avg:.8f} , kd_loss = {beta*loss_kd_total.avg:.4f}, Dice = {Eval.Dice()*100:.2f}',          
-            # suffix=f'Dice_loss = {0.5*loss_dice_total.avg:.4f} , CE_loss = {0.5*loss_ce_total.avg:.4f} , loss_kd = {beta*loss_kd_total.avg:.8f} , Dice = {Eval.Dice()*100:.2f}',          
+            suffix=f'Dice_loss = {alpha*loss_dice_total.avg:.4f} , CE_loss = {beta*loss_ce_total.avg:.4f} , Dice = {Eval.Dice()*100:.2f}',          
             bar_length=45
         )  
   
@@ -352,6 +110,11 @@ def trainer(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_class
         lr_scheduler.step()        
         
     logger.info(f'Epoch: {epoch_num} ---> Train , Loss: {loss_total.avg:.4f} , mIoU: {mIOU:.2f} , Dice: {Dice:.2f} , Pixel Accuracy: {acc:.2f}, lr: {optimizer.param_groups[0]["lr"]}')
+
+
+    obj = proto
+    file_pi = open('prototypes', 'w') 
+    pickle.dump(obj, file_pi)
 
     # Save checkpoint
     if ckpt is not None:
