ec998f752c3a7a9d818d0f2566f7c7c4c719c3cc
saleknia <salekniaamirhossein@gmail.com>
2022-08-11 08:29:34 +0000

    Commit Process

--- utils.py
+++ utils.py
@@ -230,8 +230,8 @@ class disparity(nn.Module):
     def __init__(self):
         super(disparity, self).__init__()
         # self.down_scales = [1.0, 0.5, 0.25, 0.125]
-        # self.down_scales = [1.0, 0.5, 0.25, 0.125, 0.125]
-        self.down_scales = [1.0, 0.25, 0.125, 0.0625, 0.03125]
+        self.down_scales = [1.0, 0.5, 0.25, 0.125, 0.125]
+        # self.down_scales = [1.0, 0.25, 0.125, 0.0625, 0.03125]
 
 
 
