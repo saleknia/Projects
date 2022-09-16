0542aa890a26e117f991dcae9476bca083d5ddda
saleknia <salekniaamirhossein@gmail.com>
2022-09-16 10:09:07 +0000

    Commit Process

--- dataset.py
+++ dataset.py
@@ -25,6 +25,66 @@ import random
 import cv2
 from torch.utils import data
 import pickle
+from torch.utils.data import Dataset, DataLoader
+from einops.layers.torch import Rearrange
+from scipy.ndimage.morphology import binary_dilation
+
+# ===== normalize over the dataset 
+def dataset_normalized(imgs):
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized
+       
+
+class isic_loader(Dataset):
+    """ dataset class for Brats datasets
+    """
+    def __init__(self, path_Data='/content/drive/MyDrive/ISIC2017_dataset', split='train'):
+        super(isic_loader, self)
+        if split=='train':
+            self.train = True
+            self.data   = np.load(path_Data+'data_train.npy')
+            self.mask   = np.load(path_Data+'mask_train.npy')
+        elif split=='test':
+            self.train = False
+            self.data   = np.load(path_Data+'data_test.npy')
+            self.mask   = np.load(path_Data+'mask_test.npy')
+        elif split=='valid':
+            self.train = False
+            self.data   = np.load(path_Data+'data_val.npy')
+            self.mask   = np.load(path_Data+'mask_val.npy')          
+          
+        self.data   = dataset_normalized(self.data)
+        self.mask   = np.expand_dims(self.mask, axis=3)
+        self.mask   = self.mask /255.0
+         
+    def __getitem__(self, indx):
+        img = self.data[indx]
+        seg = self.mask[indx]
+        if self.train:
+            img, seg = self.apply_augmentation(img, seg)
+        
+        seg = torch.tensor(seg.copy())
+        img = torch.tensor(img.copy())
+        img = img.permute( 2, 0, 1)
+        seg = seg.permute( 2, 0, 1)
+        
+        return img, seg
+               
+    def apply_augmentation(self, img, seg):
+        if random.random() < 0.5:
+            img  = np.flip(img,  axis=1)
+            seg  = np.flip(seg,  axis=1)
+        return img, seg
+
+    def __len__(self):
+        return len(self.data)
+    
+
 
 def shift_2d_replace(data, dx, dy, constant=0.0):
     """
