0dbf179c70e2917e6baa4a26cd0e888ce1204361
saleknia <salekniaamirhossein@gmail.com>
2022-09-18 12:27:25 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -59,7 +59,9 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
         # t_masks = targets * overlap
         # targets = targets.float()
 
-        soft_label = 0.5 * (outputs + torch.nn.functional.one_hot(targets).permute(0,3,1,2))
+        # print(targets.shape)
+        # print(outputs.shape)
+        soft_label = 0.5 * (torch.nn.functional.softmax(outputs) + torch.nn.functional.one_hot(targets.long()).permute(0,3,1,2))
         loss_ce = ce_loss(outputs, targets[:].long()) + ce_loss(soft_label, targets[:].long())
         loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
         loss = 0.5 * loss_ce + 0.5 * loss_dice
