455fa955c52b536b1e8447dcbd3cd6d9869c4da4
saleknia <salekniaamirhossein@gmail.com>
2022-10-06 13:33:00 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -952,7 +952,7 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
 
         # transformer = torch.hub.load('facebookresearch/deit:main', 'deit_tiny_distilled_patch16_224', pretrained=True)
-        transformer = torch.hub.load('facebookresearch/deit:main', 'deit_tiny_distilled_patch16_224', pretrained=False)
+        transformer = deit_tiny_distilled_patch16_224(pretrained=True)
         resnet = resnet_model.resnet34(pretrained=True)
 
         self.firstconv = resnet.conv1
