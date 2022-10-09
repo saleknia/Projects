d8ab6bab3d597d75327c90d1b979a9cc9b9bfcbb
saleknia <salekniaamirhossein@gmail.com>
2022-10-09 15:48:16 +0000

    Commit Process

--- models/U_loss.py
+++ models/U_loss.py
@@ -98,20 +98,18 @@ import math
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None, ratio=4.0):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
         super().__init__()
-        mid_channels = out_channels // ratio
-        out_channels = out_channels // ratio
-
+        mid_channels = out_channels // 4
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(mid_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(mid_channels, mid_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels*ratio, kernel_size=1, padding=0, bias=False),
-            nn.BatchNorm2d(out_channels*ratio),
+            nn.Conv2d(mid_channels, out_channels, kernel_size=1, padding=0, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
