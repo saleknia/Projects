09c37dfd7956c665cedea6bdfc3b82212da98cc7
saleknia <salekniaamirhossein@gmail.com>
2022-10-11 17:29:42 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -111,17 +111,17 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
 
         loss_proto = 0.01 * kd_loss(targets, up4, up3, up2, up1, up4_t, up3_t, up2_t, up1_t)
-        # loss_kd = 0.1 * kd_loss(preds_S=outputs, preds_T=outputs_t)
-        # loss_att = 0.01 * (im_distill(up1, up1_t) + im_distill(up2, up2_t) + im_distill(up3, up3_t) + im_distill(up4, up4_t))
+        loss_kd = 0.1 * kd_loss(preds_S=outputs, preds_T=outputs_t)
+        loss_att = 0.01 * (im_distill(up1, up1_t) + im_distill(up2, up2_t) + im_distill(up3, up3_t) + im_distill(up4, up4_t))
 
         ###############################################
         alpha = 0.5
         beta = 0.5
 
-        # loss = alpha * loss_dice + beta * loss_ce
-        loss_kd = 0.0
-        loss_att = 0.0
-        proto_loss = 0.0
+        # # loss = alpha * loss_dice + beta * loss_ce
+        # loss_kd = 0.0
+        # loss_att = 0.0
+        # proto_loss = 0.0
 
         loss = alpha * loss_dice + beta * loss_ce + loss_kd + loss_att + loss_proto
         ###############################################
