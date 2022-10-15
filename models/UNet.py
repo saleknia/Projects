df6d0b11597c0a053d57315e74055428249b9217
saleknia <salekniaamirhossein@gmail.com>
2022-10-15 17:13:55 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -892,68 +892,6 @@ def deit_base_distilled_patch16_384(pretrained=False, **kwargs):
     return model
 
 
-
-
-
-
-
-
-
-
-class FAMBlock(nn.Module):
-    def __init__(self, channels):
-        super(FAMBlock, self).__init__()
-
-        self.conv3 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
-        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=1)
-
-        self.relu3 = nn.ReLU(inplace=True)
-        self.relu1 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x3 = self.conv3(x)
-        x3 = self.relu3(x3)
-        x1 = self.conv1(x)
-        x1 = self.relu1(x1)
-        out = x3 + x1
-
-        return out
-
-
-class DecoderBottleneckLayer(nn.Module):
-    def __init__(self, in_channels, n_filters, use_transpose=True):
-        super(DecoderBottleneckLayer, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
-        self.norm1 = nn.BatchNorm2d(in_channels // 4)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        if use_transpose:
-            self.up = nn.Sequential(
-                nn.ConvTranspose2d(
-                    in_channels // 4, in_channels // 4, 3, stride=2, padding=1, output_padding=1
-                ),
-                nn.BatchNorm2d(in_channels // 4),
-                nn.ReLU(inplace=True)
-            )
-        else:
-            self.up = nn.Upsample(scale_factor=2, align_corners=True, mode="bilinear")
-
-        self.conv3 = nn.Conv2d(in_channels // 4, n_filters, 1)
-        self.norm3 = nn.BatchNorm2d(n_filters)
-        self.relu3 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-        x = self.up(x)
-        x = self.conv3(x)
-        x = self.norm3(x)
-        x = self.relu3(x)
-        return x
-
-
 class SEBlock(nn.Module):
     def __init__(self, channel, r=16):
         super(SEBlock, self).__init__()
@@ -1000,26 +938,8 @@ class UNet(nn.Module):
         self.conv_seq_img = nn.Conv2d(in_channels=192, out_channels=512, kernel_size=1, padding=0)
         self.se = SEBlock(channel=1024)
         self.conv2d = nn.Conv2d(in_channels=1024, out_channels=512, kernel_size=1, padding=0)
-
-        self.FAMBlock1 = FAMBlock(channels=64)
-        self.FAMBlock2 = FAMBlock(channels=128)
-        self.FAMBlock3 = FAMBlock(channels=256)
-        self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(6)])
-        self.FAM2 = nn.ModuleList([self.FAMBlock2 for i in range(4)])
-        self.FAM3 = nn.ModuleList([self.FAMBlock3 for i in range(2)])
-
-        filters = [64, 128, 256, 512]
-        self.decoder4 = DecoderBottleneckLayer(filters[3], filters[2])
-        self.decoder3 = DecoderBottleneckLayer(filters[2], filters[1])
-        self.decoder2 = DecoderBottleneckLayer(filters[1], filters[0])
-        self.decoder1 = DecoderBottleneckLayer(filters[0], filters[0])
-
-        self.final_conv1 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
-        self.final_relu1 = nn.ReLU(inplace=True)
-        self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.final_relu2 = nn.ReLU(inplace=True)
-        self.final_conv3 = nn.Conv2d(32, n_classes, 3, padding=1)
-
+        self.avgpool = resnet.avgpool
+        self.fc = nn.Linear(in_features=512, out_features=40)
 
     def forward(self, x):
         b, c, h, w = x.shape
@@ -1043,24 +963,10 @@ class UNet(nn.Module):
         feature_cat = torch.cat((feature_cnn, feature_tf), dim=1)
         feature_att = self.se(feature_cat)
         feature_out = self.conv2d(feature_att)
-
-        for i in range(2):
-            e3 = self.FAM3[i](e3)
-        for i in range(4):
-            e2 = self.FAM2[i](e2)
-        for i in range(6):
-            e1 = self.FAM1[i](e1)
-        d4 = self.decoder4(feature_out) + e3
-        d3 = self.decoder3(d4) + e2
-        d2 = self.decoder2(d3) + e1
-
-        out1 = self.final_conv1(d2)
-        out1 = self.final_relu1(out1)
-        out = self.final_conv2(out1)
-        out = self.final_relu2(out)
-        out = self.final_conv3(out)
-
-        return out
+        x = self.avgpool(feature_out)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
 
 
 
