eedcd0f80b3ad9af20eb2c8951918903f6b867f7
saleknia <salekniaamirhossein@gmail.com>
2022-10-18 16:18:48 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -117,10 +117,10 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
 
 
         loss_ce = ce_loss(outputs, targets[:].long()) 
-        loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
+        # loss_dice = dice_loss(inputs=outputs, target=targets, softmax=True)
 
         # loss_ce = 0.0
-        # loss_dice = 0.0
+        loss_dice = 0.0
         # for output in outputs:
         #     loss_ce = loss_ce + ce_loss(output, targets[:].long()) 
         #     loss_dice = loss_dice + dice_loss(inputs=output, target=targets, softmax=True)
@@ -128,7 +128,8 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
         # outputs = outputs[0]
 
 
-        loss = 0.5 * loss_dice + 0.5 * loss_ce + loss_kd + loss_att
+        loss = loss_ce
+        # loss = 0.5 * loss_dice + 0.5 * loss_ce + loss_kd + loss_att
  
         lr_ = 0.001 * (1.0 - iter_num / max_iterations) ** 0.9
 
