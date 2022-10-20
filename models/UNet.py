ad4ff4001de8bca1380133196bc35d579c7c0a29
saleknia <salekniaamirhossein@gmail.com>
2022-10-20 09:01:50 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -4,7 +4,85 @@ import torchvision
 import torch.nn.functional as F
 from torchvision import models as resnet_model
 import math 
+from einops import rearrange
 
+class PreNorm(nn.Module):
+    def __init__(self,dim,fn):
+        super().__init__()
+        self.ln=nn.LayerNorm(dim)
+        self.fn=fn
+    def forward(self,x,**kwargs):
+        return self.fn(self.ln(x),**kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self,dim,mlp_dim,dropout) :
+        super().__init__()
+        self.net=nn.Sequential(
+            nn.Linear(dim,mlp_dim),
+            nn.SiLU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_dim,dim),
+            nn.Dropout(dropout)
+        )
+    def forward(self,x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self,dim,heads,head_dim,dropout):
+        super().__init__()
+        inner_dim=heads*head_dim
+        project_out=not(heads==1 and head_dim==dim)
+
+        self.heads=heads
+        self.scale=head_dim**-0.5
+
+        self.attend=nn.Softmax(dim=-1)
+        self.to_qkv=nn.Linear(dim,inner_dim*3,bias=False)
+        
+        self.to_out=nn.Sequential(
+            nn.Linear(inner_dim,dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self,x):
+        qkv=self.to_qkv(x).chunk(3,dim=-1)
+        q,k,v=map(lambda t:rearrange(t,'b p n (h d) -> b p h n d',h=self.heads),qkv)
+        dots=torch.matmul(q,k.transpose(-1,-2))*self.scale
+        attn=self.attend(dots)
+        out=torch.matmul(attn,v)
+        out=rearrange(out,'b p h n d -> b p n (h d)')
+        return self.to_out(out)
+
+class Transformer(nn.Module):
+    def __init__(self,dim,depth,heads,head_dim,mlp_dim,dropout=0.):
+        super().__init__()
+        self.layers=nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim,Attention(dim,heads,head_dim,dropout)),
+                PreNorm(dim,FeedForward(dim,mlp_dim,dropout))
+            ]))
+
+
+    def forward(self,x):
+        out=x
+        for att,ffn in self.layers:
+            out=out+att(out)
+            out=out+ffn(out)
+        return out
+
+class MobileViTAttention(nn.Module):
+    def __init__(self,in_channel=512,kernel_size=3,patch_size=1):
+        super().__init__()
+        self.ph,self.pw=patch_size,patch_size
+        self.trans=Transformer(dim=in_channel,depth=4,heads=8,head_dim=64,mlp_dim=1024)
+    def forward(self,x):
+        y=x.clone() #bs,c,h,w
+        _,_,h,w=y.shape
+        y=rearrange(y,'bs dim (nh ph) (nw pw) -> bs (ph pw) (nh nw) dim',ph=self.ph,pw=self.pw) #bs,h,w,dim
+        y=self.trans(y)
+        y=rearrange(y,'bs (ph pw) (nh nw) dim -> bs dim (nh ph) (nw pw)',ph=self.ph,pw=self.pw,nh=h//self.ph,nw=w//self.pw) #bs,dim,h,w
+        return y
 
 class CBR(nn.Module):
     '''
@@ -285,7 +363,7 @@ class UNet(nn.Module):
         self.esp4 = DilatedParllelResidualBlockB(nIn=in_channels*4, nOut=in_channels*4)
         self.esp3 = DilatedParllelResidualBlockB(nIn=in_channels*2, nOut=in_channels*2)
         self.esp2 = DilatedParllelResidualBlockB(nIn=in_channels, nOut=in_channels)
-        
+        self.transformer = MobileViTAttention()
         self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
 
         if n_classes == 1:
@@ -302,7 +380,7 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
-
+        x5 = self.transformer(x5) + x5
         x = self.up4(x5, x4)
         x = self.esp4(x)
 
