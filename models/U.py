cdba5606eac3950f2331db5faf6b10c2e9846420
saleknia <salekniaamirhossein@gmail.com>
2022-10-22 14:12:09 +0000

    Commit Process

--- models/U.py
+++ models/U.py
@@ -3,38 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 
-
-# class depthwise_separable_conv(nn.Module):
-#  def __init__(self, nin, nout): 
-#    super(depthwise_separable_conv, self).__init__() 
-#    self.depthwise = nn.Conv2d(nin, nin , kernel_size=3, padding=1, groups=nin) 
-#    self.pointwise = nn.Conv2d(nin, nout, kernel_size=1) 
-  
-#  def forward(self, x): 
-#    out = self.depthwise(x) 
-#    out = self.pointwise(out) 
-#    return out
-
-# class DoubleConv(nn.Module):
-#     """(convolution => [BN] => ReLU) * 2"""
-
-#     def __init__(self, in_channels, out_channels, mid_channels=None):
-#         super().__init__()
-#         if not mid_channels:
-#             mid_channels = out_channels
-#         self.double_conv = nn.Sequential(
-#             depthwise_separable_conv(in_channels, mid_channels),
-#             nn.BatchNorm2d(mid_channels),
-#             nn.ReLU(inplace=True),
-#             depthwise_separable_conv(mid_channels, out_channels),
-#             nn.BatchNorm2d(out_channels),
-#             nn.ReLU(inplace=True)
-#         )
-
-#     def forward(self, x):
-#         x = self.double_conv(x)
-#         return x
-
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -110,7 +78,7 @@ class U(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
-        in_channels = 64
+        in_channels = 16
         self.inc = DoubleConv(n_channels, in_channels)
         self.down1 = Down(in_channels  , in_channels*2)
         self.down2 = Down(in_channels*2, in_channels*4)
@@ -136,6 +104,6 @@ class U(nn.Module):
         logits = self.outc(up4)
 
         if multiple:
-            return logits, up1, up2, up3, up4, x1, x2, x3, x4, x5
+            return logits, up1, up2, up3, up4
         else:
             return logits
--- trainer_c.py
+++ trainer_c.py
@@ -17,6 +17,26 @@ from utils import importance_maps_distillation as imd
 import os
 warnings.filterwarnings("ignore")
 
+def loss_kd_regularization(outputs, labels):
+    """
+    loss function for mannually-designed regularization: Tf-KD_{reg}
+    """
+    alpha = params.reg_alpha
+    T = params.reg_temperature
+    correct_prob = 0.99    # the probability for correct class in u(k)
+    loss_CE = F.cross_entropy(outputs, labels)
+    K = outputs.size(1)
+
+    teacher_soft = torch.ones_like(outputs).cuda()
+    teacher_soft = teacher_soft*(1-correct_prob)/(K-1)  # p^d(k)
+    for i in range(outputs.shape[0]):
+        teacher_soft[i ,labels[i]] = correct_prob
+    loss_soft_regu = nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_soft/T, dim=1))*params.multiplier
+
+    KD_loss = (1. - alpha)*loss_CE + alpha*loss_soft_regu
+
+    return KD_loss
+
 def disparity(labels, outputs):
     B, N = outputs.shape
     unique = torch.unique(labels)
