c49c7183f41dc21e1448a6fe79a15795200b0b63
saleknia <salekniaamirhossein@gmail.com>
2022-10-22 14:16:17 +0000

    Commit Process

--- trainer.py
+++ trainer.py
@@ -178,7 +178,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         loss = alpha * loss_dice + beta * loss_ce + loss_kd + loss_att + loss_proto + loss_ct
         ###############################################
 
-        lr_ = 0.001 * (1.0 - iter_num / max_iterations) ** 0.9
+        lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9
 
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr_
