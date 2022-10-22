b44fb999b616cc3dcf7cb55653a00a53a28f2c58
saleknia <salekniaamirhossein@gmail.com>
2022-10-22 16:16:51 +0000

    Commit Process

--- models/U.py
+++ models/U.py
@@ -78,7 +78,7 @@ class U(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
-        in_channels = 20
+        in_channels = 24
         self.inc = DoubleConv(n_channels, in_channels)
         self.down1 = Down(in_channels  , in_channels*2)
         self.down2 = Down(in_channels*2, in_channels*4)
