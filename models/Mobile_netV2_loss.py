62b901912df8c4a9cd125753ac2fa0f415dd128a
saleknia <salekniaamirhossein@gmail.com>
2022-10-27 10:20:29 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -1,44 +1,80 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet18, resnet50
+from torchvision.models import resnet18, resnet50, efficientnet_b0, EfficientNet_B0_Weights
 import torchvision
+
+
+class enet(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(enet, self).__init__()
+
+        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        
+        self.features = model.features
+        self.avgpool = model.avgpool
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=1280, out_features=512, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=256, out_features=40, bias=True),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
 
 
 class Mobile_netV2_loss(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2_loss, self).__init__()
-        model = resnet18(pretrained)
-        # model = resnet50(pretrained)
+        model_a = enet()
+        loaded_data_a = torch.load('/content/drive/MyDrive/checkpoint_a/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_a = loaded_data_a['net']
+        model_a.load_state_dict(pretrained_a)
 
-        # take pretrained resnet, except AvgPool and FC
-        self.conv1 = model.conv1
-        self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
-        self.layer1 = model.layer1
-        self.layer2 = model.layer2
-        self.layer3 = model.layer3
-        self.layer4 = model.layer4
-        self.avgpool = model.avgpool
-        self.fc = nn.Linear(in_features=512, out_features=40)
-        # self.fc = nn.Linear(in_features=2048, out_features=40)
+        for param in model_a.parameters():
+            param.requires_grad = False
+
+        model_b = enet()
+        loaded_data_b = torch.load('/content/drive/MyDrive/checkpoint_b/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b = loaded_data_b['net']
+        model_b.load_state_dict(pretrained_b)
+
+        for param in model_b.parameters():
+            param.requires_grad = False
+
+        self.features_a = model_a.features
+        self.features_b = model_b.features
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=2560, out_features=512, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=256, out_features=40, bias=True),
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
+        x_a = self.features_a(x)
+        x_b = self.features_b(x)
+        x = torch.cat([x_a, x_b], dim=1)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        y = x
-        x = self.fc(x)
-        if self.training:
-            return x, y
-        else:
-            return x
+        x = self.classifier(x)
+        return x
+
+
+
+
+
+
+
+
