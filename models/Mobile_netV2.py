54d8c5bcf5d4387486ab32e2e1f64449c9a505d2
saleknia <salekniaamirhossein@gmail.com>
2022-10-29 17:35:50 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -4,159 +4,291 @@ import torch.nn.functional as F
 from torchvision.models import resnet18, resnet50, efficientnet_b0, EfficientNet_B0_Weights, efficientnet_b1, EfficientNet_B1_Weights
 import torchvision
 
-def get_activation(activation_type):
-    activation_type = activation_type.lower()
-    if hasattr(nn, activation_type):
-        return getattr(nn, activation_type)()
-    else:
-        return nn.ReLU()
 
 
-class ConvBatchNorm(nn.Module):
-    """(convolution => [BN] => ReLU)"""
 
-    def __init__(self, in_channels, out_channels, activation='ReLU', esp=True):
-        super(ConvBatchNorm, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        self.norm = nn.BatchNorm2d(out_channels)
-        self.activation = get_activation(activation)
-
-    def forward(self, x):
-        out = self.conv(x)
-        out = self.norm(out)
-        out = self.activation(out)
-        return out
-
-class SEBlock(nn.Module):
-    def __init__(self, channel, r=16):
-        super(SEBlock, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // r, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // r, channel, bias=False),
-            nn.Sigmoid(),
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        # Squeeze
-        y = self.avg_pool(x).view(b, c)
-        # Excitation
-        y = self.fc(y).view(b, c, 1, 1)
-        # Fusion
-        y = torch.mul(x, y)
-        return y
-
-class Mobile_netV2(nn.Module):
-    def __init__(self, num_classes=40, pretrained=True):
-        super(Mobile_netV2, self).__init__()
-
-        model_a = efficientnet_b0(weights=EfficientNet_B0_Weights)
-        for param in model_a.parameters():
-            param.requires_grad = False
-        self.features_a = model_a.features
-
-        model_b = efficientnet_b0(weights=EfficientNet_B0_Weights)
-
-        self.features_b = model_b.features
-        self.avgpool = model_b.avgpool
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=1280, out_features=512, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=512, out_features=256, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=256, out_features=40, bias=True),
-        )
-
-        # model = resnet50(pretrained)
-        # model = resnet18(pretrained)
-
-        # take pretrained resnet, except AvgPool and FC
-        # self.conv1 = model.conv1
-        # self.bn1 = model.bn1
-        # self.relu = model.relu
-        # self.maxpool = model.maxpool
-        # self.layer1 = model.layer1
-        # self.layer2 = model.layer2
-        # self.layer3 = model.layer3
-        # self.layer4 = model.layer4
-
-        # self.avgpool = model.avgpool
-        # self.classifier = nn.Sequential(
-        #     nn.Dropout(p=0.4, inplace=True),
-        #     nn.Linear(in_features=2048, out_features=512, bias=True),
-        #     nn.Dropout(p=0.4, inplace=True),
-        #     nn.Linear(in_features=512, out_features=256, bias=True),
-        #     nn.Dropout(p=0.4, inplace=True),
-        #     nn.Linear(in_features=256, out_features=40, bias=True),
-        # )
-        # self.SE_2 = SEBlock(channel=512 )
-        # self.SE_3 = SEBlock(channel=1024)
-        # self.SE_4 = SEBlock(channel=2048)
-    def forward(self, x):
-        # x = self.conv1(x)
-        # x = self.bn1(x)
-        # x = self.relu(x)
-        # x = self.maxpool(x)
-
-        # x = self.layer1(x)
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-        # x = self.avgpool(x)
-        # x = x.view(x.size(0), -1)
-        # x = self.classifier(x)
-        features_b = self.features_b(x)
-        x = self.avgpool(features_b)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
 
 # class Mobile_netV2(nn.Module):
 #     def __init__(self, num_classes=40, pretrained=True):
 #         super(Mobile_netV2, self).__init__()
 
-#         # take pretrained resnet, except AvgPool and FC
-        
-#         # model = resnet18(pretrained)
-#         model = resnet50(pretrained)
+#         model_a = efficientnet_b0(weights=EfficientNet_B0_Weights)
+#         for param in model_a.parameters():
+#             param.requires_grad = False
+#         self.features_a = model_a.features
 
-#         self.conv1 = model.conv1
-#         self.bn1 = model.bn1
-#         self.relu = model.relu
-#         self.maxpool = model.maxpool
-#         self.layer1 = model.layer1
-#         self.layer2 = model.layer2
-#         self.layer3 = model.layer3
-#         self.layer4 = model.layer4
-#         self.avgpool = model.avgpool
+#         model_b = efficientnet_b0(weights=EfficientNet_B0_Weights)
 
-#         self.avgpool = model.avgpool
+#         self.features_b = model_b.features
+#         self.avgpool = model_b.avgpool
 #         self.classifier = nn.Sequential(
 #             nn.Dropout(p=0.4, inplace=True),
-#             nn.Linear(in_features=2048, out_features=512, bias=True),
+#             nn.Linear(in_features=1280, out_features=512, bias=True),
 #             nn.Dropout(p=0.4, inplace=True),
 #             nn.Linear(in_features=512, out_features=256, bias=True),
 #             nn.Dropout(p=0.4, inplace=True),
 #             nn.Linear(in_features=256, out_features=40, bias=True),
 #         )
 
+#         # model = resnet50(pretrained)
+#         # model = resnet18(pretrained)
 
+#         # take pretrained resnet, except AvgPool and FC
+#         # self.conv1 = model.conv1
+#         # self.bn1 = model.bn1
+#         # self.relu = model.relu
+#         # self.maxpool = model.maxpool
+#         # self.layer1 = model.layer1
+#         # self.layer2 = model.layer2
+#         # self.layer3 = model.layer3
+#         # self.layer4 = model.layer4
+
+#         # self.avgpool = model.avgpool
+#         # self.classifier = nn.Sequential(
+#         #     nn.Dropout(p=0.4, inplace=True),
+#         #     nn.Linear(in_features=2048, out_features=512, bias=True),
+#         #     nn.Dropout(p=0.4, inplace=True),
+#         #     nn.Linear(in_features=512, out_features=256, bias=True),
+#         #     nn.Dropout(p=0.4, inplace=True),
+#         #     nn.Linear(in_features=256, out_features=40, bias=True),
+#         # )
+#         # self.SE_2 = SEBlock(channel=512 )
+#         # self.SE_3 = SEBlock(channel=1024)
+#         # self.SE_4 = SEBlock(channel=2048)
 #     def forward(self, x):
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
+#         # x = self.conv1(x)
+#         # x = self.bn1(x)
+#         # x = self.relu(x)
+#         # x = self.maxpool(x)
 
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
-#         x = self.layer4(x)
-#         x = self.avgpool(x)
+#         # x = self.layer1(x)
+#         # x = self.layer2(x)
+#         # x = self.layer3(x)
+#         # x = self.layer4(x)
+#         # x = self.avgpool(x)
+#         # x = x.view(x.size(0), -1)
+#         # x = self.classifier(x)
+#         features_b = self.features_b(x)
+#         x = self.avgpool(features_b)
 #         x = x.view(x.size(0), -1)
 #         x = self.classifier(x)
 #         return x
+
+class Mobile_netV2(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2, self).__init__()
+
+        transformer = deit_tiny_distilled_patch16_224(pretrained=True)
+
+        self.patch_embed = transformer.patch_embed
+        self.transformers = nn.ModuleList(
+            [transformer.blocks[i] for i in range(12)]
+        )        
+
+        self.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=192, out_features=512, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=256, out_features=40, bias=True),
+        )
+
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        emb = self.patch_embed(x)
+        for i in range(12):
+            emb = self.transformers[i](emb)
+        feature_tf = emb.permute(0, 2, 1)
+        feature_tf = feature_tf.view(b, 192, 14, 14)
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+# Copyright (c) 2015-present, Facebook, Inc.
+# All rights reserved.
+import torch
+import torch.nn as nn
+from functools import partial
+import os
+os.system('pip install timm')
+
+from timm.models.vision_transformer import VisionTransformer, _cfg
+from timm.models.registry import register_model
+from timm.models.layers import trunc_normal_
+
+
+__all__ = [
+    'deit_tiny_patch16_224', 'deit_small_patch16_224', 'deit_base_patch16_224',
+    'deit_tiny_distilled_patch16_224', 'deit_small_distilled_patch16_224',
+    'deit_base_distilled_patch16_224', 'deit_base_patch16_384',
+    'deit_base_distilled_patch16_384',
+]
+
+
+class DistilledVisionTransformer(VisionTransformer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dist_token = nn.Parameter(torch.zeros(1, 1, self.embed_dim))
+        num_patches = self.patch_embed.num_patches
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 2, self.embed_dim))
+        self.head_dist = nn.Linear(self.embed_dim, self.num_classes) if self.num_classes > 0 else nn.Identity()
+
+        trunc_normal_(self.dist_token, std=.02)
+        trunc_normal_(self.pos_embed, std=.02)
+        self.head_dist.apply(self._init_weights)
+
+    def forward_features(self, x):
+        # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+        # with slight modifications to add the dist_token
+        B = x.shape[0]
+        x = self.patch_embed(x)
+
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        dist_token = self.dist_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, dist_token, x), dim=1)
+
+        x = x + self.pos_embed
+        x = self.pos_drop(x)
+
+        for blk in self.blocks:
+            x = blk(x)
+
+        x = self.norm(x)
+        return x[:, 0], x[:, 1]
+
+    def forward(self, x):
+        x, x_dist = self.forward_features(x)
+        x = self.head(x)
+        x_dist = self.head_dist(x_dist)
+        if self.training:
+            return x, x_dist
+        else:
+            # during inference, return the average of both classifier predictions
+            return (x + x_dist) / 2
+
+
+@register_model
+def deit_tiny_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_small_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_base_patch16_224(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_tiny_distilled_patch16_224(pretrained=False, **kwargs):
+    model = DistilledVisionTransformer(
+        patch_size=16, embed_dim=192, depth=12, num_heads=3, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_tiny_distilled_patch16_224-b40b3cf7.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_small_distilled_patch16_224(pretrained=False, **kwargs):
+    model = DistilledVisionTransformer(
+        patch_size=16, embed_dim=384, depth=12, num_heads=6, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_small_distilled_patch16_224-649709d9.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_base_distilled_patch16_224(pretrained=False, **kwargs):
+    model = DistilledVisionTransformer(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_224-df68dfff.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_base_patch16_384(pretrained=False, **kwargs):
+    model = VisionTransformer(
+        img_size=384, patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_patch16_384-8de9b5d1.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
+
+
+@register_model
+def deit_base_distilled_patch16_384(pretrained=False, **kwargs):
+    model = DistilledVisionTransformer(
+        img_size=384, patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = _cfg()
+    if pretrained:
+        checkpoint = torch.hub.load_state_dict_from_url(
+            url="https://dl.fbaipublicfiles.com/deit/deit_base_distilled_patch16_384-d0272ac0.pth",
+            map_location="cpu", check_hash=True
+        )
+        model.load_state_dict(checkpoint["model"])
+    return model
 
 
