135c6b6460802415752bef814334a0ddd5517adf
saleknia <salekniaamirhossein@gmail.com>
2022-10-31 08:47:42 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -330,7 +330,8 @@ def main(args):
     elif TASK_NAME=='Standford40':
 
         transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            # transforms.RandomResizedCrop(224),
+            transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([
             transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8
