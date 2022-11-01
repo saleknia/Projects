ec889513ea2d63f273050e82a5ac3af0aa0824ff
saleknia <salekniaamirhossein@gmail.com>
2022-11-01 11:30:01 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -332,10 +332,9 @@ def main(args):
         transform_train = transforms.Compose([
             transforms.RandomResizedCrop(size=224),
             transforms.RandomHorizontalFlip(),
-            # transforms.RandomApply([
-            # transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8
-            # ),
-            # transforms.RandomGrayscale(0.2),
+            transforms.RandomApply([
+            transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
