9b07f14d50425aae745f16042410ef198cb61168
saleknia <salekniaamirhossein@gmail.com>
2022-11-01 17:51:44 +0000

    Commit Process

--- config.py
+++ config.py
@@ -42,7 +42,7 @@ if log:
 ##########################################################################
 
 # Hyperparameters etc.
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.001
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 40
 NUM_EPOCHS = 60
