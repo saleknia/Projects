148b6d2f4e14a34fceedd88c214826fbf5c05d34
saleknia <salekniaamirhossein@gmail.com>
2022-11-03 21:24:41 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -110,6 +110,14 @@ class Mobile_netV2_loss(nn.Module):
         for param in model_a.parameters():
             param.requires_grad = False
 
+        model_b = enet()
+        loaded_data_b = torch.load('/content/drive/MyDrive/checkpoint_smc05/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b = loaded_data_b['net']
+        model_b.load_state_dict(pretrained_b)
+
+        for param in model_b.parameters():
+            param.requires_grad = False
+
         # model_b = enet()
         # loaded_data_b = torch.load('/content/drive/MyDrive/checkpoint_sm04/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         # pretrained_b = loaded_data_b['net']
@@ -127,7 +135,7 @@ class Mobile_netV2_loss(nn.Module):
         #     param.requires_grad = False
 
         self.model_a = model_a
-        # self.model_b = model_b
+        self.model_b = model_b
         # self.model_c = model_c
 
     def forward(self, x):
@@ -145,8 +153,8 @@ class Mobile_netV2_loss(nn.Module):
         # x = torch.cat([x_a, x_b], dim=1)
         # x = self.classifier(x)
 
-        # x = (self.model_a(x) + self.model_b(x) + self.model_c(x)) / 3.0
-        x = self.model_a(x)
+        x = (self.model_a(x) + self.model_b(x)) / 2.0
+        # x = self.model_a(x)
 
         return x
 
