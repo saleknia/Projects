41450ffebdaaf634e283b60b4e26dfef90841855
saleknia <salekniaamirhossein@gmail.com>
2022-11-04 07:49:14 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -103,20 +103,20 @@ class Mobile_netV2_loss(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2_loss, self).__init__()
         model_a = enet()
-        loaded_data_a = torch.load('/content/drive/MyDrive/checkpoint_sm04/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_a = torch.load('/content/drive/MyDrive/checkpoint_sm05/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_a = loaded_data_a['net']
         model_a.load_state_dict(pretrained_a)
 
         for param in model_a.parameters():
             param.requires_grad = False
 
-        model_b = enet()
-        loaded_data_b = torch.load('/content/drive/MyDrive/checkpoint/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
-        pretrained_b = loaded_data_b['net']
-        model_b.load_state_dict(pretrained_b)
+        # model_b = enet()
+        # loaded_data_b = torch.load('/content/drive/MyDrive/checkpoint/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        # pretrained_b = loaded_data_b['net']
+        # model_b.load_state_dict(pretrained_b)
 
-        for param in model_b.parameters():
-            param.requires_grad = False
+        # for param in model_b.parameters():
+        #     param.requires_grad = False
 
         # model_c = enet()
         # loaded_data_c = torch.load('/content/drive/MyDrive/checkpoint_sm03/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
@@ -127,7 +127,7 @@ class Mobile_netV2_loss(nn.Module):
         #     param.requires_grad = False
 
         self.model_a = model_a
-        self.model_b = model_b
+        # self.model_b = model_b
         # self.model_c = model_c
 
     def forward(self, x):
@@ -145,8 +145,8 @@ class Mobile_netV2_loss(nn.Module):
         # x = torch.cat([x_a, x_b], dim=1)
         # x = self.classifier(x)
 
-        x = (self.model_a(x) + self.model_b(x)) / 2.0
-        # x = self.model_a(x)
+        # x = (self.model_a(x) + self.model_b(x)) / 2.0
+        x = self.model_a(x)
 
         return x
 
--- trainer_c.py
+++ trainer_c.py
@@ -116,7 +116,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
             with torch.no_grad():
                 outputs_t = teacher_model(inputs)
                 weights = F.cross_entropy(outputs_t, targets.long(), reduce=False)
-                weights = 1.0 + (weights / weights.max())
+                weights = torch.nn.functional.softmax(weights)
                 weights = weights.detach()
 
         if teacher_model is not None:
