8b5278466a64f83a32c8c9cb70caf0f7df96b599
saleknia <salekniaamirhossein@gmail.com>
2022-11-05 13:17:42 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -1,10 +1,5 @@
-import torch
 import torch.nn as nn
-import torchvision
-import torch.nn.functional as F
-from torchvision import models as resnet_model
-import math 
-from einops import rearrange
+import torch
 
 def get_activation(activation_type):
     activation_type = activation_type.lower()
@@ -26,15 +21,15 @@ class ConvBatchNorm(nn.Module):
 
     def __init__(self, in_channels, out_channels, activation='ReLU'):
         super(ConvBatchNorm, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv = nn.Conv2d(in_channels, out_channels,
+                              kernel_size=3, padding=1)
         self.norm = nn.BatchNorm2d(out_channels)
         self.activation = get_activation(activation)
 
     def forward(self, x):
         out = self.conv(x)
         out = self.norm(out)
-        out = self.activation(out)
-        return out
+        return self.activation(out)
 
 class DownBlock(nn.Module):
     """Downscaling with maxpool convolution"""
@@ -48,7 +43,6 @@ class DownBlock(nn.Module):
         out = self.maxpool(x)
         return self.nConvs(out)
 
-
 class UpBlock(nn.Module):
     """Upscaling then conv"""
 
@@ -56,6 +50,7 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
 
         self.up = nn.Upsample(scale_factor=2)
+        # self.up = nn.ConvTranspose2d(in_channels//2,in_channels//2,(2,2),2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
 
     def forward(self, x, skip_x):
@@ -72,25 +67,20 @@ class UNet(nn.Module):
                       By default 3 (2 labels + 1 for the background)
         '''
         super().__init__()
-        self.n_classes = n_classes
         self.n_channels = n_channels
-
+        self.n_classes = n_classes
         # Question here
         in_channels = 64
         self.inc = ConvBatchNorm(n_channels, in_channels)
-        nb_Conv = 2
-        self.down1 = DownBlock(in_channels*1, in_channels*2, nb_Conv=nb_Conv)
-        self.down2 = DownBlock(in_channels*2, in_channels*4, nb_Conv=nb_Conv)
-        self.down3 = DownBlock(in_channels*4, in_channels*8, nb_Conv=nb_Conv)
-        self.down4 = DownBlock(in_channels*8, in_channels*8, nb_Conv=nb_Conv)
-
+        self.down1 = DownBlock(in_channels, in_channels*2, nb_Conv=2)
+        self.down2 = DownBlock(in_channels*2, in_channels*4, nb_Conv=2)
+        self.down3 = DownBlock(in_channels*4, in_channels*8, nb_Conv=2)
+        self.down4 = DownBlock(in_channels*8, in_channels*8, nb_Conv=2)
         self.up4 = UpBlock(in_channels*16, in_channels*4, nb_Conv=2)
         self.up3 = UpBlock(in_channels*8, in_channels*2, nb_Conv=2)
         self.up2 = UpBlock(in_channels*4, in_channels, nb_Conv=2)
         self.up1 = UpBlock(in_channels*2, in_channels, nb_Conv=2)
-
         self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
-
         if n_classes == 1:
             self.last_activation = nn.Sigmoid()
         else:
@@ -98,24 +88,23 @@ class UNet(nn.Module):
 
     def forward(self, x):
         # Question here
-
-        b, c, h, w = x.shape
-
-        x = x.float() 
+        x = x.float()
         x1 = self.inc(x)
-        x2 = self.down1(x1) 
-        x3 = self.down2(x2) 
-        x4 = self.down3(x3) 
-        x5 = self.down4(x4) 
-
-        up4 = self.up4(x5, x4) 
-        up3 = self.up3(up4, x3) 
-        up2 = self.up2(up3, x2) 
-        up1 = self.up1(up2, x1) 
-
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up4(x5, x4)
+        x = self.up3(x, x3)
+        x = self.up2(x, x2)
+        x = self.up1(x, x1)
         if self.last_activation is not None:
-            logits = self.last_activation(self.outc(up1))
+            logits = self.last_activation(self.outc(x))
+            # print("111")
         else:
-            logits = self.outc(up1)
-
+            logits = self.outc(x)
+            # print("222")
+        # logits = self.outc(x) # if using BCEWithLogitsLoss
+        # print(logits.size())
         return logits
+
