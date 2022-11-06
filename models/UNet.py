fe33c99071b370d97ff46a4474eed7f4e5d44571
saleknia <salekniaamirhossein@gmail.com>
2022-11-06 11:17:20 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -114,18 +114,13 @@ class UpBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, nb_Conv, channel, activation='ReLU', up=True):
         super(UpBlock, self).__init__()
-        self.up = up
-        if up:
-            self.up = nn.Upsample(scale_factor=2)
+        self.up = nn.Upsample(scale_factor=2)
         # self.up = nn.ConvTranspose2d(in_channels//2,in_channels//2,(2,2),2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
         self.att = ParallelPolarizedSelfAttention(channel=channel)
 
     def forward(self, x, skip_x):
-        if self.up:
-            out = self.up(x)
-        else:
-            out = x
+        out = self.up(x)
         out = self.att(x)
         x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
         return self.nConvs(x)
