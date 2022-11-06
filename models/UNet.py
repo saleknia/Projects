e63c4c1635c16a5d948e91571e91b347d7044fa7
saleknia <salekniaamirhossein@gmail.com>
2022-11-06 08:53:41 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -2,6 +2,51 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import models as resnet_model
+import numpy as np
+from torch.nn import init
+
+
+
+class ParallelPolarizedSelfAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*x
+        out=spatial_out+channel_out
+        return out
 
 class FAMBlock(nn.Module):
     def __init__(self, channels):
@@ -74,12 +119,14 @@ class UpBlock(nn.Module):
             self.up = nn.Upsample(scale_factor=2)
         # self.up = nn.ConvTranspose2d(in_channels//2,in_channels//2,(2,2),2)
         self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
+        self.att = ParallelPolarizedSelfAttention(channel=in_channels//2)
 
     def forward(self, x, skip_x):
         if self.up:
             out = self.up(x)
         else:
             out = x
+        out = self.att(out)
         x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
         return self.nConvs(x)
 
@@ -157,13 +204,13 @@ class UNet(nn.Module):
         # torch.Size([8, 512, 14, 14])
 
         x = self.up4(x5, x4)
-        x = self.esp4(x)
+        # x = self.esp4(x)
         x = self.up3(x, x3)
-        x = self.esp3(x)
+        # x = self.esp3(x)
         x = self.up2(x, x2)
-        x = self.esp2(x)
+        # x = self.esp2(x)
         x = self.up1(x, x1)
-        x = self.esp1(x)
+        # x = self.esp1(x)
         x = self.up(x)
 
 
