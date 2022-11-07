4fc57444bd330d88f06bcb0cd951d5e9a6cc9e61
saleknia <salekniaamirhossein@gmail.com>
2022-11-07 19:39:22 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -136,8 +136,8 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
 
     dice_loss = DiceLoss()
     # ce_loss = CrossEntropyLoss()
-    # ce_loss = torch.nn.BCEWithLogitsLoss()
-    ce_loss = torch.nn.BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
+    ce_loss = torch.nn.BCEWithLogitsLoss()
+    # ce_loss = torch.nn.BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
     # kd_loss = torch.nn.BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
 
     total_batchs = len(dataloader['train'])
@@ -187,8 +187,8 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
 
         targets = targets.long()
 
-        predictions = torch.round(torch.squeeze(outputs, dim=1))
-        # predictions = torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1)))
+        # predictions = torch.round(torch.squeeze(outputs, dim=1))
+        predictions = torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1)))
         Eval.add_batch(gt_image=targets,pre_image=predictions)
         # accuracy.update(Eval.Pixel_Accuracy())
 
