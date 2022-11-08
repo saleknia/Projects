9b9c8506d1b7c2653d0d63c47b2d32cd2f44d405
saleknia <salekniaamirhossein@gmail.com>
2022-11-08 12:25:11 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -5,7 +5,7 @@ from torchvision import models as resnet_model
 import numpy as np
 from torch.nn import init
 from collections import OrderedDict
-
+import torchvision
 
 
 class SKAttention(nn.Module):
@@ -175,6 +175,126 @@ class UpBlock(nn.Module):
         x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
         return self.nConvs(x)
 
+# class UNet(nn.Module):
+#     def __init__(self, n_channels=3, n_classes=1):
+#         '''
+#         n_channels : number of channels of the input.
+#                         By default 3, because we have RGB images
+#         n_labels : number of channels of the ouput.
+#                       By default 3 (2 labels + 1 for the background)
+#         '''
+#         super().__init__()
+#         self.n_channels = n_channels
+#         self.n_classes = n_classes
+
+#         # Question here
+
+#         in_channels = 64
+
+#         # self.inc = ConvBatchNorm(n_channels, in_channels)
+#         # self.down1 = DownBlock(in_channels, in_channels*2, nb_Conv=2)
+#         # self.down2 = DownBlock(in_channels*2, in_channels*4, nb_Conv=2)
+#         # self.down3 = DownBlock(in_channels*4, in_channels*8, nb_Conv=2)
+#         # self.down4 = DownBlock(in_channels*8, in_channels*8, nb_Conv=2)
+#         # self.up4 = UpBlock(in_channels*16, in_channels*4, nb_Conv=2)
+#         # self.up3 = UpBlock(in_channels*8, in_channels*2, nb_Conv=2)
+#         # self.up2 = UpBlock(in_channels*4, in_channels, nb_Conv=2)
+#         # self.up1 = UpBlock(in_channels*2, in_channels, nb_Conv=2)
+#         # self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
+
+#         resnet = resnet_model.resnet34(pretrained=True)
+#         # for param in resnet.parameters():
+#         #     param.requires_grad = False
+#         resnet.conv1.stride = 1
+
+#         self.inc = nn.Sequential(
+#             resnet.conv1,
+#             resnet.bn1,
+#             resnet.relu
+#         )
+
+#         self.maxpool = resnet.maxpool
+#         self.down1 = resnet.layer1 # 64
+#         self.down2 = resnet.layer2 # 128
+#         self.down3 = resnet.layer3 # 256
+#         self.down4 = resnet.layer4 # 512
+
+#         self.up4 = UpBlock(in_channels*12, in_channels*2, nb_Conv=2, channel=512, up=True)
+#         self.up3 = UpBlock(in_channels*4 , in_channels*1, nb_Conv=2, channel=128, up=True)
+#         self.up2 = UpBlock(in_channels*2 , in_channels*1, nb_Conv=2, channel=64 , up=True)
+#         self.up1 = UpBlock(in_channels*2 , in_channels*1, nb_Conv=2, channel=64 , up=True)
+#         # self.up = nn.Upsample(scale_factor=2) 
+#         self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
+#         # transformer = deit_tiny_distilled_patch16_224(pretrained=True)
+#         # self.patch_embed = transformer.patch_embed
+#         # self.transformers = nn.ModuleList(
+#         #     [transformer.blocks[i] for i in range(12)]
+#         # )
+
+#         # self.psi = nn.Sequential(
+#         #     nn.Conv2d(192, 1, kernel_size=1, stride=1, padding=0, bias=True),
+#         #     nn.BatchNorm2d(1),
+#         #     nn.Sigmoid()
+#         # )
+#         # self.ups2  = nn.Upsample(scale_factor=2)
+#         # self.ups4  = nn.Upsample(scale_factor=4)
+#         # self.ups8  = nn.Upsample(scale_factor=8)  
+#         # self.ups16 = nn.Upsample(scale_factor=16)  
+
+#         # self.esp4 = DilatedParllelResidualBlockB(nIn=in_channels*2, nOut=in_channels*2)
+#         # self.esp3 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
+#         # self.esp2 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
+#         # self.esp1 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
+
+#         if n_classes == 0:
+#             self.last_activation = nn.Sigmoid()
+#         else:
+#             self.last_activation = None
+
+#     def forward(self, x):
+#         # Question here
+#         b, c, h, w = x.shape
+#         x = x.float()
+#         x1 = self.inc(x) # 112
+#         x2 = self.down1(self.maxpool(x1)) # 112
+#         x3 = self.down2(x2) # 56
+#         x4 = self.down3(x3) # 28
+#         x5 = self.down4(x4) # 14
+#         # torch.Size([8, 64, 112, 112])
+#         # torch.Size([8, 64, 112, 112])
+#         # torch.Size([8, 128, 56, 56])
+#         # torch.Size([8, 256, 28, 28])
+#         # torch.Size([8, 512, 14, 14])
+
+#         # emb = self.patch_embed(x)
+#         # for i in range(12):
+#         #     emb = self.transformers[i](emb)
+#         # feature_tf = emb.permute(0, 2, 1)
+#         # feature_tf = feature_tf.view(b, 192, 14, 14)
+#         # psi = self.psi(feature_tf)
+
+#         # x1 = x1 * self.ups16(psi)
+#         # x2 = x2 * self.ups8(psi)
+#         # x3 = x3 * self.ups4(psi)
+#         # x4 = x4 * self.ups2(psi)
+
+#         x = self.up4(x5, x4)
+#         x = self.up3(x , x3)
+#         x = self.up2(x , x2)
+#         x = self.up1(x , x1)
+
+
+
+#         if self.last_activation is not None:
+#             logits = self.last_activation(self.outc(x))
+#             # print("111")
+#         else:
+#             logits = self.outc(x)
+#             # print("222")
+#         # logits = self.outc(x) # if using BCEWithLogitsLoss
+#         # print(logits.size())
+#         return logits
+
 class UNet(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         '''
@@ -187,113 +307,43 @@ class UNet(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
 
-        # Question here
+        model = torchvision.models.efficientnet_b0(weights=torchvision.models.EfficientNet_B0_Weights)
+        features = model.features
+        features[0][0].stride = (1,1)
+        self.layer1 = features[0:2]
+        self.layer2 = features[2:3]
+        self.layer3 = features[3:4]
+        self.layer4 = features[4:6]
+        self.layer5 = features[6:8]
 
-        in_channels = 64
 
-        # self.inc = ConvBatchNorm(n_channels, in_channels)
-        # self.down1 = DownBlock(in_channels, in_channels*2, nb_Conv=2)
-        # self.down2 = DownBlock(in_channels*2, in_channels*4, nb_Conv=2)
-        # self.down3 = DownBlock(in_channels*4, in_channels*8, nb_Conv=2)
-        # self.down4 = DownBlock(in_channels*8, in_channels*8, nb_Conv=2)
-        # self.up4 = UpBlock(in_channels*16, in_channels*4, nb_Conv=2)
-        # self.up3 = UpBlock(in_channels*8, in_channels*2, nb_Conv=2)
-        # self.up2 = UpBlock(in_channels*4, in_channels, nb_Conv=2)
-        # self.up1 = UpBlock(in_channels*2, in_channels, nb_Conv=2)
-        # self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
-
-        resnet = resnet_model.resnet34(pretrained=True)
-        # for param in resnet.parameters():
-        #     param.requires_grad = False
-        resnet.conv1.stride = 1
-
-        self.inc = nn.Sequential(
-            resnet.conv1,
-            resnet.bn1,
-            resnet.relu
-        )
-
-        self.maxpool = resnet.maxpool
-        self.down1 = resnet.layer1 # 64
-        self.down2 = resnet.layer2 # 128
-        self.down3 = resnet.layer3 # 256
-        self.down4 = resnet.layer4 # 512
-
-        self.up4 = UpBlock(in_channels*12, in_channels*2, nb_Conv=2, channel=512, up=True)
-        self.up3 = UpBlock(in_channels*4 , in_channels*1, nb_Conv=2, channel=128, up=True)
-        self.up2 = UpBlock(in_channels*2 , in_channels*1, nb_Conv=2, channel=64 , up=True)
-        self.up1 = UpBlock(in_channels*2 , in_channels*1, nb_Conv=2, channel=64 , up=True)
-        # self.up = nn.Upsample(scale_factor=2) 
-        self.outc = nn.Conv2d(in_channels, n_classes, kernel_size=(1,1))
-        # transformer = deit_tiny_distilled_patch16_224(pretrained=True)
-        # self.patch_embed = transformer.patch_embed
-        # self.transformers = nn.ModuleList(
-        #     [transformer.blocks[i] for i in range(12)]
-        # )
-
-        # self.psi = nn.Sequential(
-        #     nn.Conv2d(192, 1, kernel_size=1, stride=1, padding=0, bias=True),
-        #     nn.BatchNorm2d(1),
-        #     nn.Sigmoid()
-        # )
-        # self.ups2  = nn.Upsample(scale_factor=2)
-        # self.ups4  = nn.Upsample(scale_factor=4)
-        # self.ups8  = nn.Upsample(scale_factor=8)  
-        # self.ups16 = nn.Upsample(scale_factor=16)  
-
-        # self.esp4 = DilatedParllelResidualBlockB(nIn=in_channels*2, nOut=in_channels*2)
-        # self.esp3 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
-        # self.esp2 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
-        # self.esp1 = DilatedParllelResidualBlockB(nIn=in_channels*1, nOut=in_channels*1)
-
-        if n_classes == 0:
-            self.last_activation = nn.Sigmoid()
-        else:
-            self.last_activation = None
+        self.up4 = UpBlock(432, 40, nb_Conv=2, channel=512, up=True)
+        self.up3 = UpBlock(80 , 24, nb_Conv=2, channel=128, up=True)
+        self.up2 = UpBlock(48 , 16, nb_Conv=2, channel=64 , up=True)
+        self.up1 = UpBlock(32 , 16, nb_Conv=2, channel=64 , up=True)
+        self.outc = nn.Conv2d(16, n_classes, kernel_size=(1,1))
 
     def forward(self, x):
-        # Question here
         b, c, h, w = x.shape
-        x = x.float()
-        x1 = self.inc(x) # 112
-        x2 = self.down1(self.maxpool(x1)) # 112
-        x3 = self.down2(x2) # 56
-        x4 = self.down3(x3) # 28
-        x5 = self.down4(x4) # 14
-        # torch.Size([8, 64, 112, 112])
-        # torch.Size([8, 64, 112, 112])
-        # torch.Size([8, 128, 56, 56])
-        # torch.Size([8, 256, 28, 28])
-        # torch.Size([8, 512, 14, 14])
+        x0 = x.float()
+        x1 = self.layer1(x0)
+        x2 = self.layer2(x1)
+        x3 = self.layer3(x2) 
+        x4 = self.layer4(x3) 
+        x5 = self.layer5(x4) 
 
-        # emb = self.patch_embed(x)
-        # for i in range(12):
-        #     emb = self.transformers[i](emb)
-        # feature_tf = emb.permute(0, 2, 1)
-        # feature_tf = feature_tf.view(b, 192, 14, 14)
-        # psi = self.psi(feature_tf)
-
-        # x1 = x1 * self.ups16(psi)
-        # x2 = x2 * self.ups8(psi)
-        # x3 = x3 * self.ups4(psi)
-        # x4 = x4 * self.ups2(psi)
+        # torch.Size([8, 16, 224, 224])
+        # torch.Size([8, 24, 112, 112])
+        # torch.Size([8, 40, 56, 56])
+        # torch.Size([8, 112, 28, 28])
+        # torch.Size([8, 320, 14, 14])
 
         x = self.up4(x5, x4)
         x = self.up3(x , x3)
         x = self.up2(x , x2)
         x = self.up1(x , x1)
-
-
-
-        if self.last_activation is not None:
-            logits = self.last_activation(self.outc(x))
-            # print("111")
-        else:
-            logits = self.outc(x)
-            # print("222")
-        # logits = self.outc(x) # if using BCEWithLogitsLoss
-        # print(logits.size())
-        return logits
+        out = self.outc(x)
+        return out
 
 class SEBlock(nn.Module):
     def __init__(self, channel, r=16):
