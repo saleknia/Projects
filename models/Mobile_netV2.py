7252ed3e769670408196731fadb7cb62ae48a03c
saleknia <salekniaamirhossein@gmail.com>
2022-11-08 16:41:32 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -32,7 +32,6 @@ class Mobile_netV2(nn.Module):
         model = efficientnet_b0(weights=EfficientNet_B0_Weights)
         
         self.features = model.features
-        self.attention = SEBlock(channel=1280)
         self.avgpool = model.avgpool
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.4, inplace=True),
@@ -80,11 +79,32 @@ class Mobile_netV2(nn.Module):
         # x = x.view(x.size(0), -1)
         # x = self.classifier(x)
         x = self.features(x)
-        x = self.attention(x)
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-        return x
+        # x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.classifier(x)
+        # return x
+
+        x1 = self.avgpool(x[:, :, 0:4, 0:4])
+        x1 = x1.view(x1.size(0), -1)
+        x1 = self.classifier(x1)
+
+        x2 = self.avgpool(x[:, :, 4: , 0:4])
+        x2 = x2.view(x2.size(0), -1)
+        x2 = self.classifier(x2)
+
+        x3 = self.avgpool(x[:, :, 0:4, 4: ])
+        x3 = x3.view(x3.size(0), -1)
+        x3 = self.classifier(x3)
+
+        x4 = self.avgpool(x[:, :, 4: , 4: ])
+        x4 = x4.view(x4.size(0), -1)
+        x4 = self.classifier(x4)
+
+        x5 = self.avgpool(x)
+        x5 = x5.view(x5.size(0), -1)
+        x5 = self.classifier(x5)
+
+        return (x1 + x2 + x3 + x4 + x5) / 5.0
 
 
 
