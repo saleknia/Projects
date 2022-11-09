51c0d4f5df9c49df7a1c8c6ee8cf4c0349d52611
saleknia <salekniaamirhossein@gmail.com>
2022-11-09 10:37:26 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -30,27 +30,28 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        # model = efficientnet_b1(weights=EfficientNet_B1_Weights)
-        model = efficientnet_b6(weights=EfficientNet_B6_Weights)
+        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        # model = efficientnet_b6(weights=EfficientNet_B6_Weights)
 
         self.features = model.features
+        self.features[0][0].stride = (1, 1)
         self.avgpool = model.avgpool
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=2304, out_features=512, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=512, out_features=256, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=256, out_features=40, bias=True),
-        )
         # self.classifier = nn.Sequential(
         #     nn.Dropout(p=0.4, inplace=True),
-        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Linear(in_features=2304, out_features=512, bias=True),
         #     nn.Dropout(p=0.4, inplace=True),
         #     nn.Linear(in_features=512, out_features=256, bias=True),
         #     nn.Dropout(p=0.4, inplace=True),
         #     nn.Linear(in_features=256, out_features=40, bias=True),
         # )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=1280, out_features=512, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=256, out_features=40, bias=True),
+        )
 
         # model = resnet50(pretrained)
         # # # model = resnet18(pretrained)
