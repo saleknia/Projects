604fe7fa4052ee20dfc2bd6fbbc380103bc977ae
saleknia <salekniaamirhossein@gmail.com>
2022-11-10 15:46:15 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -30,9 +30,16 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        # self.model = deit_tiny_distilled_patch16_224(pretrained=True)
-        # self.model.head = nn.Linear(in_features=192, out_features=40, bias=True)
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        self.model = deit_base_distilled_patch16_224(pretrained=True)
+        self.model.head = nn.Sequential(
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=768, out_features=512, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=512, out_features=256, bias=True),
+            nn.Dropout(p=0.4, inplace=True),
+            nn.Linear(in_features=256, out_features=40, bias=True),
+        )
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
         # model = efficientnet_b6(weights=EfficientNet_B6_Weights)
         # self.patch_embed = model.patch_embed
         # self.transformers = nn.ModuleList(
@@ -62,45 +69,45 @@ class Mobile_netV2(nn.Module):
         # )
         # self.aspp = ASPP(in_channels=1280)
 
-        model = resnet50(pretrained)
+        # model = resnet50(pretrained)
 
         # # take pretrained resnet, except AvgPool and FC
-        self.conv1 = model.conv1
-        self.conv1.stride = (1, 1)
-        self.bn1 = model.bn1
-        self.relu = model.relu
-        self.maxpool = model.maxpool
-        self.layer1 = model.layer1
-        self.layer2 = model.layer2
-        self.layer3 = model.layer3
-        self.layer4 = model.layer4
+        # self.conv1 = model.conv1
+        # self.conv1.stride = (1, 1)
+        # self.bn1 = model.bn1
+        # self.relu = model.relu
+        # self.maxpool = model.maxpool
+        # self.layer1 = model.layer1
+        # self.layer2 = model.layer2
+        # self.layer3 = model.layer3
+        # self.layer4 = model.layer4
 
-        self.avgpool = model.avgpool
+        # self.avgpool = model.avgpool
 
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=2048, out_features=512, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=512, out_features=256, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=256, out_features=40, bias=True),
-        )
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=2048, out_features=512, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=512, out_features=256, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=256, out_features=40, bias=True),
+        # )
 
     def forward(self, x):
-        b, c, w, h = x.shape
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
+        # b, c, w, h = x.shape
+        # x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.relu(x)
+        # x = self.maxpool(x)
 
-        layer1 = self.layer1(x)
-        layer2 = self.layer2(layer1)
-        layer3 = self.layer3(layer2)
-        layer4 = self.layer4(layer3)
+        # layer1 = self.layer1(x)
+        # layer2 = self.layer2(layer1)
+        # layer3 = self.layer3(layer2)
+        # layer4 = self.layer4(layer3)
 
-        x = self.avgpool(layer4)
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
+        # x = self.avgpool(layer4)
+        # x = x.view(x.size(0), -1)
+        # x = self.classifier(x)
 
         # x = self.features(x)
         # x = self.avgpool(x)
@@ -114,7 +121,8 @@ class Mobile_netV2(nn.Module):
         # emb = self.avgpool(emb)
         # print(emb.shape)
         # emb = self.classifier(emb)
-        return x
+        out, _ = self.model(x)
+        return out
 
 
 
