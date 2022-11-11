26594d7be85cfd9567f41c6072ca709fb8668548
saleknia <salekniaamirhossein@gmail.com>
2022-11-11 14:45:14 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -17,26 +17,56 @@ from utils import importance_maps_distillation as imd
 import os
 warnings.filterwarnings("ignore")
 
+def loss_label_smoothing(outputs, labels):
+    """
+    loss function for label smoothing regularization
+    """
+    alpha = 0.4
+    N = outputs.size(0)  # batch_size
+    C = outputs.size(1)  # number of classes
+    smoothed_labels = torch.full(size=(N, C), fill_value= alpha / (C - 1)).cuda()
+    smoothed_labels.scatter_(dim=1, index=torch.unsqueeze(labels, dim=1), value=1-alpha)
+
+    log_prob = torch.nn.functional.log_softmax(outputs, dim=1)
+    loss = -torch.sum(log_prob * smoothed_labels) / N
+
+    return loss
 
 def loss_kd_regularization(outputs, labels):
     """
     loss function for mannually-designed regularization: Tf-KD_{reg}
     """
     alpha = 0.4
-    T = 5000
-    correct_prob = 0.99    # the probability for correct class in u(k)
     loss_CE = F.cross_entropy(outputs, labels)
     K = outputs.size(1)
 
     teacher_soft = torch.ones_like(outputs).cuda()
-    teacher_soft = teacher_soft*(1-correct_prob)/(K-1)  # p^d(k)
-    for i in range(outputs.shape[0]):
-        teacher_soft[i ,labels[i]] = correct_prob
-    loss_soft_regu = nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_soft/T, dim=1))
+    teacher_soft = teacher_soft*(1/K)  # p^d(k)
+    loss_soft_regu = nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_soft, dim=1))
 
-    KD_loss = (1. - alpha)*loss_CE + alpha*loss_soft_regu
+    KD_loss = KD_loss = (1. - alpha)*loss_CE + alpha*loss_soft_regu
 
     return KD_loss
+
+# def loss_kd_regularization(outputs, labels):
+#     """
+#     loss function for mannually-designed regularization: Tf-KD_{reg}
+#     """
+#     alpha = 0.4
+#     T = 5
+#     correct_prob = 0.99    # the probability for correct class in u(k)
+#     loss_CE = F.cross_entropy(outputs, labels)
+#     K = outputs.size(1)
+
+#     teacher_soft = torch.ones_like(outputs).cuda()
+#     teacher_soft = teacher_soft*(1-correct_prob)/(K-1)  # p^d(k)
+#     for i in range(outputs.shape[0]):
+#         teacher_soft[i ,labels[i]] = correct_prob
+#     loss_soft_regu = nn.KLDivLoss()(F.log_softmax(outputs, dim=1), F.softmax(teacher_soft/T, dim=1))
+
+#     KD_loss = (1. - alpha)*loss_CE + alpha*loss_soft_regu
+
+#     return KD_loss
 
 
 class FSP(nn.Module):
