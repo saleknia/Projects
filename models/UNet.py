2a87f0a0ffd4e413afa2f96e9e028cddf5111cb0
saleknia <salekniaamirhossein@gmail.com>
2022-11-14 20:37:14 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -20,7 +20,7 @@ def _make_nConv(in_channels, out_channels, nb_Conv, activation='ReLU'):
 class ConvBatchNorm(nn.Module):
     """(convolution => [BN] => ReLU)"""
 
-    def __init__(self, in_channels, out_channels, activation='ReLU'):
+    def __init__(self, in_channels, out_channels, activation='ReLU', kernel_size=3, padding=1):
         super(ConvBatchNorm, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels,
                               kernel_size=3, padding=1)
@@ -49,7 +49,11 @@ class UpBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
-        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.up = nn.Sequential(
+                                ConvBatchNorm(in_channels, in_channels//2, activation='ReLU', kernel_size=1, padding=0),
+                                nn.Upsample(scale_factor=2)   
+                                )
+        nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels, out_channels, nb_Conv, activation)
     def forward(self, x, skip_x):
         x = self.up(x)
