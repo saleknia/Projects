61203fa8046b578e8c052a9e35cb4aeae0197de8
saleknia <salekniaamirhossein@gmail.com>
2022-11-16 06:52:39 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -143,15 +143,12 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-        # self.att = ParallelPolarizedSelfAttention(channel=in_channels // 2)
         self.conv = _make_nConv(in_channels, out_channels, nb_Conv, activation)
-        self.SK = SKAttention(channel=out_channels)
     def forward(self, x, skip_x):
         x = self.up(x)
         # x = self.att(x)
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
-        x = self.SK(x)
         return x
 
 
@@ -177,47 +174,31 @@ class UNet(nn.Module):
         # torch.Size([8, 512, 14, 14])
         # torch.Size([8, 1024, 7, 7])
 
-        # self.up4 = UpBlock(1024, 512, nb_Conv=2)
-        # self.up3 = UpBlock(512 , 256, nb_Conv=2)
-        # self.up2 = UpBlock(256 , 128, nb_Conv=2)
-        # self.up1 = UpBlock(128 , 64 , nb_Conv=2)
+        self.up3 = UpBlock(512, 256, nb_Conv=2)
+        self.up2 = UpBlock(256, 128, nb_Conv=2)
+        self.up1 = UpBlock(128, 64 , nb_Conv=2)
 
-        self.up5 = nn.Upsample(scale_factor=16)
-        self.up4 = nn.Upsample(scale_factor=8)
-        self.up3 = nn.Upsample(scale_factor=4 )
-        self.up2 = nn.Upsample(scale_factor=2 )
+        self.final_conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
+        self.final_relu1 = nn.ReLU(inplace=True)
+        self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.final_relu2 = nn.ReLU(inplace=True)
+        self.final_conv3 = nn.Conv2d(32, n_classes, kernel_size=1, padding=0)
 
-        # self.final_conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
-        # self.final_relu1 = nn.ReLU(inplace=True)
-        # self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
-        # self.final_relu2 = nn.ReLU(inplace=True)
-        # self.final_conv3 = nn.Conv2d(32, n_classes, kernel_size=1, padding=0)
-        self.outc = nn.Conv2d(1984, 1, 1, padding=0)
-        self.up = nn.Upsample(scale_factor=2 )
     def forward(self, x):
         # Question here
         x = x.float()
-        x1, x2, x3, x4, x5 = self.encoder(x)
+        x1, x2, x3, x4, _ = self.encoder(x)
 
-        x1 = x1
-        x2 = self.up2(x2)
-        x3 = self.up3(x3)
-        x4 = self.up4(x4)
-        x5 = self.up5(x5)
+        x = self.up3(x4, x3)
+        x = self.up2(x , x2)
+        x = self.up1(x , x1)
 
-        x = torch.cat([x1, x2, x3, x4, x5], dim=1)  # dim 1 is the channel dimension
+        x = self.final_conv1(x)
+        x = self.final_relu1(x)
+        x = self.final_conv2(x)
+        x = self.final_relu2(x)
+        out = self.final_conv3(x)
 
-        # x = self.up4(x5, x4)
-        # x = self.up3(x, x3)
-        # x = self.up2(x, x2)
-        # x = self.up1(x, x1)
-
-        # x = self.final_conv1(x)
-        # x = self.final_relu1(x)
-        # x = self.final_conv2(x)
-        # x = self.final_relu2(x)
-        # out = self.final_conv3(x)
-        out = self.up(self.outc(x))
         return out
 
 
