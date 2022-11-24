87e699f79ee7b86c61e1ae50f9fb8a28c0b9e3e5
saleknia <salekniaamirhossein@gmail.com>
2022-11-24 07:57:11 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -315,9 +315,7 @@ class UNet(nn.Module):
         self.encoder.incre_modules = None
         self.encoder.conv1.stride = (1, 1)
 
-        self.SK_1 = SKAttention(channel=18)
-        self.SK_2 = SKAttention(channel=36)
-        self.SK_3 = SKAttention(channel=72)
+        self.aspp = ASPP()
 
         # self.combine_1 = nn.Conv2d(in_channels=36 , out_channels=18 , kernel_size=3, padding=1)
         # self.combine_2 = nn.Conv2d(in_channels=72 , out_channels=36 , kernel_size=3, padding=1)
@@ -403,9 +401,7 @@ class UNet(nn.Module):
 
         x1, x2, x3, x4 = xl[0], xl[1], xl[2], xl[3]
 
-        x1 = x1 + self.SK_1(x1)
-        x2 = x2 + self.SK_2(x2)
-        x3 = x3 + self.SK_3(x3)
+        x4 = self.ASPP(x4)
       
         # x0, x1, x2, x3, x4 = self.encoder(x)
 
@@ -442,6 +438,70 @@ class UNet(nn.Module):
         out = self.final_conv3(x)
 
         return out
+
+class _ASPPModule(nn.Module):
+    def __init__(self, inplanes, planes, kernel_size, padding, dilation):
+        super(_ASPPModule, self).__init__()
+        self.atrous_conv = nn.Conv2d(inplanes, planes, kernel_size=kernel_size,
+                                            stride=1, padding=padding, dilation=dilation, bias=False)
+        self.bn = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU()
+
+        self._init_weight()
+
+    def forward(self, x):
+        x = self.atrous_conv(x)
+        x = self.bn(x)
+
+        return self.relu(x)
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+class ASPP(nn.Module):
+    def __init__(self):
+        super(ASPP, self).__init__()
+        inplanes = 144
+        dilations = [1, 6, 12, 18]
+
+        self.aspp1 = _ASPPModule(inplanes, 72, 1, padding=0, dilation=dilations[0])
+        self.aspp2 = _ASPPModule(inplanes, 72, 3, padding=dilations[1], dilation=dilations[1])
+        self.aspp3 = _ASPPModule(inplanes, 72, 3, padding=dilations[2], dilation=dilations[2])
+        self.aspp4 = _ASPPModule(inplanes, 72, 3, padding=dilations[3], dilation=dilations[3])
+
+        self.conv1 = nn.Conv2d(288, 144, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(144)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
+        self._init_weight()
+
+    def forward(self, x):
+        x1 = self.aspp1(x)
+        x2 = self.aspp2(x)
+        x3 = self.aspp3(x)
+        x4 = self.aspp4(x)
+        x = torch.cat((x1, x2, x3, x4), dim=1)
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        return x
+
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                # m.weight.data.normal_(0, math.sqrt(2. / n))
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
 # class UNet(nn.Module):
 #     def __init__(self, n_channels=3, n_classes=1):
