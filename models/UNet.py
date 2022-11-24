14c744a9f679c141dd61b14e74c18ab7d004ea75
saleknia <salekniaamirhossein@gmail.com>
2022-11-24 09:21:30 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -7,10 +7,76 @@ from collections import OrderedDict
 import ml_collections
 from torchvision import models as resnet_model
 from .CTrans import ChannelTransformer
+import torch
+import torchvision.ops
+from torch import nn
 
+class DeformableConv2d(nn.Module):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 offset_channels,
+                 kernel_size=3,
+                 stride=1,
+                 padding=1,
+                 bias=False,
+                 up_scale=1.0):
+
+        super(DeformableConv2d, self).__init__()
+        
+        assert type(kernel_size) == tuple or type(kernel_size) == int
+
+        kernel_size = kernel_size if type(kernel_size) == tuple else (kernel_size, kernel_size)
+        self.stride = stride if type(stride) == tuple else (stride, stride)
+        self.padding = padding
+        
+        self.offset_conv = nn.Conv2d(offset_channels, 
+                                     2 * kernel_size[0] * kernel_size[1],
+                                     kernel_size=kernel_size, 
+                                     stride=stride,
+                                     padding=self.padding, 
+                                     bias=True)
+        self.up = nn.Upsample(scale_factor=up_scale)
+
+        nn.init.constant_(self.offset_conv.weight, 0.)
+        nn.init.constant_(self.offset_conv.bias, 0.)
+        
+        self.modulator_conv = nn.Conv2d(in_channels, 
+                                     1 * kernel_size[0] * kernel_size[1],
+                                     kernel_size=kernel_size, 
+                                     stride=stride,
+                                     padding=self.padding, 
+                                     bias=True)
+
+        nn.init.constant_(self.modulator_conv.weight, 0.)
+        nn.init.constant_(self.modulator_conv.bias, 0.)
+        
+        self.regular_conv = nn.Conv2d(in_channels=in_channels,
+                                      out_channels=out_channels,
+                                      kernel_size=kernel_size,
+                                      stride=stride,
+                                      padding=self.padding,
+                                      bias=bias)
+
+    def forward(self, x, offset):
+        #h, w = x.shape[2:]
+        #max_offset = max(h, w)/4.
+        offset = self.up(offset)
+        offset = self.offset_conv(x)#.clamp(-max_offset, max_offset)
+        modulator = 2. * torch.sigmoid(self.modulator_conv(x))
+        
+        x = torchvision.ops.deform_conv2d(input=x, 
+                                          offset=offset, 
+                                          weight=self.regular_conv.weight, 
+                                          bias=self.regular_conv.bias, 
+                                          padding=self.padding,
+                                          mask=modulator,
+                                          stride=self.stride,
+                                          )
+        return x
 class SKAttention(nn.Module):
 
-    def __init__(self, channel=512,kernels=[3,5],reduction=16,group=1,L=32):
+    def __init__(self, channel=512,kernels=[1,3,5,7],reduction=16,group=1,L=32):
         super().__init__()
         self.d=max(L,channel//reduction)
         self.convs=nn.ModuleList([])
@@ -315,7 +381,9 @@ class UNet(nn.Module):
         self.encoder.incre_modules = None
         self.encoder.conv1.stride = (1, 1)
 
-        self.aspp = ASPP()
+        self.DC_1 = DeformableConv2d(in_channels=18, out_channels=18, offset_channels=144, kernel_size=3, stride=1, padding=1, bias=False, up_scale=8.0)
+        self.DC_2 = DeformableConv2d(in_channels=36, out_channels=36, offset_channels=144, kernel_size=3, stride=1, padding=1, bias=False, up_scale=4.0)
+        self.DC_3 = DeformableConv2d(in_channels=72, out_channels=72, offset_channels=144, kernel_size=3, stride=1, padding=1, bias=False, up_scale=2.0)
 
         # self.combine_1 = nn.Conv2d(in_channels=36 , out_channels=18 , kernel_size=3, padding=1)
         # self.combine_2 = nn.Conv2d(in_channels=72 , out_channels=36 , kernel_size=3, padding=1)
@@ -401,8 +469,10 @@ class UNet(nn.Module):
 
         x1, x2, x3, x4 = xl[0], xl[1], xl[2], xl[3]
 
-        x4 = self.aspp(x4)
-      
+        x1 = x1 + self.DC_1(x1, x4)
+        x2 = x2 + self.DC_2(x2, x4)
+        x3 = x3 + self.DC_3(x3, x4)
+
         # x0, x1, x2, x3, x4 = self.encoder(x)
 
         # x1 = self.combine_1(torch.cat([x1, t1], dim=1))
