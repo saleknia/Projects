705419d451332ab513b11a29d6e88f29da9fd033
saleknia <salekniaamirhossein@gmail.com>
2022-11-27 22:31:19 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -175,7 +175,7 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
         # loss_ce = ce_loss(outputs, targets.unsqueeze(dim=1)) 
         # loss_dice = dice_loss(inputs=outputs, targets=targets)
         # loss = loss_ce + loss_dice
-        loss = structure_loss(pred=outputs, mask=targets)
+        loss = structure_loss(pred=outputs, mask=targets.unsqueeze(dim=1))
 
         # iter_num = iter_num + 1 
         # if iter_num % (total_batchs*10)==0:
