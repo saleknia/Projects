41ac995b859fcd01bd1f198a1fb7b5b4b7d5530c
saleknia <salekniaamirhossein@gmail.com>
2022-12-05 20:41:07 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -884,7 +884,7 @@ class DATUNet(nn.Module):
 
         self.FAMBlock1 = FAMBlock(channels=64)
         self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(6)])
-        self.Reduce = ConvBatchNorm(in_channels=64, out_channels=48, activation='ReLU', kernel_size=1, padding=0, dilation=1)
+        self.Reduce = ConvBatchNorm(in_channels=64, out_channels=48, activation='ReLU', kernel_size=3, padding=1, dilation=1)
 
         self.encoder = DAT(
             img_size=224,
