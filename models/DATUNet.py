eeb5e615098e6886a94ca4561a0c136935882e33
saleknia <salekniaamirhossein@gmail.com>
2022-12-06 23:02:24 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1043,10 +1043,6 @@ class DATUNet(nn.Module):
         outputs = self.encoder(x0)
         x1, x2, x3 = self.norm_1(outputs[0]), self.norm_2(outputs[1]), self.norm_3(outputs[2])
 
-        x1 = self.reduce_1(x1)
-        x2 = self.reduce_2(x2)
-        x3 = self.reduce_3(x3)
-
         x = [x1, x2, x3]
         x = self.skip(x)
 
