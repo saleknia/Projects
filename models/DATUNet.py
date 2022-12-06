6e804fa720e289edf27aed77983c4a45dfb80c77
saleknia <salekniaamirhossein@gmail.com>
2022-12-06 22:52:17 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -696,6 +696,93 @@ class BiFusion_block(nn.Module):
         else:
             return fuse
 
+def create_aa(aa_layer, channels, stride=2, enable=True):
+    if not aa_layer or not enable:
+        return nn.Identity()
+    return aa_layer(stride) if issubclass(aa_layer, nn.AvgPool2d) else aa_layer(channels=channels, stride=stride)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(
+            self, inplanes, planes, stride=1, downsample=None, cardinality=1, base_width=64,
+            reduce_first=1, dilation=1, first_dilation=None, act_layer=nn.ReLU, norm_layer=nn.BatchNorm2d,
+            attn_layer=None, aa_layer=None, drop_block=None, drop_path=None):
+        super(BasicBlock, self).__init__()
+
+        assert cardinality == 1, 'BasicBlock only supports cardinality of 1'
+        assert base_width == 64, 'BasicBlock does not support changing base width'
+        first_planes = planes // reduce_first
+        outplanes = planes * self.expansion
+        first_dilation = first_dilation or dilation
+        use_aa = aa_layer is not None and (stride == 2 or first_dilation != dilation)
+
+        self.conv1 = nn.Conv2d(
+            inplanes, first_planes, kernel_size=3, stride=1 if use_aa else stride, padding=first_dilation,
+            dilation=first_dilation, bias=False)
+        self.bn1 = norm_layer(first_planes)
+        self.drop_block = drop_block() if drop_block is not None else nn.Identity()
+        self.act1 = act_layer(inplace=True)
+        self.aa = create_aa(aa_layer, channels=first_planes, stride=stride, enable=use_aa)
+
+        self.conv2 = nn.Conv2d(
+            first_planes, outplanes, kernel_size=3, padding=dilation, dilation=dilation, bias=False)
+        self.bn2 = norm_layer(outplanes)
+
+        self.se = create_attn(attn_layer, outplanes)
+
+        self.act2 = act_layer(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+        self.dilation = dilation
+        self.drop_path = drop_path
+
+    def zero_init_last(self):
+        nn.init.zeros_(self.bn2.weight)
+
+    def forward(self, x):
+        shortcut = x
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.drop_block(x)
+        x = self.act1(x)
+        x = self.aa(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        if self.se is not None:
+            x = self.se(x)
+
+        if self.drop_path is not None:
+            x = self.drop_path(x)
+
+        if self.downsample is not None:
+            shortcut = self.downsample(shortcut)
+        x += shortcut
+        x = self.act2(x)
+
+        return x
+
+
+def make_stage():
+    num_modules = 4
+    num_branches = 3
+    num_blocks = (1, 1, 1)
+    num_channels = (48, 96, 192)
+    num_in_chs = (48, 96, 192)
+    block = BasicBlock
+    fuse_method = 'SUM'
+
+    modules = []
+    for i in range(num_modules):
+        reset_multi_scale_output = multi_scale_output or i < num_modules - 1
+        modules.append(HighResolutionModule(
+            num_branches, block, num_blocks, num_in_chs, num_channels, fuse_method, reset_multi_scale_output)
+        )
+
+    return nn.Sequential(*modules)
 
 class HighResolutionModule(nn.Module):
     def __init__(self, num_branches, blocks, num_blocks, num_in_chs,
@@ -893,7 +980,7 @@ class DATUNet(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
 
-        self.skip = timm.create_model('hrnet_w48', pretrained=True, features_only=True).stage3[0:2]
+        self.skip = make_stage()
 
         self.encoder = DAT(
             img_size=224,
@@ -924,10 +1011,6 @@ class DATUNet(nn.Module):
         self.norm_3 = LayerNormProxy(dim=384)
         self.norm_2 = LayerNormProxy(dim=192)
         self.norm_1 = LayerNormProxy(dim=96 )
-      
-        self.reduce_3 = ConvBatchNorm(in_channels=384 , out_channels=192 , activation='ReLU', kernel_size=1, padding=0)
-        self.reduce_2 = ConvBatchNorm(in_channels=192 , out_channels=96  , activation='ReLU', kernel_size=1, padding=0)
-        self.reduce_1 = ConvBatchNorm(in_channels=96  , out_channels=48  , activation='ReLU', kernel_size=1, padding=0)
 
         # self.fuse_layers = make_fuse_layers()
         # self.fuse_act = nn.ReLU()
@@ -935,15 +1018,15 @@ class DATUNet(nn.Module):
         # self.fuse_layers_2 = make_fuse_layers()
         # self.fuse_act_2 = nn.ReLU()
 
-        self.combine_3 = ConvBatchNorm(in_channels=384, out_channels=384, activation='ReLU', kernel_size=3, padding=1, dilation=1)
-        self.combine_2 = ConvBatchNorm(in_channels=192, out_channels=192, activation='ReLU', kernel_size=3, padding=1, dilation=1)
-        self.combine_1 = ConvBatchNorm(in_channels=96 , out_channels=96 , activation='ReLU', kernel_size=3, padding=1, dilation=1)
+        # self.combine_3 = ConvBatchNorm(in_channels=384, out_channels=384, activation='ReLU', kernel_size=3, padding=1, dilation=1)
+        # self.combine_2 = ConvBatchNorm(in_channels=192, out_channels=192, activation='ReLU', kernel_size=3, padding=1, dilation=1)
+        # self.combine_1 = ConvBatchNorm(in_channels=96 , out_channels=96 , activation='ReLU', kernel_size=3, padding=1, dilation=1)
 
-        self.up2 = UpBlock(384, 192, nb_Conv=2)
-        self.up1 = UpBlock(192, 96 , nb_Conv=2)
+        self.up2 = UpBlock(192, 96, nb_Conv=2)
+        self.up1 = UpBlock(96 , 48, nb_Conv=2)
 
 
-        self.final_conv1 = nn.ConvTranspose2d(96, 48, 4, 2, 1)
+        self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
         self.final_relu1 = nn.ReLU(inplace=True)
         self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
         self.final_relu2 = nn.ReLU(inplace=True)
@@ -964,10 +1047,10 @@ class DATUNet(nn.Module):
         x3 = self.reduce_3(x3)
 
         x = [x1, x2, x3]
-        y = self.skip(x)
+        x = self.skip(x)
 
-        x1, x2, x3 = self.combine_1(torch.cat([x[0], y[0]], 1)), self.combine_2(torch.cat([x[1], y[1]], 1)), self.combine_3(torch.cat([x[2], y[2]], 1))
-
+        x1, x2, x3 = x[0], x[1], x[2]
+    
         x = self.up2(x3, x2) 
         x = self.up1(x , x1) 
 
