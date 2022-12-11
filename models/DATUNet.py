73ef01c2c041f7b0e9885c8d0de061958666c96a
saleknia <salekniaamirhossein@gmail.com>
2022-12-11 18:05:07 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -626,7 +626,7 @@ class UpBlock(nn.Module):
         self.att = ParallelPolarizedSelfAttention(channel=in_channels//2)
     def forward(self, x, skip_x):
         x = self.up(x)
-        x = self.att(x)
+        x = self.att(x) + x
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
         return x
@@ -1018,18 +1018,18 @@ class DATUNet(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
 
-        # # resnet = resnet_model.resnet34(pretrained=True)
+        # resnet = resnet_model.resnet34(pretrained=True)
 
-        # # self.firstconv = resnet.conv1
-        # # self.firstbn = resnet.bn1
-        # # self.firstrelu = resnet.relu
-        # # self.encoder1 = resnet.layer1
-        # # self.encoder2 = None
-        # # self.encoder3 = None
-        # # self.encoder4 = None
-        # # self.Reduce = ConvBatchNorm(in_channels=64, out_channels=48, kernel_size=3, padding=1)
-        # # self.FAMBlock1 = FAMBlock(in_channels=48, out_channels=48)
-        # # self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(6)])
+        # self.firstconv = resnet.conv1
+        # self.firstbn = resnet.bn1
+        # self.firstrelu = resnet.relu
+        # self.encoder1 = resnet.layer1
+        # self.encoder2 = None
+        # self.encoder3 = None
+        # self.encoder4 = None
+        # self.Reduce = ConvBatchNorm(in_channels=64, out_channels=48, kernel_size=3, padding=1)
+        # self.FAMBlock1 = FAMBlock(in_channels=48, out_channels=48)
+        # self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(6)])
 
 
         # self.encoder = DAT(
@@ -1061,42 +1061,25 @@ class DATUNet(nn.Module):
         # self.norm_4 = LayerNormProxy(dim=384)
         # self.norm_3 = LayerNormProxy(dim=192)
         # self.norm_2 = LayerNormProxy(dim=96 )
-        # # self.norm_1 = LayerNormProxy(dim=48 )
+        # self.norm_1 = LayerNormProxy(dim=48 )
 
-        # # self.fuse_layers = make_fuse_layers()
-        # # self.fuse_act = nn.ReLU()
+        # self.fuse_layers = make_fuse_layers()
+        # self.fuse_act = nn.ReLU()
 
         # self.up3 = UpBlock(384, 192, nb_Conv=2)
         # self.up2 = UpBlock(192, 96 , nb_Conv=2)
-        # # self.up1 = UpBlock(96 , 48 , nb_Conv=2)
+        # self.up1 = UpBlock(96 , 48 , nb_Conv=2)
 
-        # # self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
-        # # self.final_relu1 = nn.ReLU(inplace=True)
-        # # self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
-        # # self.final_relu2 = nn.ReLU(inplace=True)
-        # # self.final_conv3 = nn.Conv2d(24, n_classes, 3, padding=1)
-
-        # self.final_conv1 = nn.ConvTranspose2d(96, 48, 4, 2, 1)
+        # self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
         # self.final_relu1 = nn.ReLU(inplace=True)
         # self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
         # self.final_relu2 = nn.ReLU(inplace=True)
         # self.final_conv3 = nn.Conv2d(24, n_classes, 3, padding=1)
-        # self.final_up = nn.Upsample(scale_factor=2.0)
 
 
-        # resnet = resnet_model.resnet34(pretrained=True)
-
-        # self.firstconv = resnet.conv1
-        # self.firstbn = resnet.bn1
-        # self.firstrelu = resnet.relu
-        # self.encoder1 = resnet.layer1
-        # self.encoder2 = None
-        # self.encoder3 = None
-        # self.encoder4 = None
-        # self.Reduce = ConvBatchNorm(in_channels=64, out_channels=48, kernel_size=3, padding=1)
-        # self.FAMBlock1 = FAMBlock(in_channels=48, out_channels=48)
-        # self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(6)])
-
+        #####################################################################
+        #####################################################################
+        #####################################################################
 
         self.encoder = DAT(
             img_size=224,
@@ -1144,6 +1127,10 @@ class DATUNet(nn.Module):
         x_input = x.float()
         b, c, h, w = x.shape
 
+        #####################################################################
+        #####################################################################
+        #####################################################################
+
         # x0 = self.firstconv(x_input)
         # x0 = self.firstbn(x0)
         # x0 = self.firstrelu(x0)
@@ -1153,7 +1140,6 @@ class DATUNet(nn.Module):
         #     x0 = self.FAM1[i](x0)
 
         # outputs = self.encoder(x_input)
-        # # x1, x2, x3 = self.norm_2(outputs[0]), self.norm_3(outputs[1]), self.norm_4(outputs[2])
 
         # x0, x1, x2, x3 = self.norm_1(x0), self.norm_2(outputs[0]), self.norm_3(outputs[1]), self.norm_4(outputs[2])
 
@@ -1180,7 +1166,10 @@ class DATUNet(nn.Module):
         # x = self.final_conv2(x)
         # x = self.final_relu2(x)
         # x = self.final_conv3(x)
-        # # x = self.final_up(x)
+
+        #####################################################################
+        #####################################################################
+        #####################################################################
 
         outputs = self.encoder(x_input)
         x1, x2, x3 = self.norm_2(outputs[0]), self.norm_3(outputs[1]), self.norm_4(outputs[2])
