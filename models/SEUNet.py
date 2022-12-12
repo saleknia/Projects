23a301b65952c1dfb905b971e62446fe227caa80
saleknia <salekniaamirhossein@gmail.com>
2022-12-12 09:14:14 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -67,15 +67,14 @@ class UpBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
-
-        # self.up = nn.Upsample(scale_factor=2)
-        self.up = nn.ConvTranspose2d(in_channels//2,in_channels//2,(2,2),2)
-        self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.conv = _make_nConv(in_channels=(in_channels//2)+out_channels, out_channels=out_channels, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
 
     def forward(self, x, skip_x):
-        out = self.up(x)
-        x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
-        return self.nConvs(x)
+        x = self.up(x)
+        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        x = self.conv(x)
+        return x
 
 class SEUNet(nn.Module):
     def __init__(self, n_channels=3, n_classes=9):
@@ -137,7 +136,7 @@ class SEUNet(nn.Module):
             e2 = self.FAM2[i](e2)
         for i in range(6):
             e1 = self.FAM1[i](e1)
-            
+
         x = self.up3(e4, e3)
         x = self.up2(x , e2)
         x = self.up1(x , e1)
