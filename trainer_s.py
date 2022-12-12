2d4dca3a3e20c2e9ea18b256ec43c49cf4be3e06
saleknia <salekniaamirhossein@gmail.com>
2022-12-12 12:23:49 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -162,7 +162,7 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
             outputs = model(inputs)
             if type(outputs)==tuple:
                 loss_ce = ce_loss(outputs[0], targets.unsqueeze(dim=1)) + ce_loss(outputs[1], targets.unsqueeze(dim=1)) 
-                loss_dice = dice_loss(inputs=outputs, targets=targets) + dice_loss(inputs=outputs[1], targets=targets)
+                loss_dice = dice_loss(inputs=outputs[0], targets=targets) + dice_loss(inputs=outputs[1], targets=targets)
                 loss = loss_ce + loss_dice         
             else:
                 loss_ce = ce_loss(outputs, targets.unsqueeze(dim=1)) 
