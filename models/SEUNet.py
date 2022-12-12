033d8abf8f0dbfa0935f44362765b62ac1350a24
saleknia <salekniaamirhossein@gmail.com>
2022-12-12 11:35:16 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -295,6 +295,8 @@ class SEUNet(nn.Module):
                 out = self.final_conv3_1(out)
 
             if alpha==1:
+                e1, e2, e3, e4 = e1.detach(), e2.detach(), e3.detach(), e4.detach()
+                
                 for i in range(2):
                     e3 = self.FAM3_2[i](e3)
                 for i in range(4):
