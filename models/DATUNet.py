278adb1c699a67b878735ea75b5e5000f181695f
saleknia <salekniaamirhossein@gmail.com>
2022-12-13 21:39:57 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1223,10 +1223,10 @@ class DATUNet(nn.Module):
         self.norm_2 = LayerNormProxy(dim=96 )
         self.norm_1 = LayerNormProxy(dim=48 )
 
-        # self.fuse_layers = make_fuse_layers()
-        # self.fuse_act = nn.ReLU()
+        self.fuse_layers = make_fuse_layers()
+        self.fuse_act = nn.ReLU()
 
-        # self.AttentionBlock_3 = AttentionBlock(384) 
+        self.AttentionBlock_3 = AttentionBlock(384) 
         self.AttentionBlock_2 = AttentionBlock(192) 
         self.AttentionBlock_1 = AttentionBlock(96) 
         self.AttentionBlock_0 = AttentionBlock(48) 
@@ -1308,25 +1308,25 @@ class DATUNet(nn.Module):
 
         x0, x1, x2, x3 = self.norm_1(x0), self.norm_2(outputs[0]), self.norm_3(outputs[1]), self.norm_4(outputs[2])
 
-        # x = [x0, x1, x2, x3]
+        x = [x0, x1, x2, x3]
 
-        # x_fuse = []
-        # for i, fuse_outer in enumerate(self.fuse_layers):
-        #     y = x[0] if i == 0 else fuse_outer[0](x[0])
-        #     for j in range(1, 4):
-        #         if i == j:
-        #             y = y + x[j]
-        #         else:
-        #             y = y + fuse_outer[j](x[j])
-        #     x_fuse.append(self.fuse_act(y))
+        x_fuse = []
+        for i, fuse_outer in enumerate(self.fuse_layers):
+            y = x[0] if i == 0 else fuse_outer[0](x[0])
+            for j in range(1, 4):
+                if i == j:
+                    y = y + x[j]
+                else:
+                    y = y + fuse_outer[j](x[j])
+            x_fuse.append(self.fuse_act(y))
 
 
-        # x0, x1, x2, x3 = x[0] + x_fuse[0], x[1] + x_fuse[1], x[2] + x_fuse[2], x[3] + x_fuse[3]
+        x0, x1, x2, x3 = x[0] + x_fuse[0], x[1] + x_fuse[1], x[2] + x_fuse[2], x[3] + x_fuse[3]
 
         x0 = self.AttentionBlock_0(x0) + x0
         x1 = self.AttentionBlock_1(x1) + x1
         x2 = self.AttentionBlock_2(x2) + x2
-        # x3 = self.AttentionBlock_3(x3) + x3
+        x3 = self.AttentionBlock_3(x3) + x3
 
    
         x = self.up3(x3, x2) 
