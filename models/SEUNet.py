12b81cdb7f4ac5f8effa55d3c22592e75fa94fd0
saleknia <salekniaamirhossein@gmail.com>
2022-12-20 12:26:49 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -110,70 +110,10 @@ class UpBlock(nn.Module):
         x = self.conv(x)
         return x
 
-class PSPModule(nn.Module):
-    # In the original inmplementation they use precise RoI pooling 
-    # Instead of using adaptative average pooling
-    def __init__(self, in_channels, bin_sizes=[1, 2, 4, 6]):
-        super(PSPModule, self).__init__()
-        out_channels = in_channels // len(bin_sizes)
-        self.stages = nn.ModuleList([self._make_stages(in_channels, out_channels, b_s) 
-                                                        for b_s in bin_sizes])
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(in_channels+(out_channels * len(bin_sizes)), in_channels, 
-                                    kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(in_channels),
-            nn.ReLU(inplace=True),
-            nn.Dropout2d(0.1)
-        )
-
-    def _make_stages(self, in_channels, out_channels, bin_sz):
-        prior = nn.AdaptiveAvgPool2d(output_size=bin_sz)
-        conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
-        bn = nn.BatchNorm2d(out_channels)
-        relu = nn.ReLU(inplace=True)
-        return nn.Sequential(prior, conv, bn, relu)
-    
-    def forward(self, features):
-        h, w = features.size()[2], features.size()[3]
-        pyramids = [features]
-        pyramids.extend([F.interpolate(stage(features), size=(h, w), mode='bilinear', 
-                                        align_corners=True) for stage in self.stages])
-        output = self.bottleneck(torch.cat(pyramids, dim=1))
-        return output
-
-def up_and_add(x, y):
-    return F.interpolate(x, size=(y.size(2), y.size(3)), mode='bilinear', align_corners=True) + y
-
-class FPN_fuse(nn.Module):
-    def __init__(self, feature_channels=[64, 128, 256, 512], fpn_out=256):
-        super(FPN_fuse, self).__init__()
-        assert feature_channels[0] == fpn_out
-        self.conv1x1 = nn.ModuleList([nn.Conv2d(ft_size, fpn_out, kernel_size=1)
-                                    for ft_size in feature_channels[1:]])
-        self.smooth_conv =  nn.ModuleList([nn.Conv2d(fpn_out, fpn_out, kernel_size=3, padding=1)] 
-                                    * (len(feature_channels)-1))
-        self.conv_fusion = nn.Sequential(
-            nn.Conv2d(len(feature_channels)*fpn_out, fpn_out, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(fpn_out),
-            nn.ReLU(inplace=True)
-        )
-
-    def forward(self, features):
-        
-        features[1:] = [conv1x1(feature) for feature, conv1x1 in zip(features[1:], self.conv1x1)]
-        P = [up_and_add(features[i], features[i-1]) for i in reversed(range(1, len(features)))]
-        P = [smooth_conv(x) for smooth_conv, x in zip(self.smooth_conv, P)]
-        P = list(reversed(P))
-        P.append(features[-1]) #P = [P1, P2, P3, P4]
-        H, W = P[0].size(2), P[0].size(3)
-        P[1:] = [F.interpolate(feature, size=(H, W), mode='bilinear', align_corners=True) for feature in P[1:]]
-
-        x = self.conv_fusion(torch.cat((P), dim=1))
-        return x
 
 
 class SEUNet(nn.Module):
-    def __init__(self, n_channels=3, n_classes=9):
+    def __init__(self, n_channels=1, n_classes=9):
         '''
         n_channels : number of channels of the input.
                         By default 3, because we have RGB images
@@ -195,69 +135,16 @@ class SEUNet(nn.Module):
         self.encoder3 = resnet.layer3
         self.encoder4 = resnet.layer4
 
-        #####################################################################
-        #####################################################################
-        #####################################################################
+        self.up3 = UpBlock(512, 256, nb_Conv=2)
+        self.up2 = UpBlock(256, 128, nb_Conv=2)
+        self.up1 = UpBlock(128, 64 , nb_Conv=2)
 
-        self.FAMBlock1_1 = FAMBlock(channels=64)
-        self.FAMBlock2_1 = FAMBlock(channels=128)
-        self.FAMBlock3_1 = FAMBlock(channels=256)
-        self.FAM1_1 = nn.ModuleList([self.FAMBlock1_1 for i in range(6)])
-        self.FAM2_1 = nn.ModuleList([self.FAMBlock2_1 for i in range(4)])
-        self.FAM3_1 = nn.ModuleList([self.FAMBlock3_1 for i in range(2)])
+        self.final_conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
+        self.final_relu1 = nn.ReLU(inplace=True)
+        self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.final_relu2 = nn.ReLU(inplace=True)
+        self.final_conv3 = nn.Conv2d(32, n_classes, 3, padding=1)
 
-        self.up3_1 = UpBlock(512, 256, nb_Conv=2)
-        self.up2_1 = UpBlock(256, 128, nb_Conv=2)
-        self.up1_1 = UpBlock(128, 64 , nb_Conv=2)
-
-        self.final_conv1_1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
-        self.final_relu1_1 = nn.ReLU(inplace=True)
-        self.final_conv2_1 = nn.Conv2d(32, 32, 3, padding=1)
-        self.final_relu2_1 = nn.ReLU(inplace=True)
-        self.final_conv3_1 = nn.Conv2d(32, n_classes, 3, padding=1)
-
-        #####################################################################
-        #####################################################################
-        #####################################################################
-
-        self.FAMBlock1_2 = FAMBlock(channels=64)
-        self.FAMBlock2_2 = FAMBlock(channels=128)
-        self.FAMBlock3_2 = FAMBlock(channels=256)
-        self.FAM1_2 = nn.ModuleList([self.FAMBlock1_2 for i in range(6)])
-        self.FAM2_2 = nn.ModuleList([self.FAMBlock2_2 for i in range(4)])
-        self.FAM3_2 = nn.ModuleList([self.FAMBlock3_2 for i in range(2)])
-
-        filters = [64, 128, 256, 512]
-        self.decoder3 = DecoderBottleneckLayer(filters[3], filters[2])
-        self.decoder2 = DecoderBottleneckLayer(filters[2], filters[1])
-        self.decoder1 = DecoderBottleneckLayer(filters[1], filters[0])
-
-        self.final_conv1_2 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
-        self.final_relu1_2 = nn.ReLU(inplace=True)
-        self.final_conv2_2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.final_relu2_2 = nn.ReLU(inplace=True)
-        self.final_conv3_2 = nn.Conv2d(32, n_classes, 3, padding=1)
-
-        #####################################################################
-        #####################################################################
-        #####################################################################
-
-        # self.FAMBlock1_3 = FAMBlock(channels=64)
-        # self.FAMBlock2_3 = FAMBlock(channels=128)
-        # self.FAMBlock3_3 = FAMBlock(channels=256)
-        # self.FAM1_3 = nn.ModuleList([self.FAMBlock1_3 for i in range(6)])
-        # self.FAM2_3 = nn.ModuleList([self.FAMBlock2_3 for i in range(4)])
-        # self.FAM3_3 = nn.ModuleList([self.FAMBlock3_3 for i in range(2)])
-
-        # feature_channels = [64, 128, 256, 512]
-        # self.PPN = PSPModule(feature_channels[-1])
-        # self.FPN = FPN_fuse(feature_channels, fpn_out=64)
-
-        # self.final_conv1_3 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
-        # self.final_relu1_3 = nn.ReLU(inplace=True)
-        # self.final_conv2_3 = nn.Conv2d(32, 32, 3, padding=1)
-        # self.final_relu2_3 = nn.ReLU(inplace=True)
-        # self.final_conv3_3 = nn.Conv2d(32, n_classes, 3, padding=1)
 
     def forward(self, x):
         # Question here
@@ -273,45 +160,15 @@ class SEUNet(nn.Module):
         e4 = self.encoder4(e3)
 
 
-        for i in range(2):
-            a3 = self.FAM3_1[i](e3)
-        for i in range(4):
-            a2 = self.FAM2_1[i](e2)
-        for i in range(6):
-            a1 = self.FAM1_1[i](e1)
+        x = self.up3(e4, e3)
+        x = self.up2(x , e2)
+        x = self.up1(x , e1)
 
-        a = self.up3_1(e4, a3)
-        a = self.up2_1(a , a2)
-        a = self.up1_1(a , a1)
-
-        out_1 = self.final_conv1_1(a)
-        out_1 = self.final_relu1_1(out_1)
-        out_1 = self.final_conv2_1(out_1)
-        out_1 = self.final_relu2_1(out_1)
-        out_1 = self.final_conv3_1(out_1)
-
-        # q1, q2, q3, q4 = e1.clone().detach(), e2.clone().detach(), e3.clone().detach(), e4.clone().detach()
-        q1, q2, q3, q4 = e1, e2, e3, e4
-
-
-        for i in range(2):
-            b3 = self.FAM3_2[i](q3)
-        for i in range(4):
-            b2 = self.FAM2_2[i](q2)
-        for i in range(6):
-            b1 = self.FAM1_2[i](q1)
-
-        d4 = self.decoder3(q4) + b3
-        d3 = self.decoder2(d4) + b2
-        d2 = self.decoder1(d3) + b1
-
-        out_2 = self.final_conv1_2(d2)
-        out_2 = self.final_relu1_2(out_2)
-        out_2 = self.final_conv2_2(out_2)
-        out_2 = self.final_relu2_2(out_2)
-        out_2 = self.final_conv3_2(out_2)
-
-        out = (out_1, out_2)
+        out = self.final_conv1(x)
+        out = self.final_relu1(out)
+        out = self.final_conv2(out)
+        out = self.final_relu2(out)
+        out = self.final_conv3(out)
 
         return out
 
