a10eb5f960b82c4a65f64dca3cb4e6b1eaecbf93
saleknia <salekniaamirhossein@gmail.com>
2022-12-20 12:44:11 +0000

    Commit Process

--- models/U.py
+++ models/U.py
@@ -174,6 +174,45 @@ class U(nn.Module):
         
         return x
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class Conv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride, padding, dilation=(1, 1), groups=1, bn_acti=False, bias=False):
+        super().__init__()
+        
+        self.bn_acti = bn_acti
+        
+        self.conv = nn.Conv2d(nIn, nOut, kernel_size = kSize,
+                              stride=stride, padding=padding,
+                              dilation=dilation,groups=groups,bias=bias)
+        
+        if self.bn_acti:
+            self.bn_relu = BNPReLU(nOut)
+            
+    def forward(self, input):
+        output = self.conv(input)
+
+        if self.bn_acti:
+            output = self.bn_relu(output)
+
+        return output  
+    
+    
+class BNPReLU(nn.Module):
+    def __init__(self, nIn):
+        super().__init__()
+        self.bn = nn.BatchNorm2d(nIn, eps=1e-3)
+        self.acti = nn.PReLU(nIn)
+
+    def forward(self, input):
+        output = self.bn(input)
+        output = self.acti(output)
+        
+        return output
 
 class CFPModule(nn.Module):
     def __init__(self, nIn, d=1, KSize=3,dkSize=3):
