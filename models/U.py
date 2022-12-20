4e6f59bfc3f7b64eb97bdf3dc954ace83b30137e
saleknia <salekniaamirhossein@gmail.com>
2022-12-20 11:19:06 +0000

    Commit Process

--- models/U.py
+++ models/U.py
@@ -8,29 +8,38 @@ reduce_factor = 4
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Sequential(
-                        nn.Conv2d(in_channels                , in_channels//reduce_factor , kernel_size=1, padding=0),
-                        nn.Conv2d(in_channels//reduce_factor , mid_channels//reduce_factor, kernel_size=3, padding=1),
-                        nn.Conv2d(mid_channels//reduce_factor, mid_channels               , kernel_size=1, padding=0),
-                        ),
-            nn.BatchNorm2d(mid_channels),
+        
+        self.reduction = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels//reduce_factor, kernel_size=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_channels//reduce_factor),
             nn.ReLU(inplace=True),
-            nn.Sequential(
-                        nn.Conv2d(mid_channels                , mid_channels//reduce_factor , kernel_size=1, padding=0),
-                        nn.Conv2d(mid_channels//reduce_factor , out_channels//reduce_factor, kernel_size=3, padding=1),
-                        nn.Conv2d(out_channels//reduce_factor , out_channels               , kernel_size=1, padding=0),
-                        ),            
+        )
+
+        self.expand = nn.Sequential(
+            nn.Conv2d(out_channels//reduce_factor, out_channels, kernel_size=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_channels//reduce_factor),
+            nn.ReLU(inplace=True),
+        )
+
+        in_channels = in_channels // reduce_factor
+        out_channels = out_channels // reduce_factor
+
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
-        return self.double_conv(x)
+        x = self.reduction(x)
+        x = self.double_conv(x)
+        x = self.expand(x)
+        return x
 
 class DoubleConv_(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
