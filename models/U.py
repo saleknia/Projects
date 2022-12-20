9eac29f728e313a4a830b13876af1de533a9a78c
saleknia <salekniaamirhossein@gmail.com>
2022-12-20 10:39:04 +0000

    Commit Process

--- models/U.py
+++ models/U.py
@@ -102,7 +102,7 @@ class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.up = DecoderBottleneckLayer(in_channels=in_channels, out_channels=in_channels//2)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
