c43f1648fc23acba60608b30de0ff644592f3d39
saleknia <salekniaamirhossein@gmail.com>
2022-12-22 19:51:37 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1114,8 +1114,8 @@ class DATUNet(nn.Module):
                     y = y + fuse_outer[j](x[j])
             x_fuse.append(self.fuse_act(y))
 
-        x1, x2, x3, x4 = x[0] + self.CPF_1(x_fuse[0]) , x[1] + self.CPF_2(x_fuse[1]) , x[2] + self.CPF_3(x_fuse[2]) , x[3] + self.CPF_4(x_fuse[3])
-        # x1, x2, x3, x4 = self.CPF_1(x_fuse[0]) , self.CPF_2(x_fuse[1]) , self.CPF_3(x_fuse[2]) , x_fuse[3] 
+        # x1, x2, x3, x4 = x[0] + self.CPF_1(x_fuse[0]) , x[1] + self.CPF_2(x_fuse[1]) , x[2] + self.CPF_3(x_fuse[2]) , x[3] + self.CPF_4(x_fuse[3])
+        x1, x2, x3, x4 = self.CPF_1(x_fuse[0]) , self.CPF_2(x_fuse[1]) , self.CPF_3(x_fuse[2]) , self.CPF_4(x_fuse[3])
 
         x3 = self.up3(x4, x3) 
         x2 = self.up2(x3, x2) 
