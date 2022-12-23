65ab89a611cd608517fc82e5ea3cc64f07e79d07
saleknia <salekniaamirhossein@gmail.com>
2022-12-23 14:10:31 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1206,7 +1206,7 @@ class DATUNet(nn.Module):
         x2 = self.up2(x3, x2) 
         x1 = self.up1(x2, x1) 
 
-        x = self.final_conv1(x)
+        x = self.final_conv1(x1)
         x = self.final_relu1(x)
         x = self.final_conv2(x)
         x = self.final_relu2(x)
