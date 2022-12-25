8f4403a56565feabdf906d9a89d49c292fd4aba1
saleknia <salekniaamirhossein@gmail.com>
2022-12-25 11:10:26 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1239,7 +1239,7 @@ class DATUNet(nn.Module):
         # self.fuse_layers = make_fuse_layers()
         # self.fuse_act = nn.ReLU()
 
-        # self.skip = make_stage()
+        self.skip = make_stage()
 
         # self.deformable_head = make_deformable_head()
         # self.project = nn.Sequential(
@@ -1312,8 +1312,8 @@ class DATUNet(nn.Module):
         x3 = self.norm_3(outputs[1]) 
         x4 = self.norm_4(outputs[2]) 
 
-        # x = [x2, x3, x4]
-        # x = self.skip(x)
+        x = [x2, x3, x4]
+        x = self.skip(x)
 
         # x_fuse = []
         # num_branches = 3
@@ -1329,7 +1329,7 @@ class DATUNet(nn.Module):
 
         # # x1, x2, x3, x4 = x1 + x_fuse[0], x2 + x_fuse[1], x3 + x_fuse[2], x4 + x_fuse[3]
 
-        # x2, x3, x4 = x[0], x[1], x[2]
+        x2, x3, x4 = x[0], x[1], x[2]
 
 
         x3 = self.up3(x4, x3) 
