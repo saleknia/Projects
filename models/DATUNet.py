2467d38b6fff49cfcb8d9376f6d45323b38e9310
saleknia <salekniaamirhossein@gmail.com>
2022-12-30 09:56:53 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -622,12 +622,11 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
-        self.coatt = CCA(F_g=in_channels//2, F_x=in_channels//2)
     def forward(self, x, skip_x):
-        x = self.up(x)
-        skip_x = self.coatt(g=x, x=skip_x)
+        up = self.up(x)
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        x = self.conv(x) 
+        x = self.conv(x)
+        x = x + up + skip_x 
         return x
 
 
