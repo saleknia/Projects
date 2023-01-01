15254a28292a613b6873eda4623f11682237ba67
saleknia <salekniaamirhossein@gmail.com>
2023-01-01 10:35:06 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1295,16 +1295,16 @@ class DATUNet(nn.Module):
         self.up2 = UpBlock(192, 96 , nb_Conv=2)
         self.up1 = UpBlock(96 , 48 , nb_Conv=2)
 
-        self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
-        self.final_relu1 = nn.ReLU(inplace=True)
-        self.final_conv2 = nn.Conv2d(48, 1, 1, padding=0)
-
-        # self.DilatedParllelResidualBlockB = DilatedParllelResidualBlockB(nIn=48, nOut=48)
         # self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
         # self.final_relu1 = nn.ReLU(inplace=True)
-        # self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
-        # self.final_relu2 = nn.ReLU(inplace=True)
-        # self.final_conv3 = nn.Conv2d(24, n_classes, 3, padding=1)
+        # self.final_conv2 = nn.Conv2d(48, 1, 1, padding=0)
+
+        self.DilatedParllelResidualBlockB = DilatedParllelResidualBlockB(nIn=48, nOut=48)
+        self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
+        self.final_relu1 = nn.ReLU(inplace=True)
+        self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
+        self.final_relu2 = nn.ReLU(inplace=True)
+        self.final_conv3 = nn.Conv2d(24, n_classes, 3, padding=1)
 
     def forward(self, x):
         # # Question here
@@ -1355,16 +1355,16 @@ class DATUNet(nn.Module):
         x2 = self.up2(x3, x2) 
         x1 = self.up1(x2, x1) 
 
-        x = self.final_conv1(x1)
-        x = self.final_relu1(x)
-        x = self.final_conv2(x)
-
-        # x = self.DilatedParllelResidualBlockB(x1)
-        # x = self.final_conv1(x)
+        # x = self.final_conv1(x1)
         # x = self.final_relu1(x)
         # x = self.final_conv2(x)
-        # x = self.final_relu2(x)
-        # x = self.final_conv3(x)
+
+        x = self.DilatedParllelResidualBlockB(x1)
+        x = self.final_conv1(x)
+        x = self.final_relu1(x)
+        x = self.final_conv2(x)
+        x = self.final_relu2(x)
+        x = self.final_conv3(x)
 
         return x
 
