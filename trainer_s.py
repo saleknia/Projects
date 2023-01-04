3cfc6b2882f668fb0ae33fc4bce4a17ce26e2b5f
saleknia <salekniaamirhossein@gmail.com>
2023-01-04 06:35:48 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -165,7 +165,7 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
         with torch.autocast(device_type=device, dtype=torch.float16):
             outputs = model(inputs)
             if type(outputs)==tuple:
-                loss_ce = ce_loss(outputs[0], targets.unsqueeze(dim=1)) + ce_loss(outputs[1], boundary.unsqueeze(dim=1))
+                loss_ce = ce_loss(outputs[0], targets.unsqueeze(dim=1)) #+ ce_loss(outputs[1], boundary.unsqueeze(dim=1))
                 loss_dice = dice_loss(inputs=outputs[0], targets=targets) + dice_loss(inputs=outputs[1], targets=boundary)
                 loss = loss_ce + loss_dice         
             else:
