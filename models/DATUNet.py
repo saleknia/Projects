985140b527be34f362d42f3c42347813b2c978c6
saleknia <salekniaamirhossein@gmail.com>
2023-01-04 15:04:37 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -644,6 +644,88 @@ class AttentionBlock(nn.Module):
         out = skip_connection * psi
         return out
 
+class ConvBnRelu(nn.Module):
+    def __init__(self, in_planes, out_planes, ksize, stride, pad, dilation=1,
+                 groups=1, has_bn=True, norm_layer=nn.BatchNorm2d,
+                 has_relu=True, inplace=True, has_bias=False):
+        super(ConvBnRelu, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=ksize,
+                              stride=stride, padding=pad,
+                              dilation=dilation, groups=groups, bias=has_bias)
+        self.has_bn = has_bn
+        if self.has_bn:
+            self.bn = nn.BatchNorm2d(out_planes)
+        self.has_relu = has_relu
+        if self.has_relu:
+            self.relu = nn.ReLU(inplace=inplace)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.has_bn:
+            x = self.bn(x)
+        if self.has_relu:
+            x = self.relu(x)
+
+        return x
+
+class SAPblock(nn.Module):
+    def __init__(self, in_channels):
+        super(SAPblock, self).__init__()
+        self.conv3x3=nn.Conv2d(in_channels=in_channels, out_channels=in_channels,dilation=1,kernel_size=3, padding=1)
+        
+        self.bn=nn.ModuleList([nn.BatchNorm2d(in_channels),nn.BatchNorm2d(in_channels),nn.BatchNorm2d(in_channels)]) 
+        self.conv1x1=nn.ModuleList([nn.Conv2d(in_channels=2*in_channels, out_channels=in_channels,dilation=1,kernel_size=1, padding=0),
+                                    nn.Conv2d(in_channels=2*in_channels, out_channels=in_channels,dilation=1,kernel_size=1, padding=0)])
+        self.conv3x3_1=nn.ModuleList([nn.Conv2d(in_channels=in_channels, out_channels=in_channels//2,dilation=1,kernel_size=3, padding=1),
+                                      nn.Conv2d(in_channels=in_channels, out_channels=in_channels//2,dilation=1,kernel_size=3, padding=1)])
+        self.conv3x3_2=nn.ModuleList([nn.Conv2d(in_channels=in_channels//2, out_channels=2,dilation=1,kernel_size=3, padding=1),
+                                      nn.Conv2d(in_channels=in_channels//2, out_channels=2,dilation=1,kernel_size=3, padding=1)])
+        self.conv_last=ConvBnRelu(in_planes=in_channels,out_planes=in_channels,ksize=1,stride=1,pad=0,dilation=1)
+
+
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+    
+        self.relu=nn.ReLU(inplace=True)
+
+    def forward(self, x):
+
+        x_size= x.size()
+
+        branches_1=self.conv3x3(x)
+        branches_1=self.bn[0](branches_1)
+
+        branches_2=F.conv2d(x,self.conv3x3.weight,padding=2,dilation=2)#share weight
+        branches_2=self.bn[1](branches_2)
+
+        branches_3=F.conv2d(x,self.conv3x3.weight,padding=4,dilation=4)#share weight
+        branches_3=self.bn[2](branches_3)
+
+        feat=torch.cat([branches_1,branches_2],dim=1)
+        feat=self.relu(self.conv1x1[0](feat))
+        feat=self.relu(self.conv3x3_1[0](feat))
+        att=self.conv3x3_2[0](feat)
+        att = F.softmax(att, dim=1)
+        
+        att_1=att[:,0,:,:].unsqueeze(1)
+        att_2=att[:,1,:,:].unsqueeze(1)
+
+        fusion_1_2=att_1*branches_1+att_2*branches_2
+
+        feat1=torch.cat([fusion_1_2,branches_3],dim=1)
+        feat1=self.relu(self.conv1x1[0](feat1))
+        feat1=self.relu(self.conv3x3_1[0](feat1))
+        att1=self.conv3x3_2[0](feat1)
+        att1 = F.softmax(att1, dim=1)
+        
+        att_1_2=att1[:,0,:,:].unsqueeze(1)
+        att_3=att1[:,1,:,:].unsqueeze(1)
+
+        ax=self.relu(self.gamma*(att_1_2*fusion_1_2+att_3*branches_3)+(1-self.gamma)*x)
+        ax=self.conv_last(ax)
+
+        return ax
+
 class UpBlock(nn.Module):
     """Upscaling then conv"""
 
@@ -651,11 +733,13 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels//1, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
+        self.SAPblock = SAPblock(in_channels=in_channels//2)
     
     def forward(self, x, skip_x):
         x = self.up(x)
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
+        x = self.SAPblock(x)
         return x
 
 # class UpBlock(nn.Module):
