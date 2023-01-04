eb2351dff6649bd31a497f2350d26c3cc06ca1ad
saleknia <salekniaamirhossein@gmail.com>
2023-01-04 12:57:32 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -644,33 +644,33 @@ class AttentionBlock(nn.Module):
         out = skip_connection * psi
         return out
 
-# class UpBlock(nn.Module):
-#     """Upscaling then conv"""
-
-#     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
-#         super(UpBlock, self).__init__()
-#         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-#         self.conv = _make_nConv(in_channels=in_channels//1, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
-    
-#     def forward(self, x, skip_x):
-#         x = self.up(x)
-#         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-#         x = self.conv(x)
-#         return x
-
 class UpBlock(nn.Module):
     """Upscaling then conv"""
 
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-        self.sigmoid = nn.Sigmoid()
+        self.conv = _make_nConv(in_channels=in_channels//1, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
     
     def forward(self, x, skip_x):
         x = self.up(x)
-        att = 1.0 - self.sigmoid(x)
-        x = x + (att * skip_x)
+        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        x = self.conv(x)
         return x
+
+# class UpBlock(nn.Module):
+#     """Upscaling then conv"""
+
+#     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
+#         super(UpBlock, self).__init__()
+#         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+#         self.sigmoid = nn.Sigmoid()
+    
+#     def forward(self, x, skip_x):
+#         x = self.up(x)
+#         att = 1.0 - self.sigmoid(x)
+#         x = x + (att * skip_x)
+#         return x
 
 
 class Conv(nn.Module):
--- requirements.py
+++ requirements.py
@@ -25,6 +25,10 @@ os.system('pip install wget')
 os.system('pip install tensorboardX==2.4')
 os.system('pip install SimpleITK==2.1.1')
 os.system('pip install MedPy==0.4.0')
+os.system('pip install paddle')
+os.system('pip install opencv-python')
+os.system('pip install paddleseg')
+
 # os.system('pip install --upgrade --force-reinstall --no-deps albumentations')
 
 
