4a43b947877202804066d837f28d2f210e4fda39
saleknia <salekniaamirhossein@gmail.com>
2023-01-04 13:12:11 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -169,12 +169,12 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
             if type(outputs)==tuple:
                 loss_ce = ce_loss(outputs[0], targets.unsqueeze(dim=1)) #+ ce_loss(outputs[1], boundary.unsqueeze(dim=1))
                 loss_dice = dice_loss(inputs=outputs[0], targets=targets) + 0.1 * dice_loss(inputs=outputs[1], targets=boundary)
-                loss_scl = scl_loss(logits=torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1))), labels=targets)
                 loss = loss_ce + loss_dice + loss_scl
             else:
                 loss_ce = ce_loss(outputs, targets.unsqueeze(dim=1)) 
                 loss_dice = dice_loss(inputs=outputs, targets=targets)
-                loss = loss_ce + loss_dice
+                loss_scl = scl_loss(logits=torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1))), labels=targets)
+                loss = loss_ce + loss_dice + loss_scl
 
 
         scaler.scale(loss).backward()
