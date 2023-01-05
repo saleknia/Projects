45b8274c8ba371616325aaa582d2440d9f576214
saleknia <salekniaamirhossein@gmail.com>
2023-01-05 18:51:10 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -816,13 +816,13 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-        self.conv = _make_nConv(in_channels=in_channels//1, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
+        self.conv = _make_nConv(in_channels=in_channels//2, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
         self.att = AttentionBlock(F_g=out_channels, F_l=out_channels, n_coefficients=out_channels)
     
     def forward(self, x, skip_x, II):
-        x = self.up(x)
+        x = self.up(x) + skip_x
         x = x + self.att(x, II)
-        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
         return x
 
