bc79089da11e74d59bc9e512570058d63132562f
saleknia <salekniaamirhossein@gmail.com>
2023-01-08 11:30:49 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -833,7 +833,7 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
         self.BiFusion_block = BiFusion_block(ch_1=in_channels // 2, ch_2=in_channels // 2, r_2=4, ch_int=in_channels // 2, ch_out=in_channels // 2)
-        self.conv = _make_nConv(in_channels=in_channels //2, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
+        # self.conv = _make_nConv(in_channels=in_channels //2, out_channels=in_channels//2, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
     
     def forward(self, x, skip_x):
         x = self.up(x)
