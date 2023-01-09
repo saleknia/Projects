82c22dfdd38df7767415054d41fe9770ae05272f
saleknia <salekniaamirhossein@gmail.com>
2023-01-09 13:51:15 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1509,7 +1509,7 @@ class DATUNet(nn.Module):
         x = [x1, x2, x3, x4]
         x = self.combine(x)
 
-        # x1, x2, x3, x4 = x[0], x[1], x[2], x[3]
+        x1, x2, x3, x4 = x[0], x[1], x[2], x[3]
 
         # x = [x1, x2, x3, x4]
         # x_fuse = []
