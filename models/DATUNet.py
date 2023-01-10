8b96ff66d5e714ef8f336f3d34b18549e80b353b
saleknia <salekniaamirhossein@gmail.com>
2023-01-10 09:33:29 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1156,6 +1156,53 @@ def get_CTranS_config():
     config.n_classes = 1
     return config
 
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import init
+
+class ParallelPolarizedSelfAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*x
+        out=spatial_out+channel_out
+        return out
+
+
 
 class PEE(nn.Module):
 
@@ -1299,7 +1346,12 @@ class DATUNet(nn.Module):
         self.conv_1 = ConvBatchNorm(in_channels=48 , out_channels=48, kernel_size=1, padding=0)
         self.conv_2 = ConvBatchNorm(in_channels=96 , out_channels=48, kernel_size=1, padding=0)
         self.conv_3 = ConvBatchNorm(in_channels=192, out_channels=48, kernel_size=1, padding=0)
-        self.conv_4 = ConvBatchNorm(in_channels=384, out_channels=48, kernel_size=1, padding=0)
+        self.conv_4 = ConvBatchNorm(in_channels=384, out_channels=48, kernel_size=1, padding=0)\
+
+        self.PSA_1 = ParallelPolarizedSelfAttention(48)
+        self.PSA_2 = ParallelPolarizedSelfAttention(48)        
+        self.PSA_3 = ParallelPolarizedSelfAttention(48)        
+        self.PSA_4 = ParallelPolarizedSelfAttention(48)
 
         self.up3 = UpBlock(48, 48, nb_Conv=2)
         self.up2 = UpBlock(48, 48, nb_Conv=2)
@@ -1346,10 +1398,10 @@ class DATUNet(nn.Module):
 
         x1, x2, x3, x4 = x1 + (x_fuse[0]), x2 + (x_fuse[1]) , x3 + (x_fuse[2]), x4 + (x_fuse[3])
 
-        x1 = self.conv_1(x1)
-        x2 = self.conv_2(x2)
-        x3 = self.conv_3(x3)
-        x4 = self.conv_4(x4)
+        x1 = self.PSA_1(self.conv_1(x1))
+        x2 = self.PSA_2(self.conv_2(x2))
+        x3 = self.PSA_3(self.conv_3(x3))
+        x4 = self.PSA_4(self.conv_4(x4))
 
         x3 = self.up3(x4, x3) 
         x2 = self.up2(x3, x2) 
