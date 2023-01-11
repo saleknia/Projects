3f36b5794db3f144c1531019db1e016a55888f79
saleknia <salekniaamirhossein@gmail.com>
2023-01-11 17:10:15 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -1329,19 +1329,11 @@ class DATUNet(nn.Module):
         self.up2 = UpBlock(192, 96 , nb_Conv=2)
         self.up1 = UpBlock(96 , 48, nb_Conv=2)
 
-        self.pool_3  = torch.nn.AvgPool2d(3 , stride=1, padding=1, ceil_mode=False, count_include_pad=True, divisor_override=None)
-        self.pool_5  = torch.nn.AvgPool2d(5 , stride=1, padding=2, ceil_mode=False, count_include_pad=True, divisor_override=None)
-        self.pool_7  = torch.nn.AvgPool2d(7 , stride=1, padding=3, ceil_mode=False, count_include_pad=True, divisor_override=None)
-        self.pool_9  = torch.nn.AvgPool2d(9 , stride=1, padding=4, ceil_mode=False, count_include_pad=True, divisor_override=None)
-
-        self.final_conv  = nn.Conv2d(48, n_classes, 1, padding=0)
-        self.final_up = nn.Upsample(scale_factor=2)
-
-        # self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
-        # self.final_relu1 = nn.ReLU(inplace=True)
-        # self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
-        # self.final_relu2 = nn.ReLU(inplace=True)
-        # self.final_conv  = nn.Conv2d(24, n_classes, 3, padding=1)
+        self.final_conv1 = nn.ConvTranspose2d(48, 48, 4, 2, 1)
+        self.final_relu1 = nn.ReLU(inplace=True)
+        self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
+        self.final_relu2 = nn.ReLU(inplace=True)
+        self.final_conv  = nn.Conv2d(24, n_classes, 3, padding=1)
 
     def forward(self, x):
         # # Question here
@@ -1384,14 +1376,11 @@ class DATUNet(nn.Module):
         x2 = self.up2(x3, x2) 
         x1 = self.up1(x2, x1) 
 
-        x = x1 + self.pool_3(x1) + self.pool_5(x1) + self.pool_7(x1) + self.pool_9(x1)
 
-        # x = self.final_conv1(x1)
-        # x = self.final_relu1(x)
-        # x = self.final_conv2(x)
-        # x = self.final_relu2(x)
-        x = self.final_conv(x)
-        x = self.final_up(x)
+        x = self.final_conv1(x1)
+        x = self.final_relu1(x)
+        x = self.final_conv2(x)
+        x = self.final_relu2(x)
 
         return x
 
