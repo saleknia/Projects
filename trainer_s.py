9f01c930696b6705becb5dc2b38515b68f663e39
saleknia <salekniaamirhossein@gmail.com>
2023-01-13 16:46:59 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -218,7 +218,8 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
                 loss_att = 0.0
                 loss = loss_ce + loss_dice 
             else:
-                loss_ce = ce_loss(outputs, targets.unsqueeze(dim=1), weight=weight)
+                # loss_ce = ce_loss(outputs, targets.unsqueeze(dim=1), weight=weight)
+                loss_ce = nn.functional.binary_cross_entropy_with_logits(outputs, targets.unsqueeze(dim=1), weight=weight, reduction='mean')
                 loss_dice = dice_loss(inputs=outputs, targets=targets)
                 loss_att = 0.0
                 loss = loss_ce + loss_dice + loss_att
