93fc651596907d734db116317d7aae91337601e3
saleknia <salekniaamirhossein@gmail.com>
2023-01-14 14:50:58 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -916,58 +916,6 @@ class BasicConv2d(nn.Module):
         x = self.relu(x)
         return x
 
-class MLP(nn.Module):
-    """
-    Linear Embedding
-    """
-    def __init__(self, input_dim=2048, embed_dim=768):
-        super().__init__()
-        self.proj = nn.Linear(input_dim, embed_dim)
-
-    def forward(self, x):
-        x = x.flatten(2).transpose(1, 2)
-        x = self.proj(x)
-        return x
-
-
-class SegFormerHead(nn.Module):
-    """
-    SegFormer: Simple and Efficient Design for Semantic Segmentation with Transformers
-    """
-    def __init__(self):
-        super(SegFormerHead, self).__init__()
-
-        c1_in_channels, c2_in_channels, c3_in_channels = 48, 96, 192
-
-        embedding_dim = 48
-
-        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
-        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
-        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
-
-        self.up3 = nn.Upsample(scale_factor=4)
-        self.up2 = nn.Upsample(scale_factor=2)
-
-        self.linear_fuse = BasicConv2d(embedding_dim*3, embedding_dim, 1)
-
-    def forward(self, x1, x2, x3):
-        c1, c2, c3 = x1, x2, x3
-
-        ############## MLP decoder on C1-C4 ###########
-        n, _, h, w = c3.shape
-
-        _c3 = self.linear_c3(c3).permute(0,2,1).reshape(n, -1, c3.shape[2], c3.shape[3])
-        _c3 = self.up3(_c3)
-
-        _c2 = self.linear_c2(c2).permute(0,2,1).reshape(n, -1, c2.shape[2], c2.shape[3])
-        _c2 = self.up2(_c2)
-
-        _c1 = self.linear_c1(c1).permute(0,2,1).reshape(n, -1, c1.shape[2], c1.shape[3])
-
-        _c = self.linear_fuse(torch.cat([_c3, _c2, _c1], dim=1))
-
-        return _c
-
 
 class _ASPPModule(nn.Module):
     def __init__(self, inplanes, planes, kernel_size, padding, dilation):
@@ -992,7 +940,7 @@ class ASPP(nn.Module):
         self.aspp4 = _ASPPModule(inplanes, inplanes, 3, padding=7, 7)
 
         self.conv1 = nn.Conv2d(inplanes*4, inplanes, 1, bias=False)
-        self.bn1 = BatchNorm(inplanes)
+        self.bn1 = nn.BatchNorm2dBatchNorm(inplanes)
         self.relu = nn.ReLU()
 
     def forward(self, x):
