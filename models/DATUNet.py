e55e47ebcd790affd80d85aa5568cd6d327b250b
saleknia <salekniaamirhossein@gmail.com>
2023-01-16 17:05:05 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -617,12 +617,13 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
+        # self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
-        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        x = self.conv(x)
+        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        # x = self.conv(x)
+        x = x + skip_x
         return x
 
 class Up(nn.Module):
@@ -669,8 +670,9 @@ class DownBlock(nn.Module):
         self.nConvs = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=nb_Conv, activation=activation, dilation=1, padding=1)
 
     def forward(self, x):
+        out = self.nConvs(out)
         out = self.maxpool(x)
-        return self.nConvs(out)
+        return out
 
 def make_stage(multi_scale_output=True):
     num_modules = 1
