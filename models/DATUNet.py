ae593f554d236a5a07ebe6ae4571b1f052da57d1
saleknia <salekniaamirhossein@gmail.com>
2023-01-19 09:17:02 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -582,14 +582,15 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.patchSize = config.patch_size
         self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
+        # self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
         self.mtc = ChannelTransformer(config, vis=False, img_size=img_size, channel_num=in_channels//2, patchSize=self.patchSize)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
         x, skip_x = self.mtc(x, skip_x)
-        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        x = self.conv(x)
+        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        # x = self.conv(x)
+        x = x + skip_x
         return x
 
 
