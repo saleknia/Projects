e327f66df73d66c3c3f90026b9f222b8aac796ed
saleknia <salekniaamirhossein@gmail.com>
2023-01-20 12:02:03 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -582,8 +582,10 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
+        self.aspp = ASPP(in_channels//2)
     
     def forward(self, x, skip_x):
+        skip_x = self.aspp(skip_x)
         x = self.up(x) 
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
@@ -843,101 +845,6 @@ def get_CTranS_config(num_channels, patch_size):
     config.base_channel = num_channels # base channel of U-Net
     return config
 
-class RefUnet(nn.Module):
-    def __init__(self,in_ch,inc_ch):
-        super(RefUnet, self).__init__()
-
-        self.conv0 = nn.Conv2d(in_ch,inc_ch,3,padding=1)
-
-        self.conv1 = nn.Conv2d(inc_ch,64,3,padding=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        self.pool1 = nn.MaxPool2d(2,2,ceil_mode=True)
-
-        self.conv2 = nn.Conv2d(64,64,3,padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.relu2 = nn.ReLU(inplace=True)
-
-        self.pool2 = nn.MaxPool2d(2,2,ceil_mode=True)
-
-        self.conv3 = nn.Conv2d(64,64,3,padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU(inplace=True)
-
-        self.pool3 = nn.MaxPool2d(2,2,ceil_mode=True)
-
-        self.conv4 = nn.Conv2d(64,64,3,padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.relu4 = nn.ReLU(inplace=True)
-
-        self.pool4 = nn.MaxPool2d(2,2,ceil_mode=True)
-
-        #####
-
-        self.conv5 = nn.Conv2d(64,64,3,padding=1)
-        self.bn5 = nn.BatchNorm2d(64)
-        self.relu5 = nn.ReLU(inplace=True)
-
-        #####
-
-        self.conv_d4 = nn.Conv2d(128,64,3,padding=1)
-        self.bn_d4 = nn.BatchNorm2d(64)
-        self.relu_d4 = nn.ReLU(inplace=True)
-
-        self.conv_d3 = nn.Conv2d(128,64,3,padding=1)
-        self.bn_d3 = nn.BatchNorm2d(64)
-        self.relu_d3 = nn.ReLU(inplace=True)
-
-        self.conv_d2 = nn.Conv2d(128,64,3,padding=1)
-        self.bn_d2 = nn.BatchNorm2d(64)
-        self.relu_d2 = nn.ReLU(inplace=True)
-
-        self.conv_d1 = nn.Conv2d(128,64,3,padding=1)
-        self.bn_d1 = nn.BatchNorm2d(64)
-        self.relu_d1 = nn.ReLU(inplace=True)
-
-        self.conv_d0 = nn.Conv2d(64,1,3,padding=1)
-
-        self.upscore2 = nn.Upsample(scale_factor=2, mode='bilinear')
-
-
-    def forward(self,x):
-
-        hx = x
-        hx = self.conv0(hx)
-
-        hx1 = self.relu1(self.bn1(self.conv1(hx)))
-        hx = self.pool1(hx1)
-
-        hx2 = self.relu2(self.bn2(self.conv2(hx)))
-        hx = self.pool2(hx2)
-
-        hx3 = self.relu3(self.bn3(self.conv3(hx)))
-        hx = self.pool3(hx3)
-
-        hx4 = self.relu4(self.bn4(self.conv4(hx)))
-        hx = self.pool4(hx4)
-
-        hx5 = self.relu5(self.bn5(self.conv5(hx)))
-
-        hx = self.upscore2(hx5)
-
-        d4 = self.relu_d4(self.bn_d4(self.conv_d4(torch.cat((hx,hx4),1))))
-        hx = self.upscore2(d4)
-
-        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx,hx3),1))))
-        hx = self.upscore2(d3)
-
-        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx,hx2),1))))
-        hx = self.upscore2(d2)
-
-        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx,hx1),1))))
-
-        residual = self.conv_d0(d1)
-
-        return residual
-
 class DATUNet(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         '''
@@ -1021,8 +928,6 @@ class DATUNet(nn.Module):
         self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
         self.final_relu2 = nn.ReLU(inplace=True)
         self.final_conv  = nn.Conv2d(24, n_classes, 3, padding=1)
-        ## -------------Refine Module-------------
-        self.refunet = RefUnet(4,64)
 
     def forward(self, x):
         # # Question here
@@ -1076,7 +981,6 @@ class DATUNet(nn.Module):
         x = self.final_conv2(x)
         x = self.final_relu2(x)
         x = self.final_conv(x)
-        x = self.refunet(torch.cat([x, x_input], dim=1))
 
         return x
 
