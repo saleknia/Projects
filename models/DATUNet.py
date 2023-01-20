766745b7fb67467e02ab7f9e3f499483a97d44b3
saleknia <salekniaamirhossein@gmail.com>
2023-01-20 20:59:08 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -599,7 +599,6 @@ class UpBlock_b(nn.Module):
         self.conv = _make_nConv(in_channels=in_channels//2, out_channels=out_channels, nb_Conv=2, activation=activation, dilation=1, padding=1)
     
     def forward(self, x, skip_x):
-        skip_x = self.aspp(skip_x)
         x = self.up(x) 
         x = x + skip_x
         x = self.conv(x)
