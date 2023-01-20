b083fd4936d4a5d5c86414c509007a2df31b0a95
saleknia <salekniaamirhossein@gmail.com>
2023-01-20 08:17:43 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -843,6 +843,101 @@ def get_CTranS_config(num_channels, patch_size):
     config.base_channel = num_channels # base channel of U-Net
     return config
 
+class RefUnet(nn.Module):
+    def __init__(self,in_ch,inc_ch):
+        super(RefUnet, self).__init__()
+
+        self.conv0 = nn.Conv2d(in_ch,inc_ch,3,padding=1)
+
+        self.conv1 = nn.Conv2d(inc_ch,64,3,padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        self.pool1 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        self.conv2 = nn.Conv2d(64,64,3,padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.ReLU(inplace=True)
+
+        self.pool2 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        self.conv3 = nn.Conv2d(64,64,3,padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU(inplace=True)
+
+        self.pool3 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        self.conv4 = nn.Conv2d(64,64,3,padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU(inplace=True)
+
+        self.pool4 = nn.MaxPool2d(2,2,ceil_mode=True)
+
+        #####
+
+        self.conv5 = nn.Conv2d(64,64,3,padding=1)
+        self.bn5 = nn.BatchNorm2d(64)
+        self.relu5 = nn.ReLU(inplace=True)
+
+        #####
+
+        self.conv_d4 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d4 = nn.BatchNorm2d(64)
+        self.relu_d4 = nn.ReLU(inplace=True)
+
+        self.conv_d3 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d3 = nn.BatchNorm2d(64)
+        self.relu_d3 = nn.ReLU(inplace=True)
+
+        self.conv_d2 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d2 = nn.BatchNorm2d(64)
+        self.relu_d2 = nn.ReLU(inplace=True)
+
+        self.conv_d1 = nn.Conv2d(128,64,3,padding=1)
+        self.bn_d1 = nn.BatchNorm2d(64)
+        self.relu_d1 = nn.ReLU(inplace=True)
+
+        self.conv_d0 = nn.Conv2d(64,1,3,padding=1)
+
+        self.upscore2 = nn.Upsample(scale_factor=2, mode='bilinear')
+
+
+    def forward(self,x):
+
+        hx = x
+        hx = self.conv0(hx)
+
+        hx1 = self.relu1(self.bn1(self.conv1(hx)))
+        hx = self.pool1(hx1)
+
+        hx2 = self.relu2(self.bn2(self.conv2(hx)))
+        hx = self.pool2(hx2)
+
+        hx3 = self.relu3(self.bn3(self.conv3(hx)))
+        hx = self.pool3(hx3)
+
+        hx4 = self.relu4(self.bn4(self.conv4(hx)))
+        hx = self.pool4(hx4)
+
+        hx5 = self.relu5(self.bn5(self.conv5(hx)))
+
+        hx = self.upscore2(hx5)
+
+        d4 = self.relu_d4(self.bn_d4(self.conv_d4(torch.cat((hx,hx4),1))))
+        hx = self.upscore2(d4)
+
+        d3 = self.relu_d3(self.bn_d3(self.conv_d3(torch.cat((hx,hx3),1))))
+        hx = self.upscore2(d3)
+
+        d2 = self.relu_d2(self.bn_d2(self.conv_d2(torch.cat((hx,hx2),1))))
+        hx = self.upscore2(d2)
+
+        d1 = self.relu_d1(self.bn_d1(self.conv_d1(torch.cat((hx,hx1),1))))
+
+        residual = self.conv_d0(d1)
+
+        return x + residual
+
 class DATUNet(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         '''
@@ -902,11 +997,6 @@ class DATUNet(nn.Module):
         # self.RSA_2 = ReverseSpatialSelfAttention(96)
         # self.RSA_1 = ReverseSpatialSelfAttention(48)
 
-        self.ASPP_1 = ASPP(48)
-        self.ASPP_2 = ASPP(96)
-        self.ASPP_3 = ASPP(192)
-        self.ASPP_4 = ASPP(384)
-
         self.norm_4 = LayerNormProxy(dim=384)
         self.norm_3 = LayerNormProxy(dim=192)
         self.norm_2 = LayerNormProxy(dim=96)
@@ -931,6 +1021,8 @@ class DATUNet(nn.Module):
         self.final_conv2 = nn.Conv2d(48, 24, 3, padding=1)
         self.final_relu2 = nn.ReLU(inplace=True)
         self.final_conv  = nn.Conv2d(24, n_classes, 3, padding=1)
+        ## -------------Refine Module-------------
+        self.refunet = RefUnet(1,64)
 
     def forward(self, x):
         # # Question here
@@ -975,11 +1067,6 @@ class DATUNet(nn.Module):
         x3 = x_fuse[2] + (x3*(1.0-self.sigmoid_3(x_fuse[2])))
         x4 = x_fuse[3] + (x4*(1.0-self.sigmoid_4(x_fuse[3])))
 
-        x1 = self.ASPP_1(x1) 
-        x2 = self.ASPP_2(x2) 
-        x3 = self.ASPP_3(x3)
-        x4 = self.ASPP_4(x4)
-
         x3 = self.up3(x4, x3) 
         x2 = self.up2(x3, x2)     
         x1 = self.up1(x2, x1) 
@@ -989,6 +1076,7 @@ class DATUNet(nn.Module):
         x = self.final_conv2(x)
         x = self.final_relu2(x)
         x = self.final_conv(x)
+        x = self.refunet(x)
 
         return x
 
