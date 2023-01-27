26e05a4abddd11a5d2e1d690a2b6e61480629879
saleknia <salekniaamirhossein@gmail.com>
2023-01-27 13:42:52 +0000

    Commit Process

--- models/DATUNet.py
+++ models/DATUNet.py
@@ -626,13 +626,13 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up   = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.SKAttention = SKAttention(in_channels//2)
+        # self.SKAttention = SKAttention(in_channels//2)
         self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
-        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        x = self.SKAttention(x, skip_x)
+        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        # x = self.SKAttention(x, skip_x)
         x = self.conv(x)
         # x = x + skip_x
         return x
