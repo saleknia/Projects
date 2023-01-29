b19682a8d4cafd43720beeacf5699415d1062504
saleknia <salekniaamirhossein@gmail.com>
2023-01-29 17:12:00 +0000

    Commit Process

--- valid_s.py
+++ valid_s.py
@@ -272,6 +272,6 @@ def valid_s(end_epoch,epoch_num,model,dataloader,device,ckpt,num_class,writer,lo
 
     # # Save checkpoint
     if ckpt is not None:
-        ckpt.save_best(acc=loss_total.avg, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=None)
+        ckpt.save_best(acc=Dice, acc_per_class=Dice_per_class, epoch=epoch_num, net=model, optimizer=optimizer,lr_scheduler=None)
 
 
