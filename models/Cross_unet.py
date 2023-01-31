fbf34c6ef1ab245cd3c43228588558a0e4f785a6
saleknia <salekniaamirhossein@gmail.com>
2023-01-31 20:44:27 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -27,6 +27,42 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
+class SKAttention(nn.Module):
+
+    def __init__(self, channel=512, reduction=4, group=1):
+        super().__init__()
+        self.d=channel//reduction
+        self.fc=nn.Linear(channel,self.d)
+        self.fcs=nn.ModuleList([])
+        for i in range(2):
+            self.fcs.append(nn.Linear(self.d,channel))
+        self.softmax=nn.Softmax(dim=0)
+
+    def forward(self, x, y):
+        bs, c, _, _ = x.size()
+        conv_outs=[x, y]
+        feats=torch.stack(conv_outs,0)#k,bs,channel,h,w
+
+        ### fuse
+        U=sum(conv_outs) #bs,c,h,w
+
+        ### reduction channel
+        S=U.mean(-1).mean(-1) #bs,c
+        Z=self.fc(S) #bs,d
+
+        ### calculate attention weight
+        weights=[]
+        for fc in self.fcs:
+            weight=fc(Z)
+            weights.append(weight.view(bs,c,1,1)) #bs,channel
+        attention_weights=torch.stack(weights,0)  #k,bs,channel,1,1
+        attention_weights=torch.sigmoid(attention_weights)#k,bs,channel,1,1
+
+        ### fuse
+        V=(attention_weights*feats)
+        V=feats[0] + feats[1]
+        return V
+
 
 class UpBlock(nn.Module):
     """Upscaling then conv"""
@@ -34,9 +70,11 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU'):
         super(UpBlock, self).__init__()
         self.up  = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.att = SKAttention(in_channels//2)
     def forward(self, x, skip_x):
         x = self.up(x) 
-        x = x + skip_x
+        # x = x + skip_x
+        x = self.att(x, skip_x)
         return x
 
 class ConvBatchNorm(nn.Module):
