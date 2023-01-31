d256ed867ccb05f0da2dae90b565de10c26d45b8
saleknia <salekniaamirhossein@gmail.com>
2023-01-31 20:24:02 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -78,42 +78,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class ChannelPool(nn.Module):
-    def forward(self, x):
-        return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
-
-class SpatialGate(nn.Module):
-    def __init__(self):
-        super(SpatialGate, self).__init__()
-        kernel_size = 7
-        self.compress = ChannelPool()
-        self.spatial = nn.Conv2d(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2)
-    def forward(self, x):
-        x_compress = self.compress(x)
-        x_out = self.spatial(x_compress)
-        scale = F.sigmoid(x_out)
-        return x * scale
-
-class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.1):
-        super().__init__()
-        out_features = out_features or in_features
-        hidden_features = hidden_features or in_features
-        self.fc1 = nn.Linear(in_features, hidden_features)
-        self.act = act_layer()
-        self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.act(x)
-        x = self.drop(x)
-        x = self.fc2(x)
-        x = self.drop(x)
-        return x
-
-
 class MetaFormer(nn.Module):
 
     def __init__(self, drop_path=0., num_skip=3, skip_dim=[96, 192, 384], act_layer=nn.GELU, norm_layer=nn.LayerNorm):
@@ -231,6 +195,8 @@ class Cross_unet(nn.Module):
         x3 = outputs[2] 
         x2 = outputs[1] 
         x1 = outputs[0] 
+
+        x1, x2, x3 = self.MetaFormer(x1, x2, x3)
 
         x3 = self.up3(x4, x3) 
         x2 = self.up2(x3, x2) 
