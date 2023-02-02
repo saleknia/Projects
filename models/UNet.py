ebe5039030722eb5ce049b525159b2be104fe7ed
saleknia <salekniaamirhossein@gmail.com>
2023-02-02 09:47:04 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -210,9 +210,9 @@ class UNet(nn.Module):
         #     nn.ConvTranspose2d(channel, n_classes, kernel_size=2, stride=2)
         # )
 
-        # self.ESP_4 = DilatedParllelResidualBlockB(18, 18)
-        # self.ESP_3 = DilatedParllelResidualBlockB(18, 18)        
-        # self.ESP_2 = DilatedParllelResidualBlockB(18, 18)
+        self.ESP_4 = DilatedParllelResidualBlockB(18, 18)
+        self.ESP_3 = DilatedParllelResidualBlockB(18, 18)        
+        self.ESP_2 = DilatedParllelResidualBlockB(18, 18)
 
     def forward(self, x):
         # Question here
@@ -251,13 +251,13 @@ class UNet(nn.Module):
         # x = self.up2(x , x2) 
         # x = self.up1(x , x1) 
 
-        # z4 = self.ESP_4(z4) 
+        z4 = self.ESP_4(z4) 
 
         z3 = self.conv_4(torch.cat([z4, z3], dim=1))
-        # z3 = self.ESP_3(z3) 
+        z3 = self.ESP_3(z3) 
 
         z2 = self.conv_3(torch.cat([z3, z2], dim=1))
-        # z2 = self.ESP_2(z2) 
+        z2 = self.ESP_2(z2) 
 
         z2 = self.classifier(z2)
 
