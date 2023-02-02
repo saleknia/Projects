ecae807d8a196bcaa3ce622463dd48ff3b8792ed
saleknia <salekniaamirhossein@gmail.com>
2023-02-02 08:25:12 +0000

    Commit Process

--- models/UNet.py
+++ models/UNet.py
@@ -138,10 +138,10 @@ def make_fuse_layers():
 class GCN(nn.Module):
     def __init__(self,c,out_c,k=15): #out_Channel=21 in paper
         super(GCN, self).__init__()
-        self.conv_l1 = nn.Conv2d(c, out_c, kernel_size=(k,1), padding =((k-1)/2,0))
-        self.conv_l2 = nn.Conv2d(out_c, out_c, kernel_size=(1,k), padding =(0,(k-1)/2))
-        self.conv_r1 = nn.Conv2d(c, out_c, kernel_size=(1,k), padding =((k-1)/2,0))
-        self.conv_r2 = nn.Conv2d(out_c, out_c, kernel_size=(k,1), padding =(0,(k-1)/2))
+        self.conv_l1 = nn.Conv2d(c, out_c, kernel_size=(k,1), padding =((k-1)//2,0))
+        self.conv_l2 = nn.Conv2d(out_c, out_c, kernel_size=(1,k), padding =(0,(k-1)//2))
+        self.conv_r1 = nn.Conv2d(c, out_c, kernel_size=(1,k), padding =((k-1)//2,0))
+        self.conv_r2 = nn.Conv2d(out_c, out_c, kernel_size=(k,1), padding =(0,(k-1)//2))
         
     def forward(self, x):
         x_l = self.conv_l1(x)
