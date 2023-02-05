feacbafe7ac71a84f27236ad35d721160197b120
saleknia <salekniaamirhossein@gmail.com>
2023-02-05 15:34:54 +0000

    Commit Process

--- models/Cross.py
+++ models/Cross.py
@@ -42,8 +42,8 @@ class Cross(nn.Module):
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=768, out_features=512, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
+            # nn.Linear(in_features=5122, out_features=512, bias=True),
+            # nn.Dropout(p=0.4, inplace=True),
             nn.Linear(in_features=512, out_features=256, bias=True),
             nn.Dropout(p=0.4, inplace=True),
             nn.Linear(in_features=256, out_features=40, bias=True),
