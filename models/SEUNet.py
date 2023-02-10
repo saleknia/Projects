b878abb84045eab80a5d0fa17cecda54ec72808a
saleknia <salekniaamirhossein@gmail.com>
2023-02-10 09:47:16 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -4,6 +4,51 @@ import torch
 import torch.nn.functional as F
 from torch.nn import Softmax
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import Softmax
+
+
+def INF(B,H,W):
+     return -torch.diag(torch.tensor(float("inf")).repeat(H),0).unsqueeze(0).repeat(B*W,1,1)
+
+
+class CrissCrossAttention(nn.Module):
+    """ Criss-Cross Attention Module"""
+    def __init__(self, in_dim):
+        super(CrissCrossAttention,self).__init__()
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.softmax = Softmax(dim=3)
+        self.INF = INF
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+
+    def forward(self, x):
+        m_batchsize, _, height, width = x.size()
+        proj_query = self.query_conv(x)
+        proj_query_H = proj_query.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height).permute(0, 2, 1)
+        proj_query_W = proj_query.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width).permute(0, 2, 1)
+        proj_key = self.key_conv(x)
+        proj_key_H = proj_key.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_key_W = proj_key.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        proj_value = self.value_conv(x)
+        proj_value_H = proj_value.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_value_W = proj_value.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        energy_H = (torch.bmm(proj_query_H, proj_key_H)+self.INF(m_batchsize, height, width)).view(m_batchsize,width,height,height).permute(0,2,1,3)
+        energy_W = torch.bmm(proj_query_W, proj_key_W).view(m_batchsize,height,width,width)
+        concate = self.softmax(torch.cat([energy_H, energy_W], 3))
+
+        att_H = concate[:,:,:,0:height].permute(0,2,1,3).contiguous().view(m_batchsize*width,height,height)
+        #print(concate)
+        #print(att_H) 
+        att_W = concate[:,:,:,height:height+width].contiguous().view(m_batchsize*height,width,width)
+        out_H = torch.bmm(proj_value_H, att_H.permute(0, 2, 1)).view(m_batchsize,width,-1,height).permute(0,2,3,1)
+        out_W = torch.bmm(proj_value_W, att_W.permute(0, 2, 1)).view(m_batchsize,height,-1,width).permute(0,2,1,3)
+        #print(out_H.size(),out_W.size())
+        return self.gamma*(out_H + out_W) + x
 
 
 
@@ -62,8 +107,8 @@ class UpBlock(nn.Module):
     def __init__(self, in_channels, out_channels, nb_Conv, activation='ReLU', reduce=False, reduction_rate=1):
         super(UpBlock, self).__init__()
 
-        self.up = nn.ConvTranspose2d(in_channels,in_channels,(2,2),2)
-        self.nConvs = _make_nConv(in_channels*2, out_channels, nb_Conv, activation, reduce=reduce, reduction_rate=reduction_rate)
+        self.up = nn.ConvTranspose2d(in_channels,in_channels//2,(2,2),2)
+        self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation, reduce=reduce, reduction_rate=reduction_rate)
 
     def forward(self, x, skip_x):
         out = self.up(x)
@@ -151,25 +196,17 @@ class SEUNet(nn.Module):
         self.encoder3  = resnet.layer3
         self.encoder4  = resnet.layer4
 
-        # self.up3 = UpBlock(in_channels=512, out_channels=256, nb_Conv=2)
-        # self.up2 = UpBlock(in_channels=256, out_channels=128, nb_Conv=2)
-        # self.up1 = UpBlock(in_channels=128, out_channels=64 , nb_Conv=2)
+        self.up3 = UpBlock(in_channels=512, out_channels=256, nb_Conv=2)
+        self.up2 = UpBlock(in_channels=256, out_channels=128, nb_Conv=2)
+        self.up1 = UpBlock(in_channels=128, out_channels=64 , nb_Conv=2)
 
-        self.reduction_1 = ConvBatchNorm(in_channels=64 , out_channels=64, activation='ReLU', kernel_size=1, padding=0)
-        self.reduction_2 = ConvBatchNorm(in_channels=128, out_channels=64, activation='ReLU', kernel_size=1, padding=0)
-        self.reduction_3 = ConvBatchNorm(in_channels=256, out_channels=64, activation='ReLU', kernel_size=1, padding=0)
-        self.reduction_4 = ConvBatchNorm(in_channels=512, out_channels=64, activation='ReLU', kernel_size=1, padding=0)
-
-        self.up3 = UpBlock(in_channels=64, out_channels=64, nb_Conv=2)
-        self.up2 = UpBlock(in_channels=64, out_channels=64, nb_Conv=2)
-        self.up1 = UpBlock(in_channels=64, out_channels=64, nb_Conv=2)
+        self.CCA = CrissCrossAttention(256)
 
         self.final_conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
         self.final_relu1 = nn.ReLU(inplace=True)
         self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
         self.final_relu2 = nn.ReLU(inplace=True)
-        # self.final_conv3 = nn.ConvTranspose2d(32, n_classes, kernel_size=2, stride=2)
-        self.final_conv3 = nn.Conv2d(32, n_classes, 3, padding=1)
+        self.final_conv3 = nn.ConvTranspose2d(32, n_classes, kernel_size=2, stride=2)
 
     def forward(self, x):
         b, c, h, w = x.shape
@@ -179,17 +216,14 @@ class SEUNet(nn.Module):
         e0 = self.firstconv(x)
         e0 = self.firstbn(e0)
         e0 = self.firstrelu(e0)
-        # e0 = self.maxpool(e0)
+        e0 = self.maxpool(e0)
 
         e1 = self.encoder1(e0)
         e2 = self.encoder2(e1)
         e3 = self.encoder3(e2)
         e4 = self.encoder4(e3)
 
-        e1 = self.reduction_1(e1)
-        e2 = self.reduction_2(e2)
-        e3 = self.reduction_3(e3)
-        e4 = self.reduction_4(e4)
+        e3 = self.CCA(e3)
 
         e = self.up3(e4, e3)
         e = self.up2(e , e2)
