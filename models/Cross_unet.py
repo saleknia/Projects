32733480b7816699203d8eb2f105f8ce339f90c8
saleknia <salekniaamirhossein@gmail.com>
2023-02-12 13:39:58 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -154,15 +154,20 @@ class knitt(nn.Module):
     def __init__(self):
         super(knitt, self).__init__()
 
+        self.fusion_e3 = UpBlock(768, 384)
         self.fusion_e2 = UpBlock(384, 192)
         self.fusion_e1 = UpBlock(192, 96)
 
+        self.fusion_x3 = UpBlock(768, 384)
         self.fusion_x2 = UpBlock(384, 192)
         self.fusion_x1 = UpBlock(192, 96)
 
         self.combine = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
 
-    def forward(self, x1, x2, x3, e1, e2, e3):
+    def forward(self, x1, x2, x3, x4, e1, e2, e3, e4):
+
+        e3 = self.fusion_e3(x4, e3)
+        x3 = self.fusion_x3(e4, x3)
 
         e2 = self.fusion_e2(x3, e2)
         x2 = self.fusion_x2(e3, x2)
