b18f63ae775e172406d045b21a4f2f50ad0ff0a2
saleknia <salekniaamirhossein@gmail.com>
2023-02-15 14:25:41 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -219,13 +219,13 @@ def main(args):
         )
     logger.info(model_table)
 
-    # optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01)
+    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01)
     # lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=NUM_EPOCHS, eta_min=1e-6)
 
     # optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=LEARNING_RATE, momentum=0.9, weight_decay=0.0001)
     # optimizer = optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01, momentum=0.9)
 
-    optimizer = None
+    # optimizer = None
 
     if COSINE_LR is True:
         lr_scheduler = utils.CosineAnnealingWarmRestarts(optimizer, T_0=10, T_mult=1, eta_min=1e-4)
@@ -447,66 +447,66 @@ def main(args):
     else:
         checkpoint = None
 
-    # if args.train=='True':
-    #     logger.info(50*'*')
-    #     logger.info('Training Phase')
-    #     logger.info(50*'*')
-    #     loss_function = collect(start_epoch=56)
-    #     for epoch in range(start_epoch,end_epoch+1):
-    #         trainer(
-    #             end_epoch=end_epoch,
-    #             epoch_num=epoch,
-    #             model=model,
-    #             teacher_model = teacher_model,
-    #             dataloader=data_loader['train'],
-    #             optimizer=optimizer,
-    #             device=DEVICE,
-    #             ckpt=checkpoint,                
-    #             num_class=NUM_CLASS,
-    #             lr_scheduler=lr_scheduler,
-    #             writer=writer,
-    #             logger=logger,
-    #             loss_function=loss_function)
-            
-    #         if epoch==end_epoch:
-    #             if SAVE_MODEL and 0 < checkpoint.best_accuracy():
-    #                 pretrained_model_path = '/content/drive/MyDrive/checkpoint/' + CKPT_NAME + '_best.pth'
-    #                 loaded_data = torch.load(pretrained_model_path, map_location='cuda')
-    #                 pretrained = loaded_data['net']
-    #                 model2_dict = model.state_dict()
-    #                 state_dict = {k:v for k,v in pretrained.items() if ((k in model2_dict.keys()) and (v.shape==model2_dict[k].shape))}
-    #                 # logger.info(state_dict.keys())
-    #                 model2_dict.update(state_dict)
-    #                 model.load_state_dict(model2_dict)
-
-    #                 acc=loaded_data['acc']
-    #                 acc_per_class=loaded_data['acc_per_class'].tolist()
-    #                 # acc_per_class=[round(x,2) for x in acc_per_class]
-    #                 best_epoch=loaded_data['best_epoch']
-
-    #                 logger.info(50*'*')
-    #                 logger.info(f'Best Accuracy over training: {acc:.2f}')
-    #                 logger.info(f'Best Accuracy Per Class over training: {acc_per_class}')
-    #                 logger.info(f'Epoch Number: {best_epoch}')
-
-    if args.inference=='True':
+    if args.train=='True':
         logger.info(50*'*')
-        logger.info('Inference Phase')
-        # logger.info(50*'*')
-        # inference(model=model,logger=logger)
-        tester(
-            end_epoch=1,
-            epoch_num=1,
-            model=copy.deepcopy(model),
-            dataloader=data_loader['valid'],
-            device=DEVICE,
-            ckpt=None,
-            num_class=NUM_CLASS,
-            writer=writer,
-            logger=logger,
-            optimizer=None,
-            lr_scheduler=None,
-            early_stopping=None)
+        logger.info('Training Phase')
+        logger.info(50*'*')
+        loss_function = collect(start_epoch=56)
+        for epoch in range(start_epoch,end_epoch+1):
+            trainer(
+                end_epoch=end_epoch,
+                epoch_num=epoch,
+                model=model,
+                teacher_model = teacher_model,
+                dataloader=data_loader['train'],
+                optimizer=optimizer,
+                device=DEVICE,
+                ckpt=checkpoint,                
+                num_class=NUM_CLASS,
+                lr_scheduler=lr_scheduler,
+                writer=writer,
+                logger=logger,
+                loss_function=loss_function)
+            
+            if epoch==end_epoch:
+                if SAVE_MODEL and 0 < checkpoint.best_accuracy():
+                    pretrained_model_path = '/content/drive/MyDrive/checkpoint/' + CKPT_NAME + '_best.pth'
+                    loaded_data = torch.load(pretrained_model_path, map_location='cuda')
+                    pretrained = loaded_data['net']
+                    model2_dict = model.state_dict()
+                    state_dict = {k:v for k,v in pretrained.items() if ((k in model2_dict.keys()) and (v.shape==model2_dict[k].shape))}
+                    # logger.info(state_dict.keys())
+                    model2_dict.update(state_dict)
+                    model.load_state_dict(model2_dict)
+
+                    acc=loaded_data['acc']
+                    acc_per_class=loaded_data['acc_per_class'].tolist()
+                    # acc_per_class=[round(x,2) for x in acc_per_class]
+                    best_epoch=loaded_data['best_epoch']
+
+                    logger.info(50*'*')
+                    logger.info(f'Best Accuracy over training: {acc:.2f}')
+                    logger.info(f'Best Accuracy Per Class over training: {acc_per_class}')
+                    logger.info(f'Epoch Number: {best_epoch}')
+
+                    if args.inference=='True':
+                        logger.info(50*'*')
+                        logger.info('Inference Phase')
+                        # logger.info(50*'*')
+                        # inference(model=model,logger=logger)
+                        tester(
+                            end_epoch=1,
+                            epoch_num=1,
+                            model=copy.deepcopy(model),
+                            dataloader=data_loader['valid'],
+                            device=DEVICE,
+                            ckpt=None,
+                            num_class=NUM_CLASS,
+                            writer=writer,
+                            logger=logger,
+                            optimizer=None,
+                            lr_scheduler=None,
+                            early_stopping=None)
 
     logger.info(50*'*')
     logger.info(50*'*')
