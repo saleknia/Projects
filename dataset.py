ac2de9f0594487f7d55e481ec0895d0e9706c4a5
saleknia <salekniaamirhossein@gmail.com>
2023-02-17 15:59:28 +0000

    Commit Process

--- dataset.py
+++ dataset.py
@@ -33,7 +33,9 @@ from scipy.ndimage.morphology import binary_dilation
 import skimage
 import skimage.transform
 from skimage.color import rgb2gray
-
+import cv2
+import torch.utils.data as data
+import util
 # ===== normalize over the dataset 
 def dataset_normalized(imgs):
     imgs_normalized = np.empty(imgs.shape)
@@ -45,6 +47,56 @@ def dataset_normalized(imgs):
         imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
     return imgs_normalized
      
+
+class CreateDataset(data.Dataset):
+    def __init__(self, img_paths, label_paths, resize, phase, aug=False):
+        super(CreateDataset, self).__init__()
+        self.phase = phase
+        self.resize = resize
+        self.aug = aug
+
+        self.paths_imgs = util.get_image_paths(img_paths)
+        assert self.paths_imgs, 'Error: imgs paths are empty.'
+
+        self.paths_label = util.get_image_paths(label_paths)
+        assert self.paths_label, 'Error: paths_label are empty.'
+
+    def __getitem__(self, index):
+
+        img_path = self.paths_imgs[index]
+
+        img = util.read_img(img_path)  # h w c
+
+        lable_path = self.paths_label[index]
+
+        label = util.read_nodule_label(lable_path)  # h w
+
+        img = cv2.resize(img, (self.resize, self.resize))
+        label = cv2.resize(label, (self.resize, self.resize))
+
+        if self.phase == 'train':
+            if self.aug:
+                img, label= util.augment([img, np.expand_dims(label, axis=2)], hflip=True, rot=True)
+
+                label = label.squeeze(2) #HW
+                img = img[:, :, [2, 1, 0]] # bgr -> rgb
+
+        elif self.phase == 'val':
+            if img.shape[2] == 3:
+                img = img[:, :, [2, 1, 0]]
+
+            # if label.shape[2] == 3:
+            #     label = label[:,:, 1]
+        # print(label.shape)
+        img = torch.from_numpy(np.ascontiguousarray(np.transpose(img, (2, 0, 1)))).float()
+
+        label = torch.from_numpy(np.ascontiguousarray(label)).long()
+
+
+        return img, label # , img_path
+
+    def __len__(self):
+        return len(self.paths_imgs)
 
 
 class ISIC2018(Dataset):
