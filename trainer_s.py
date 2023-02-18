d3fb381d7e75cd696c75afd10246e9008cc04c8a
saleknia <salekniaamirhossein@gmail.com>
2023-02-18 12:03:19 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -357,8 +357,7 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
         targets = targets.long()
 
         if type(outputs)==tuple:
-            outputs = (outputs[0] + outputs[1] + outputs[2]) / 3.0
-            predictions = torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1)))
+            predictions = torch.round(torch.sigmoid(torch.squeeze(outputs[0], dim=1)))
 
         else:
             predictions = torch.round(torch.sigmoid(torch.squeeze(outputs, dim=1)))
