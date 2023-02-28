e33cd937a76012a79824df0b37608eeeddcb3e1e
saleknia <salekniaamirhossein@gmail.com>
2023-02-28 17:37:35 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -38,6 +38,7 @@ class Mobile_netV2(nn.Module):
         # model.features[0][0].in_channels = 4
 
         self.features = model.features
+        self.features[0][0].stride = (1, 1)
         self.avgpool = model.avgpool
 
         self.classifier = nn.Sequential(
