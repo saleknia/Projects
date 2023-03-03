08b8a01ffcf54f91db3ab23333685d884e0d9c93
saleknia <salekniaamirhossein@gmail.com>
2023-03-03 19:04:37 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -11,22 +11,20 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
 
-        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+        # model = efficientnet_b3(weights=EfficientNet_B3_Weights)
 
-        # model.features[0][0].stride = (1, 1)
-        # model.features[0][0].in_channels = 4
+        model.features[0][0].stride = (1, 1)
 
         self.features = model.features
-        # self.features[0][0].stride = (1, 1)
         self.avgpool = model.avgpool
 
         # for param in self.features[0:8].parameters():
         #     param.requires_grad = False
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=1536, out_features=40, bias=True),
+            nn.Linear(in_features=1280, out_features=40, bias=True),
         )
 
 
@@ -54,58 +52,44 @@ class Mobile_netV2(nn.Module):
         else:
             return torch.softmax(x, dim=1)
 
-class Mobile_netV2_teacher(nn.Module):
-    def __init__(self, num_classes=7, pretrained=True):
-        super(Mobile_netV2_teacher, self).__init__()
+class SequentialPolarizedSelfAttention(nn.Module):
 
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
-        model.features[0][0].stride = (1, 1)
-        self.features = model.features
-        self.avgpool = model.avgpool
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
 
-
-        self.drop_1  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_1 = nn.Linear(in_features=1280, out_features=512, bias=True)
-        self.drop_2  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_2 = nn.Linear(in_features=512, out_features=256, bias=True)
-        self.drop_3  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_3 = nn.Linear(in_features=256, out_features=128, bias=True)
-        self.drop_4  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_4 = nn.Linear(in_features=128, out_features=num_classes, bias=True)
-
-        # self.classifier = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=1280, out_features=512, bias=True),
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=512, out_features=256, bias=True),
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=256, out_features=128, bias=True),
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=128, out_features=num_classes, bias=True),
-        # )
-        
     def forward(self, x):
-        b, c, w, h = x.shape
+        b, c, h, w = x.size()
 
-        x = self.features(x)
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        # x = self.classifier(x)
-
-        x1 = self.drop_1(x)
-        x1 = self.dense_1(x1)
-
-        x2 = self.drop_2(x1)
-        x2 = self.dense_2(x2)        
-        
-        x3 = self.drop_3(x2)
-        x3 = self.dense_3(x3)
-
-        x4 = self.drop_4(x3)
-        x4 = self.dense_4(x4)
-
-        return x4
-
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(channel_out) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(channel_out) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*channel_out
+        return spatial_out
 
 
