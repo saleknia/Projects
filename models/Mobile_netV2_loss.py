e89f41369647eb6e0f7d4a76833e813c77d6c5a9
saleknia <salekniaamirhossein@gmail.com>
2023-03-03 12:21:28 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -4,174 +4,106 @@ import torch.nn.functional as F
 from torchvision.models import resnet18, resnet50, efficientnet_b0, EfficientNet_B0_Weights, efficientnet_b1, EfficientNet_B1_Weights, efficientnet_b4, EfficientNet_B4_Weights
 import torchvision
 
-def get_activation(activation_type):
-    activation_type = activation_type.lower()
-    if hasattr(nn, activation_type):
-        return getattr(nn, activation_type)()
-    else:
-        return nn.ReLU()
-
-class ConvBatchNorm(nn.Module):
-    """(convolution => [BN] => ReLU)"""
-
-    def __init__(self, in_channels, out_channels, activation='ReLU', kernel_size=3, padding=1, dilation=1):
-        super(ConvBatchNorm, self).__init__()
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, dilation=dilation)
-        self.norm = nn.BatchNorm2d(out_channels)
-        self.activation = get_activation(activation)
-
-    def forward(self, x):
-        out = self.conv(x)
-        out = self.norm(out)
-        return self.activation(out)
-
-def _make_nConv(in_channels, out_channels, nb_Conv, activation='ReLU', dilation=1, padding=1):
-    layers = []
-    layers.append(ConvBatchNorm(in_channels=in_channels, out_channels=out_channels, activation=activation, dilation=dilation, padding=padding))
-
-    for _ in range(nb_Conv - 1):
-        layers.append(ConvBatchNorm(in_channels=out_channels, out_channels=out_channels, activation=activation, dilation=dilation, padding=padding))
-    return nn.Sequential(*layers)
-
 class Mobile_netV2_loss(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2_loss, self).__init__()
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
 
-        self.encoder_group_1 = Mobile_netV2(num_classes=3)
-        loaded_data_group_1 = torch.load('/content/drive/MyDrive/checkpoint_group_1/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
-        pretrained_group_1 = loaded_data_group_1['net']
-        self.encoder_group_1.load_state_dict(pretrained_group_1)
+        self.b_0 = Mobile_netV2_0()
+        loaded_data_b_0 = torch.load('/content/drive/MyDrive/checkpoint_B0_83_92/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b_0 = loaded_data_b_0['net']
+        self.b_0.load_state_dict(pretrained_b_0)
 
-        for param in self.encoder_group_1.parameters():
-            param.requires_grad = False
-
-        self.encoder_group_2 = Mobile_netV2(num_classes=4)
-        loaded_data_group_2 = torch.load('/content/drive/MyDrive/checkpoint_group_2/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
-        pretrained_group_2 = loaded_data_group_2['net']
-        self.encoder_group_2.load_state_dict(pretrained_group_2)
-
-        for param in self.encoder_group_2.parameters():
-            param.requires_grad = False
-
-        self.encoder_classifier = Mobile_netV2_classifier(num_classes=2)
-        loaded_data_classifier = torch.load('/content/drive/MyDrive/checkpoint_classifier/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
-        pretrained_classifier = loaded_data_classifier['net']
-        self.encoder_classifier.load_state_dict(pretrained_classifier)
-
-        for param in self.encoder_classifier.parameters():
-            param.requires_grad = False
-
-        self.drop_1  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_1 = nn.Linear(in_features=1280, out_features=512, bias=True)
-        self.drop_2  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_2 = nn.Linear(in_features=512, out_features=256, bias=True)
-        self.drop_3  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_3 = nn.Linear(in_features=256, out_features=128, bias=True)
-        self.drop_4  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_4 = nn.Linear(in_features=128, out_features=num_classes, bias=True)
-
-    def forward(self, x):
-        b, c, h, w = x.shape
-
-        x_group_1 = self.encoder_group_1(x)
-        x_group_2 = self.encoder_group_2(x)
-
-        y_out = []
-        x = self.encoder_classifier(x)
-
-
-        for i in range(128):
-            x_group_1[i] = (1.0-x[i])  * x_group_1[i]
-            x_group_2[i] = x[i]        * x_group_2[i]
-
-            y_out.append([x_group_1[i,0],x_group_1[i, 1],x_group_2[i,1],x_group_2[i,0],x_group_2[i,3],x_group_1[i, 2],x_group_2[i,2]])
-
+        self.b_1 = Mobile_netV2_1()
+        loaded_data_b_1 = torch.load('/content/drive/MyDrive/checkpoint_B1_84_51/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b_1 = loaded_data_b_1['net']
+        self.b_1.load_state_dict(pretrained_b_1)        
         
+        self.b_2 = Mobile_netV2_2()
+        loaded_data_b_2 = torch.load('/content/drive/MyDrive/checkpoint_B2_86_01/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b_2 = loaded_data_b_2['net']
+        self.b_2.load_state_dict(pretrained_b_2)
+
+        self.b_3 = Mobile_netV2_3()
+        loaded_data_b_3 = torch.load('/content/drive/MyDrive/checkpoint_B3_86_82/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b_3 = loaded_data_b_3['net']
+        self.b_3.load_state_dict(pretrained_b_3)
 
 
-
-        # x = self.avgpool(x_fuse)
-        # x = x.view(x.size(0), -1)
-
-        # x1 = self.drop_1(x)
-        # x1 = self.dense_1(x1)
-
-        # x2 = self.drop_2(x1)
-        # x2 = self.dense_2(x2)        
-        
-        # x3 = self.drop_3(x2)
-        # x3 = self.dense_3(x3)
-
-        # x4 = self.drop_4(x3)
-        # x4 = self.dense_4(x4)
-
-        return torch.tensor(y_out,device='cuda')
-
-
-class Mobile_netV2(nn.Module):
-    def __init__(self, num_classes=2, pretrained=True):
-        super(Mobile_netV2, self).__init__()
-
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
-        model.features[0][0].stride = (1, 1)
-        self.features = model.features
-        self.avgpool = model.avgpool
-
-
-        self.drop_1  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_1 = nn.Linear(in_features=1280, out_features=512, bias=True)
-        self.drop_2  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_2 = nn.Linear(in_features=512, out_features=256, bias=True)
-        self.drop_3  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_3 = nn.Linear(in_features=256, out_features=128, bias=True)
-        self.drop_4  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_4 = nn.Linear(in_features=128, out_features=num_classes, bias=True)
-
-        
     def forward(self, x):
         b, c, w, h = x.shape
 
-        x = self.features(x)
+        x0 = self.b_0(x)
+        x1 = self.b_1(x)
+        x2 = self.b_2(x)
+        x3 = self.b_3(x)
 
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        # x = self.classifier(x)
+        x = x0 + x1 + x2 + x3
 
-        x1 = self.drop_1(x)
-        x1 = self.dense_1(x1)
+        if self.training:
+            return x
+        else:
+            return torch.softmax(x, dim=1)
 
-        x2 = self.drop_2(x1)
-        x2 = self.dense_2(x2)        
-        
-        x3 = self.drop_3(x2)
-        x3 = self.dense_3(x3)
 
-        x4 = self.drop_4(x3)
-        x4 = self.dense_4(x4)
 
-        # x4 = torch.softmax(x4, dim=1)[:, 1:]
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision.models import resnet18, resnet50, efficientnet_b0, EfficientNet_B0_Weights, efficientnet_b1, EfficientNet_B1_Weights, efficientnet_b2, EfficientNet_B2_Weights, EfficientNet_B3_Weights, efficientnet_b3, EfficientNet_B5_Weights, efficientnet_b5
+from torchvision.models.segmentation import DeepLabV3_ResNet50_Weights, DeepLabV3_MobileNet_V3_Large_Weights
+import random
 
-        return x4
 
-class Mobile_netV2_classifier(nn.Module):
-    def __init__(self, num_classes=2, pretrained=True):
-        super(Mobile_netV2_classifier, self).__init__()
+class Mobile_netV2_3(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2_3, self).__init__()
 
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
-        model.features[0][0].stride = (1, 1)
+        # self.teacher = Mobile_netV2_teacher()
+        # loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_teacher/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
+        # pretrained_teacher = loaded_data_teacher['net']
+        # self.teacher.load_state_dict(pretrained_teacher)
+
+        # for param in self.teacher.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        # model.features[0][0].stride = (1, 1)
+        # self.features = model.features
+        # self.avgpool = model.avgpool
+
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_resnet50(weights=DeepLabV3_ResNet50_Weights)
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(weights=DeepLabV3_MobileNet_V3_Large_Weights)
+
+        # for param in self.segmentation.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+
+        # model.features[0][0].stride = (1, 1)
+        # model.features[0][0].in_channels = 4
+
         self.features = model.features
+        # self.features[0][0].stride = (1, 1)
         self.avgpool = model.avgpool
 
+        # for param in self.features[0:8].parameters():
+        #     param.requires_grad = False
 
-        self.drop_1  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_1 = nn.Linear(in_features=1280, out_features=512, bias=True)
-        self.drop_2  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_2 = nn.Linear(in_features=512, out_features=256, bias=True)
-        self.drop_3  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_3 = nn.Linear(in_features=256, out_features=128, bias=True)
-        self.drop_4  = nn.Dropout(p=0.5, inplace=True)
-        self.dense_4 = nn.Linear(in_features=128, out_features=num_classes, bias=True)
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=1536, out_features=40, bias=True),
+        )
+
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
 
     def forward(self, x):
         b, c, w, h = x.shape
@@ -180,21 +112,206 @@ class Mobile_netV2_classifier(nn.Module):
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
+        x = self.classifier(x)
 
-        x1 = self.drop_1(x)
-        x1 = self.dense_1(x1)
+        if self.training:
+            return x
+        else:
+            return torch.softmax(x, dim=1)
 
-        x2 = self.drop_2(x1)
-        x2 = self.dense_2(x2)        
-        
-        x3 = self.drop_3(x2)
-        x3 = self.dense_3(x3)
+class Mobile_netV2_2(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2_2, self).__init__()
 
-        x4 = self.drop_4(x3)
-        x4 = self.dense_4(x4)
+        # self.teacher = Mobile_netV2_teacher()
+        # loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_teacher/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
+        # pretrained_teacher = loaded_data_teacher['net']
+        # self.teacher.load_state_dict(pretrained_teacher)
 
-        x4 = torch.softmax(x4, dim=1)
+        # for param in self.teacher.parameters():
+        #     param.requires_grad = False
 
-        return torch.argmax(x4, dim=1)
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        # model.features[0][0].stride = (1, 1)
+        # self.features = model.features
+        # self.avgpool = model.avgpool
+
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_resnet50(weights=DeepLabV3_ResNet50_Weights)
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(weights=DeepLabV3_MobileNet_V3_Large_Weights)
+
+        # for param in self.segmentation.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        model = efficientnet_b2(weights=EfficientNet_B2_Weights)
+
+        # model.features[0][0].stride = (1, 1)
+        # model.features[0][0].in_channels = 4
+
+        self.features = model.features
+        # self.features[0][0].stride = (1, 1)
+        self.avgpool = model.avgpool
+
+        # for param in self.features[0:8].parameters():
+        #     param.requires_grad = False
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=1408, out_features=40, bias=True),
+        )
+
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
+
+    def forward(self, x):
+        b, c, w, h = x.shape
+
+        x = self.features(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        if self.training:
+            return x
+        else:
+            return torch.softmax(x, dim=1)
+
+
+class Mobile_netV2_1(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2_1, self).__init__()
+
+        # self.teacher = Mobile_netV2_teacher()
+        # loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_teacher/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
+        # pretrained_teacher = loaded_data_teacher['net']
+        # self.teacher.load_state_dict(pretrained_teacher)
+
+        # for param in self.teacher.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        # model.features[0][0].stride = (1, 1)
+        # self.features = model.features
+        # self.avgpool = model.avgpool
+
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_resnet50(weights=DeepLabV3_ResNet50_Weights)
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(weights=DeepLabV3_MobileNet_V3_Large_Weights)
+
+        # for param in self.segmentation.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        model = efficientnet_b1(weights=EfficientNet_B1_Weights)
+
+        # model.features[0][0].stride = (1, 1)
+        # model.features[0][0].in_channels = 4
+
+        self.features = model.features
+        # self.features[0][0].stride = (1, 1)
+        self.avgpool = model.avgpool
+
+        # for param in self.features[0:8].parameters():
+        #     param.requires_grad = False
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=1280, out_features=40, bias=True),
+        )
+
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
+
+    def forward(self, x):
+        b, c, w, h = x.shape
+
+        x = self.features(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        if self.training:
+            return x
+        else:
+            return torch.softmax(x, dim=1)
+
+class Mobile_netV2_0(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2_0, self).__init__()
+
+        # self.teacher = Mobile_netV2_teacher()
+        # loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_teacher/Mobile_NetV2_FER2013_best.pth', map_location='cuda')
+        # pretrained_teacher = loaded_data_teacher['net']
+        # self.teacher.load_state_dict(pretrained_teacher)
+
+        # for param in self.teacher.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        # model.features[0][0].stride = (1, 1)
+        # self.features = model.features
+        # self.avgpool = model.avgpool
+
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_resnet50(weights=DeepLabV3_ResNet50_Weights)
+        # self.segmentation = torchvision.models.segmentation.deeplabv3_mobilenet_v3_large(weights=DeepLabV3_MobileNet_V3_Large_Weights)
+
+        # for param in self.segmentation.parameters():
+        #     param.requires_grad = False
+
+        # model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        # model.features[0][0].stride = (1, 1)
+        # model.features[0][0].in_channels = 4
+
+        self.features = model.features
+        # self.features[0][0].stride = (1, 1)
+        self.avgpool = model.avgpool
+
+        # for param in self.features[0:8].parameters():
+        #     param.requires_grad = False
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=1280, out_features=40, bias=True),
+        )
+
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
+
+    def forward(self, x):
+        b, c, w, h = x.shape
+
+        x = self.features(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+
+        if self.training:
+            return x
+        else:
+            return torch.softmax(x, dim=1)
+
+
 
 
