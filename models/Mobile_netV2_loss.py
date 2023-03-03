1043d8a06bf9b33089bed4a5cf455f327e27c801
saleknia <salekniaamirhossein@gmail.com>
2023-03-03 18:57:08 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -29,17 +29,17 @@ class Mobile_netV2_loss(nn.Module):
         pretrained_b_3 = loaded_data_b_3['net']
         self.b_3.load_state_dict(pretrained_b_3)
 
-        for param in self.b_0.parameters():
-            param.requires_grad = False
+        # for param in self.b_0.parameters():
+        #     param.requires_grad = False
 
-        for param in self.b_1.parameters():
-            param.requires_grad = False
+        # for param in self.b_1.parameters():
+        #     param.requires_grad = False
 
-        for param in self.b_2.parameters():
-            param.requires_grad = False
+        # for param in self.b_2.parameters():
+        #     param.requires_grad = False
 
-        for param in self.b_3.parameters():
-            param.requires_grad = False
+        # for param in self.b_3.parameters():
+        #     param.requires_grad = False
 
     def forward(self, x):
         b, c, w, h = x.shape
