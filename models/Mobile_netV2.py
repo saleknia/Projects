55dc1d441c2d7c298fff9765ab98a2a33f4eaecd
saleknia <salekniaamirhossein@gmail.com>
2023-03-03 21:14:43 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -10,7 +10,64 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
+        self.teacher = Mobile_netV2_teacher()
+        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B3_86_82/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_teacher = loaded_data_teacher['net']
+        self.teacher.load_state_dict(pretrained_teacher)
+
+        for param in self.teacher.parameters():
+            param.requires_grad = False
+
         model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+
+        # model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+
+        # model.features[0][0].stride = (1, 1)
+
+        self.features = model.features
+        self.avgpool = model.avgpool
+
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=1280, out_features=40, bias=True),
+        )
+
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
+
+    def forward(self, x0):
+        b, c, w, h = x0.shape
+
+        x1_t, x2_t, x3_t = self.teacher(x0)
+
+        # x = self.features(x)
+
+        x1 = self.features[0:4](x0)
+        x2 = self.features[4:5](x1)        
+        x3 = self.features[5:7](x2)
+        x  = self.features[7: ](x3)
+        x = self.avgpool(x) 
+        
+        x = x.view(x.size(0), -1)
+
+        x = self.classifier(x)
+
+        if self.training:
+            return x, x1_t, x2_t, x3_t, x1, x2, x3
+        else:
+            return torch.softmax(x, dim=1)
+
+
+class Mobile_netV2_teacher(nn.Module):
+    def __init__(self, num_classes=40, pretrained=True):
+        super(Mobile_netV2_teacher, self).__init__()
+
+        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
 
         # model = efficientnet_b3(weights=EfficientNet_B3_Weights)
 
@@ -23,73 +80,38 @@ class Mobile_netV2(nn.Module):
         #     param.requires_grad = False
 
 
-        # self.classifier = nn.Sequential(
-        #     nn.Linear(in_features=1280, out_features=40, bias=True),
-        # )
-
         self.classifier = nn.Sequential(
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=1280, out_features=512, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=512 , out_features=256, bias=True),
-            nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=256 , out_features=40, bias=True),
+            nn.Linear(in_features=1280, out_features=40, bias=True),
         )
 
-    def forward(self, x):
-        b, c, w, h = x.shape
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=1280, out_features=512, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=512 , out_features=256, bias=True),
+        #     nn.Dropout(p=0.4, inplace=True),
+        #     nn.Linear(in_features=256 , out_features=40, bias=True),
+        # )
 
-        x = self.features(x)
+    def forward(self, x0):
+        b, c, w, h = x0.shape
 
-        x = self.avgpool(x) 
+        x1 = self.features[0:4](x0)
+        x2 = self.features[4:5](x1)        
+        x3 = self.features[5:7](x2)
+
+        return x1, x2, x3
+
+        # x = self.avgpool(x) 
         
-        x = x.view(x.size(0), -1)
+        # x = x.view(x.size(0), -1)
 
-        x = self.classifier(x)
+        # x = self.classifier(x)
 
-        if self.training:
-            return x 
-        else:
-            return torch.softmax(x, dim=1)
+        # if self.training:
+        #     return x 
+        # else:
+        #     return torch.softmax(x, dim=1)
 
-class SequentialPolarizedSelfAttention(nn.Module):
-
-    def __init__(self, channel=512):
-        super().__init__()
-        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
-        self.softmax_channel=nn.Softmax(1)
-        self.softmax_spatial=nn.Softmax(-1)
-        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
-        self.ln=nn.LayerNorm(channel)
-        self.sigmoid=nn.Sigmoid()
-        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.agp=nn.AdaptiveAvgPool2d((1,1))
-
-    def forward(self, x):
-        b, c, h, w = x.size()
-
-        #Channel-only Self-Attention
-        channel_wv=self.ch_wv(x) #bs,c//2,h,w
-        channel_wq=self.ch_wq(x) #bs,1,h,w
-        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
-        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
-        channel_wq=self.softmax_channel(channel_wq)
-        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
-        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
-        channel_out=channel_weight*x
-
-        #Spatial-only Self-Attention
-        spatial_wv=self.sp_wv(channel_out) #bs,c//2,h,w
-        spatial_wq=self.sp_wq(channel_out) #bs,c//2,h,w
-        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
-        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
-        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
-        spatial_wq=self.softmax_spatial(spatial_wq)
-        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
-        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
-        spatial_out=spatial_weight*channel_out
-        return spatial_out
 
 
