f674c4cae0ed0549f2a85e0a98388eb6415db6ea
saleknia <salekniaamirhossein@gmail.com>
2023-03-04 17:12:06 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -32,8 +32,6 @@ class Mobile_netV2(nn.Module):
         self.features = model.features
         self.avgpool = model.avgpool
 
-        self.att = MobileViTv2Attention(320)
-
         self.classifier = nn.Sequential(
             nn.Linear(in_features=1280, out_features=40, bias=True),
         )
@@ -50,9 +48,7 @@ class Mobile_netV2(nn.Module):
     def forward(self, x0):
         b, c, w, h = x0.shape
 
-        x = self.features[0:8](x0)
-        x = self.att(x)
-        x = self.features[8:9](x)
+        x = self.features(x0)
 
         x = self.avgpool(x) 
         x = x.view(x.size(0), -1)
@@ -62,57 +58,6 @@ class Mobile_netV2(nn.Module):
             return x
         else:
             return torch.softmax(x, dim=1)
-
-class MobileViTv2Attention(nn.Module):
-    '''
-    Scaled dot-product attention
-    '''
-
-    def __init__(self, d_model):
-        '''
-        :param d_model: Output dimensionality of the model
-        :param d_k: Dimensionality of queries and keys
-        :param d_v: Dimensionality of values
-        :param h: Number of heads
-        '''
-        super(MobileViTv2Attention, self).__init__()
-        self.fc_i = nn.Linear(d_model,1)
-        self.fc_k = nn.Linear(d_model, d_model)
-        self.fc_v = nn.Linear(d_model, d_model)
-        self.fc_o = nn.Linear(d_model, d_model)
-
-        self.d_model = d_model
-        self.init_weights()
-
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-
-    def forward(self, input):
-        '''
-        Computes
-        :param queries: Queries (b_s, nq, d_model)
-        :return:
-        '''
-        i = self.fc_i(input) #(bs,nq,1)
-        weight_i = torch.softmax(i, dim=1) #bs,nq,1
-        context_score = weight_i * self.fc_k(input) #bs,nq,d_model
-        context_vector = torch.sum(context_score,dim=1,keepdim=True) #bs,1,d_model
-        v = self.fc_v(input) * context_vector #bs,nq,d_model
-        out = self.fc_o(v) #bs,nq,d_model
-
-        return out
 
 class Mobile_netV2_teacher(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
@@ -170,6 +115,8 @@ class Mobile_netV2_teacher(nn.Module):
         x2 = self.features[7:9](x1)
 
         return x1, x2
+
+
 
 
 
