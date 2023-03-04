a3dab3f15e1dc785f9e7a43552fddd8befe56220
saleknia <salekniaamirhossein@gmail.com>
2023-03-04 17:06:17 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -5,31 +5,34 @@ import torchvision
 from torchvision.models import resnet18, resnet50, efficientnet_b0, EfficientNet_B0_Weights, efficientnet_b1, EfficientNet_B1_Weights, efficientnet_b2, EfficientNet_B2_Weights, EfficientNet_B3_Weights, efficientnet_b3, EfficientNet_B5_Weights, efficientnet_b5
 from torchvision.models.segmentation import DeepLabV3_ResNet50_Weights, DeepLabV3_MobileNet_V3_Large_Weights
 import random
+from torch.nn import init
 
 class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        self.teacher = Mobile_netV2_teacher()
-        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B3_91_85/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
-        pretrained_teacher = loaded_data_teacher['net']
-        self.teacher.load_state_dict(pretrained_teacher)
+        # self.teacher = Mobile_netV2_teacher()
+        # loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B3_91_85/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        # pretrained_teacher = loaded_data_teacher['net']
+        # self.teacher.load_state_dict(pretrained_teacher)
 
-        for param in self.teacher.parameters():
-            param.requires_grad = False
+        # for param in self.teacher.parameters():
+        #     param.requires_grad = False
 
         model = efficientnet_b0(weights=EfficientNet_B0_Weights)
 
         # model = efficientnet_b5(weights=EfficientNet_B5_Weights)
 
-        model.features[0][0].stride = (1, 1)
+        # model.features[0][0].stride = (1, 1)
         # model.features[6][0].block[1][0].stride = (1, 1)
 
-        for param in model.features[0:5].parameters():
-            param.requires_grad = False
+        # for param in model.features[0:5].parameters():
+        #     param.requires_grad = False
 
         self.features = model.features
         self.avgpool = model.avgpool
+
+        self.att = MobileViTv2Attention(320)
 
         self.classifier = nn.Sequential(
             nn.Linear(in_features=1280, out_features=40, bias=True),
@@ -47,20 +50,69 @@ class Mobile_netV2(nn.Module):
     def forward(self, x0):
         b, c, w, h = x0.shape
 
-        x1_t, x2_t = self.teacher(x0)
+        x = self.features[0:8](x0)
+        x = self.att(x)
+        x = self.features[8:9](x)
 
-        x1 = self.features[0:7](x0)
-        x2 = self.features[7:9](x1)
-
-        x = self.avgpool(x2) 
+        x = self.avgpool(x) 
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
 
         if self.training:
-            return x, x1, x2, x1_t, x2_t
+            return x
         else:
             return torch.softmax(x, dim=1)
 
+class MobileViTv2Attention(nn.Module):
+    '''
+    Scaled dot-product attention
+    '''
+
+    def __init__(self, d_model):
+        '''
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        '''
+        super(MobileViTv2Attention, self).__init__()
+        self.fc_i = nn.Linear(d_model,1)
+        self.fc_k = nn.Linear(d_model, d_model)
+        self.fc_v = nn.Linear(d_model, d_model)
+        self.fc_o = nn.Linear(d_model, d_model)
+
+        self.d_model = d_model
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, input):
+        '''
+        Computes
+        :param queries: Queries (b_s, nq, d_model)
+        :return:
+        '''
+        i = self.fc_i(input) #(bs,nq,1)
+        weight_i = torch.softmax(i, dim=1) #bs,nq,1
+        context_score = weight_i * self.fc_k(input) #bs,nq,d_model
+        context_vector = torch.sum(context_score,dim=1,keepdim=True) #bs,1,d_model
+        v = self.fc_v(input) * context_vector #bs,nq,d_model
+        out = self.fc_o(v) #bs,nq,d_model
+
+        return out
 
 class Mobile_netV2_teacher(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
--- trainer_c.py
+++ trainer_c.py
@@ -194,7 +194,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         # targets[targets==43.00] = 3.00            
 
 
-        outputs, x1, x2, x1_t, x2_t = model(inputs)
+        outputs = model(inputs)
         # loss_function(outputs=outputs, labels=targets.long(), epoch=epoch_num)
 
         predictions = torch.argmax(input=outputs,dim=1).long()
@@ -228,9 +228,9 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         # loss_ce = ce_loss(outputs, targets.long())
 
         # loss_disparity = distillation(outputs, targets.long())
-        # loss_disparity = 0
+        loss_disparity = 0.0
         # loss_disparity = disparity_loss(labels=targets, outputs=outputs)
-        loss_disparity = 10.0 * (importance_maps_distillation(s=x1, t=x1_t) + importance_maps_distillation(s=x2, t=x2_t))
+        # loss_disparity = 10.0 * (importance_maps_distillation(s=x1, t=x1_t) + importance_maps_distillation(s=x2, t=x2_t))
         # loss_disparity = 5.0 * disparity_loss(fm_s=features_b, fm_t=features_a)
         ###############################################
         loss = loss_ce + loss_disparity
