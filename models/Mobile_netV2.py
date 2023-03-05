175bdf8ab5fb2020392fc659198909eda0a2ee1a
saleknia <salekniaamirhossein@gmail.com>
2023-03-05 08:58:06 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -128,12 +128,10 @@ import torch.nn.functional as F
 class h_sigmoid(nn.Module):
     def __init__(self, inplace=True):
         super(h_sigmoid, self).__init__()
-        # self.relu = nn.ReLU6(inplace=inplace)
-        self.relu = nn.SiLU(inplace=True)
+        self.relu = nn.ReLU6(inplace=inplace)
 
     def forward(self, x):
-        # return self.relu(x + 3) / 6
-        return self.relu(x)
+        return self.relu(x + 3) / 6
 
 class h_swish(nn.Module):
     def __init__(self, inplace=True):
