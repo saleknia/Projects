c0b6d99078dc47523c82f7278b48c946937d51ea
saleknia <salekniaamirhossein@gmail.com>
2023-03-05 07:49:20 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -23,10 +23,28 @@ class Mobile_netV2(nn.Module):
 
         # model = efficientnet_b5(weights=EfficientNet_B5_Weights)
 
-        # model.features[0][0].stride = (1, 1)
+        model.features[0][0].stride = (1, 1)
 
-        for param in model.features[0:8].parameters():
+        for param in model.features[0:5].parameters():
             param.requires_grad = False
+
+        for feature in model.features[5]:
+            feature = nn.Sequential(
+                feature,
+                CoordAtt(112, 112),
+            )
+
+        for feature in model.features[6]:
+            feature = nn.Sequential(
+                feature,
+                CoordAtt(192, 192),
+            )
+
+        for feature in model.features[7]:
+            feature = nn.Sequential(
+                feature,
+                CoordAtt(320, 320),
+            )
 
         self.features = model.features
         self.avgpool = model.avgpool
@@ -114,6 +132,66 @@ class Mobile_netV2_teacher(nn.Module):
         x2 = self.features[7:9](x1)
 
         return x1, x2
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+        
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        
+
+    def forward(self, x):
+        identity = x
+        
+        n,c,h,w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y) 
+        
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+
 
 
 
