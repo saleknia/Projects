d458b3f368cac9d4167d3a56f2ff6af33d1da370
saleknia <salekniaamirhossein@gmail.com>
2023-03-05 10:13:45 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -31,7 +31,7 @@ class Mobile_netV2(nn.Module):
         for i in [5, 6, 7]:
             for feature in model.features[i]:
                 channels = feature.block[2].fc1.in_channels
-                feature.block[2] = CoordAtt(channels, channels)
+                feature.block[2] = ECAAttention()
 
         self.features = model.features
         self.avgpool = model.avgpool
@@ -121,63 +121,44 @@ class Mobile_netV2_teacher(nn.Module):
         return x1, x2
 
 
+
+import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from torch import nn
+from torch.nn import init
+from collections import OrderedDict
 
-class h_sigmoid(nn.Module):
-    def __init__(self, inplace=True):
-        super(h_sigmoid, self).__init__()
-        self.relu = nn.ReLU6(inplace=inplace)
 
-    def forward(self, x):
-        return self.relu(x + 3) / 6
 
-class h_swish(nn.Module):
-    def __init__(self, inplace=True):
-        super(h_swish, self).__init__()
-        self.sigmoid = h_sigmoid(inplace=inplace)
+class ECAAttention(nn.Module):
 
-    def forward(self, x):
-        return x * self.sigmoid(x)
+    def __init__(self, kernel_size=3):
+        super().__init__()
+        self.gap=nn.AdaptiveAvgPool2d(1)
+        self.conv=nn.Conv1d(1,1,kernel_size=kernel_size,padding=(kernel_size-1)//2)
+        self.sigmoid=nn.Sigmoid()
 
-class CoordAtt(nn.Module):
-    def __init__(self, inp, oup, reduction=24):
-        super(CoordAtt, self).__init__()
-        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
-        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
-
-        mip = max(8, inp // reduction)
-
-        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
-        self.bn1 = nn.BatchNorm2d(mip)
-        self.act = h_swish()
-        
-        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
-        
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
 
     def forward(self, x):
-        identity = x
-        
-        n,c,h,w = x.size()
-        x_h = self.pool_h(x)
-        x_w = self.pool_w(x).permute(0, 1, 3, 2)
-
-        y = torch.cat([x_h, x_w], dim=2)
-        y = self.conv1(y)
-        y = self.bn1(y)
-        y = self.act(y) 
-        
-        x_h, x_w = torch.split(y, [h, w], dim=2)
-        x_w = x_w.permute(0, 1, 3, 2)
-
-        a_h = self.conv_h(x_h).sigmoid()
-        a_w = self.conv_w(x_w).sigmoid()
-
-        out = identity * a_w * a_h
-
-        return out
+        y=self.gap(x) #bs,c,1,1
+        y=y.squeeze(-1).permute(0,2,1) #bs,1,c
+        y=self.conv(y) #bs,1,c
+        y=self.sigmoid(y) #bs,1,c
+        y=y.permute(0,2,1).unsqueeze(-1) #bs,c,1,1
+        return x*y.expand_as(x)
 
 
 
