cb0ed4eb79d5b9efcf664c5d9360020f70ab73b1
saleknia <salekniaamirhossein@gmail.com>
2023-03-06 18:00:05 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -12,7 +12,7 @@ class Mobile_netV2(nn.Module):
         super(Mobile_netV2, self).__init__()
 
         self.teacher = Mobile_netV2_teacher()
-        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B3_86_82/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B2_86_61/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_teacher = loaded_data_teacher['net']
         a = pretrained_teacher.copy()
         for key in a.keys():
@@ -23,7 +23,7 @@ class Mobile_netV2(nn.Module):
         for param in self.teacher.parameters():
             param.requires_grad = False
 
-        model = efficientnet_b2(weights=EfficientNet_B2_Weights)
+        model = efficientnet_b1(weights=EfficientNet_B1_Weights)
 
         # model = efficientnet_b5(weights=EfficientNet_B5_Weights)
 
@@ -36,7 +36,7 @@ class Mobile_netV2(nn.Module):
         self.avgpool = model.avgpool
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=1408, out_features=40, bias=True),
+            nn.Linear(in_features=1280, out_features=40, bias=True),
         )
 
         # self.classifier = nn.Sequential(
@@ -70,7 +70,7 @@ class Mobile_netV2_teacher(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2_teacher, self).__init__()
 
-        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+        model = efficientnet_b2(weights=EfficientNet_B2_Weights)
 
         # model = efficientnet_b3(weights=EfficientNet_B3_Weights)
 
@@ -87,7 +87,7 @@ class Mobile_netV2_teacher(nn.Module):
 
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=1536, out_features=40, bias=True),
+            nn.Linear(in_features=1408, out_features=40, bias=True),
         )
 
         # self.classifier = nn.Sequential(
--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -74,9 +74,10 @@ class Mobile_netV2_loss(nn.Module):
         x0 = self.b_0(x)
         x1 = self.b_1(x)
         x2 = self.b_2(x)
-        # x3 = self.b_3(x)
+        x3 = self.b_3(x)
 
         # x = 1.0 * x0 + 1.45 * x1 + 1.67 * x2 + 2.0 * x3
+        # x = 1.0 * x0 + 1.47 * x1 + 1.67 * x2 + 2.0 * x3
         x = x0 + x1 + x2 + x3
 
         if self.training:
