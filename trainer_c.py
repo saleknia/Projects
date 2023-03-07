04a3405a3a30c8484e40390309d0ce7432cf840f
saleknia <salekniaamirhossein@gmail.com>
2023-03-07 07:13:08 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -231,7 +231,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         # loss_disparity = distillation(outputs, targets.long())
         # loss_disparity = 0.0
         # loss_disparity = disparity_loss(labels=targets, outputs=outputs)
-        loss_disparity = 1.0 * (importance_maps_distillation(s=x1, t=x1_t) + importance_maps_distillation(s=x2, t=x2_t))
+        loss_disparity = 2.0 * (importance_maps_distillation(s=x1, t=x1_t) + importance_maps_distillation(s=x2, t=x2_t))
         # loss_disparity = 5.0 * disparity_loss(fm_s=features_b, fm_t=features_a)
         ###############################################
         loss = loss_ce + loss_disparity
