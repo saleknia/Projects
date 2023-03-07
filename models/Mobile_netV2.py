ee4d706de1d2f413f853c50f2ca44119e5c1132b
saleknia <salekniaamirhossein@gmail.com>
2023-03-07 09:44:45 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -12,7 +12,7 @@ class Mobile_netV2(nn.Module):
         super(Mobile_netV2, self).__init__()
 
         self.teacher = Mobile_netV2_teacher()
-        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_VS_89_46/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint_B3_87_27/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_teacher = loaded_data_teacher['net']
         a = pretrained_teacher.copy()
         for key in a.keys():
@@ -25,7 +25,7 @@ class Mobile_netV2(nn.Module):
 
         # model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
 
-        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+        model = efficientnet_b2(weights=EfficientNet_B2_Weights)
 
         # model.features[0][0].stride = (1, 1)
 
@@ -36,7 +36,7 @@ class Mobile_netV2(nn.Module):
         self.avgpool = model.avgpool
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=1536, out_features=40, bias=True),
+            nn.Linear(in_features=1408, out_features=40, bias=True),
         )
 
         # self.classifier = nn.Sequential(
@@ -51,7 +51,7 @@ class Mobile_netV2(nn.Module):
     def forward(self, x0):
         b, c, w, h = x0.shape
 
-        x_t, x1_t, x2_t = self.teacher(x0)
+        x1_t, x2_t = self.teacher(x0)
 
         x1 = self.features[0:7](x0)
         x2 = self.features[7:8](x1)
@@ -64,7 +64,7 @@ class Mobile_netV2(nn.Module):
         x = self.classifier(x)
 
         if self.training:
-            return x, x1, x2, x_t, x1_t, x2_t
+            return x, x1, x2, x1_t, x2_t
         else:
             return torch.softmax(x, dim=1)
 
@@ -74,9 +74,9 @@ class Mobile_netV2_teacher(nn.Module):
 
         # model = efficientnet_b1(weights=EfficientNet_B1_Weights)
 
-        model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
+        # model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
 
-        # model = efficientnet_b3(weights=EfficientNet_B3_Weights)
+        model = efficientnet_b3(weights=EfficientNet_B3_Weights)
 
         # model.features[0][0].stride = (1, 1)
 
@@ -91,7 +91,7 @@ class Mobile_netV2_teacher(nn.Module):
 
 
         self.classifier = nn.Sequential(
-            nn.Linear(in_features=1280, out_features=40, bias=True),
+            nn.Linear(in_features=1530, out_features=40, bias=True),
         )
 
         # self.classifier = nn.Sequential(
@@ -124,13 +124,8 @@ class Mobile_netV2_teacher(nn.Module):
 
         x1 = self.features[0:7](x0)
         x2 = self.features[7:8](x1)
-        x3 = self.features[8:9](x2)
 
-        x = self.avgpool(x3) 
-        x = x.view(x.size(0), -1)
-        x = self.classifier(x)
-
-        return torch.softmax(x, dim=1), x1, x2
+        return x1, x2
 
 
 
