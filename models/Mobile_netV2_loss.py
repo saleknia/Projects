036a360d383f3b0945af2ffa20ef9a0d54ab1493
saleknia <salekniaamirhossein@gmail.com>
2023-03-07 09:16:20 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -9,20 +9,20 @@ class Mobile_netV2_loss(nn.Module):
         super(Mobile_netV2_loss, self).__init__()
         model = efficientnet_b0(weights=EfficientNet_B0_Weights)
 
-        # self.b_0 = Mobile_netV2_0()
-        # loaded_data_b_0 = torch.load('/content/drive/MyDrive/checkpoint_B0_84_40/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
-        # pretrained_b_0 = loaded_data_b_0['net']
+        self.b_0 = Mobile_netV2_0()
+        loaded_data_b_0 = torch.load('/content/drive/MyDrive/checkpoint_B0_83_92/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        pretrained_b_0 = loaded_data_b_0['net']
 
-        # a = pretrained_b_0.copy()
-        # for key in a.keys():
-        #     if 'teacher' in key:
-        #         pretrained_b_0.pop(key)
+        a = pretrained_b_0.copy()
+        for key in a.keys():
+            if 'teacher' in key:
+                pretrained_b_0.pop(key)
 
-        # self.b_0.load_state_dict(pretrained_b_0)
+        self.b_0.load_state_dict(pretrained_b_0)
 
 
         self.b_1 = Mobile_netV2_1()
-        loaded_data_b_1 = torch.load('/content/drive/MyDrive/checkpoint_B1_85_40/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_b_1 = torch.load('/content/drive/MyDrive/checkpoint_B1_84_51/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_b_1 = loaded_data_b_1['net']
 
         a = pretrained_b_1.copy()
@@ -34,7 +34,7 @@ class Mobile_netV2_loss(nn.Module):
 
 
         self.b_2 = Mobile_netV2_2()
-        loaded_data_b_2 = torch.load('/content/drive/MyDrive/checkpoint_B2_86_61/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_b_2 = torch.load('/content/drive/MyDrive/checkpoint_B2_86_01/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_b_2 = loaded_data_b_2['net']
 
         a = pretrained_b_2.copy()
@@ -46,7 +46,7 @@ class Mobile_netV2_loss(nn.Module):
 
 
         self.b_3 = Mobile_netV2_3()
-        loaded_data_b_3 = torch.load('/content/drive/MyDrive/checkpoint_B3_86_82/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
+        loaded_data_b_3 = torch.load('/content/drive/MyDrive/checkpoint_B3_87_27/Mobile_NetV2_Standford40_best.pth', map_location='cuda')
         pretrained_b_3 = loaded_data_b_3['net']
 
         a = pretrained_b_3.copy()
@@ -76,14 +76,15 @@ class Mobile_netV2_loss(nn.Module):
     def forward(self, x):
         b, c, w, h = x.shape
 
-        # x0 = self.b_0(x)
+        x0 = self.b_0(x)
         x1 = self.b_1(x)
         x2 = self.b_2(x)
         x3 = self.b_3(x)
 
         # x = 1.0 * x0 + 1.45 * x1 + 1.67 * x2 + 2.0 * x3
         # x = 1.0 * x0 + 1.47 * x1 + 1.67 * x2 + 2.0 * x3
-        x = self.w1 * x1 + self.w2 * x2 + self.w3 * x3
+        # x = self.w1 * x1 + self.w2 * x2 + self.w3 * x3
+        x = x0 + x1 + x2 + x3
 
         if self.training:
             return x
