f24cf211c7fb8801de5487fe78ec9e4fd61b93eb
saleknia <salekniaamirhossein@gmail.com>
2023-03-08 05:46:18 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -11,14 +11,14 @@ class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
         super(Mobile_netV2, self).__init__()
 
-        model = efficientnet_b5(weights=EfficientNet_B5_Weights)
+        model = efficientnet_b1(weights=EfficientNet_B1_Weights)
 
         self.features = model.features
         self.avgpool = model.avgpool
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.4, inplace=True),
-            nn.Linear(in_features=2048, out_features=512, bias=True),
+            nn.Linear(in_features=1280, out_features=512, bias=True),
             nn.Dropout(p=0.4, inplace=True),
             nn.Linear(in_features=512, out_features=256, bias=True),
             nn.Dropout(p=0.4, inplace=True),
