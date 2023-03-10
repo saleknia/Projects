b954a655947ba9afbcb5c86fb1a82fc98ad81a0e
saleknia <salekniaamirhossein@gmail.com>
2023-03-10 18:21:02 +0000

    Commit Process

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ if log:
     logger.info(f'Logging Directory: {logging_log}')   
 ##########################################################################
 
-LEARNING_RATE = 0.1
+LEARNING_RATE = 0.025
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 40
 NUM_EPOCHS = 120
