fc27ffed41b4f595f98117a39cc23e101191ff1b
saleknia <salekniaamirhossein@gmail.com>
2023-03-11 07:32:24 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -231,7 +231,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         else:
             # loss_ce = ce_loss(outputs, targets.long()) 
             # loss_ce = ce_loss(outputs, outputs_t)
-            loss_ce = ce_loss(outputs, targets.long()) + 0.1 * torch.nn.functional.mse_loss(outputs, outputs_t)
+            loss_ce = ce_loss(outputs, targets.long()) + 1.0 * torch.nn.functional.mse_loss(outputs, outputs_t)
             # loss_ce = (0.1 * ce_loss(outputs, targets.long())) + (F.kl_div(F.log_softmax(outputs/4.0, dim=1),F.softmax(outputs_t/4.0, dim=1),reduction='batchmean') * 16.0 * 0.9)
             # loss_ce = loss_label_smoothing(outputs=outputs, labels=targets.long())
 
