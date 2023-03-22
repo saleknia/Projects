662fc40b5016bb15c75c3081d5e5d3a9c15cb4ec
saleknia <salekniaamirhossein@gmail.com>
2023-03-22 12:09:39 +0000

    Commit Process

--- proto.py
+++ proto.py
@@ -442,7 +442,7 @@ def main(args):
     elif TASK_NAME=='Standford40':
 
         transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ])
