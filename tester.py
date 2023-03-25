40663822179208ca66ebed12cae6b4111dd3fb91
saleknia <salekniaamirhossein@gmail.com>
2023-03-25 19:11:14 +0000

    Commit Process

--- tester.py
+++ tester.py
@@ -33,8 +33,8 @@ def tester(end_epoch,epoch_num,model,dataloader,device,ckpt,num_class,writer,log
 
             targets = targets.float()
 
-            targets[targets!=4.0] = 0.0
-            targets[targets==4.0] = 1.0
+            # targets[targets!=4.0] = 0.0
+            # targets[targets==4.0] = 1.0
 
             outputs = model(inputs)
 
@@ -49,8 +49,8 @@ def tester(end_epoch,epoch_num,model,dataloader,device,ckpt,num_class,writer,log
 
             predictions = torch.argmax(input=outputs,dim=1).long()
 
-            predictions[predictions!=6.0] = 0.0
-            predictions[predictions==6.0] = 1.0
+            # predictions[predictions!=6.0] = 0.0
+            # predictions[predictions==6.0] = 1.0
 
             Eval.add_batch(gt_image=targets,pre_image=predictions)
             hd95_acc = hd95(masks=targets,preds=predictions,num_class=num_class)
