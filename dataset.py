18a032df49149b22413e0718d8ec2088fe218b74
saleknia <salekniaamirhossein@gmail.com>
2023-03-25 19:51:10 +0000

    Commit Process

--- dataset.py
+++ dataset.py
@@ -750,7 +750,7 @@ class Synapse_dataset(Dataset):
         # print(type(sample['image']))
         # print(type(sample['label']))
 
-        sample['image'],sample['label'] = self.img_transform(sample['image']), self.gt_tansform(sample['label'])
+        sample['image'],sample['label'] = self.img_transform(sample['image']), self.gt_transform(sample['label'])
 
         image,mask = sample['image'],sample['label'] 
 
