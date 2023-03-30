7003789680b9e3ba342c9113e3d409748d92fcb7
saleknia <salekniaamirhossein@gmail.com>
2023-03-30 11:37:27 +0000

    Commit Process

--- trainer_s.py
+++ trainer_s.py
@@ -275,7 +275,7 @@ def trainer_s(end_epoch,epoch_num,model,dataloader,optimizer,device,ckpt,num_cla
 
     total_batchs = len(dataloader['train'])
     loader = dataloader['train'] 
-    pos_weight = dataloader['pos_weight']
+    # pos_weight = dataloader['pos_weight']
     dice_loss = DiceLoss()
     ce_loss = torch.nn.BCEWithLogitsLoss(pos_weight=None)
 
