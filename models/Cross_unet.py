459aca0cd802089bf2b9111aeb4ebb888eeed7ef
saleknia <salekniaamirhossein@gmail.com>
2023-04-14 19:03:26 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -615,6 +615,8 @@ class Cross_unet(nn.Module):
         y = self.head_1(x1, x2, x3)
         z = self.head_2(e1, e2, e3)
 
+        # return z
+        
         if self.training:
             return x, y, z
         else:
