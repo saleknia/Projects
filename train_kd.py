99a6a0f1b26dfc5d3eb5f3b631eb61b2ada46ead
saleknia <salekniaamirhossein@gmail.com>
2023-04-15 14:32:09 +0000

    Commit Process

--- train_kd.py
+++ train_kd.py
@@ -21,6 +21,7 @@ from torch.utils.data import random_split
 from tqdm.notebook import tqdm
 import torch.optim as optim
 from models.SEUNet import SEUNet
+from models.SEUNet_loss import SEUNet_loss
 from models.UNet import UNet
 from models.UNet_loss import UNet_loss
 from models.UNet_plus import NestedUNet
@@ -199,6 +200,8 @@ def main(args):
     elif MODEL_NAME=='SEUNet':
         model = SEUNet(n_channels=3, n_classes=NUM_CLASS).to(DEVICE)
 
+    elif MODEL_NAME=='SEUNet_loss':
+        model = SEUNet_loss(n_channels=3, n_classes=NUM_CLASS).to(DEVICE)
 
     elif MODEL_NAME=='Cross_unet':
         model = Cross_unet().to(DEVICE)
