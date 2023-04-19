1f26bb404afb67c7a7e40e725705978366bc8cc4
saleknia <salekniaamirhossein@gmail.com>
2023-04-19 13:48:39 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -415,39 +415,39 @@ class Cross_unet(nn.Module):
                                     use_checkpoint=False,
                                     merge_size=[[2, 4], [2,4], [2, 4]])
 
-        self.encoder_2 = DAT(
-                            img_size=224,
-                            patch_size=4,
-                            num_classes=1000,
-                            expansion=4,
-                            dim_stem=96,
-                            dims=[96, 192, 384, 768],
-                            depths=[2, 2, 6, 2],
-                            stage_spec=[['L', 'S'], ['L', 'S'], ['L', 'D', 'L', 'D', 'L', 'D'], ['L', 'D']],
-                            heads=[3, 6, 12, 24],
-                            window_sizes=[7, 7, 7, 7] ,
-                            groups=[-1, -1, 3, 6],
-                            use_pes=[False, False, True, True],
-                            dwc_pes=[False, False, False, False],
-                            strides=[-1, -1, 1, 1],
-                            sr_ratios=[-1, -1, -1, -1],
-                            offset_range_factor=[-1, -1, 2, 2],
-                            no_offs=[False, False, False, False],
-                            fixed_pes=[False, False, False, False],
-                            use_dwc_mlps=[False, False, False, False],
-                            use_conv_patches=False,
-                            drop_rate=0.0,
-                            attn_drop_rate=0.0,
-                            drop_path_rate=0.2,
-                        )
+        # self.encoder_2 = DAT(
+        #                     img_size=224,
+        #                     patch_size=4,
+        #                     num_classes=1000,
+        #                     expansion=4,
+        #                     dim_stem=96,
+        #                     dims=[96, 192, 384, 768],
+        #                     depths=[2, 2, 6, 2],
+        #                     stage_spec=[['L', 'S'], ['L', 'S'], ['L', 'D', 'L', 'D', 'L', 'D'], ['L', 'D']],
+        #                     heads=[3, 6, 12, 24],
+        #                     window_sizes=[7, 7, 7, 7] ,
+        #                     groups=[-1, -1, 3, 6],
+        #                     use_pes=[False, False, True, True],
+        #                     dwc_pes=[False, False, False, False],
+        #                     strides=[-1, -1, 1, 1],
+        #                     sr_ratios=[-1, -1, -1, -1],
+        #                     offset_range_factor=[-1, -1, 2, 2],
+        #                     no_offs=[False, False, False, False],
+        #                     fixed_pes=[False, False, False, False],
+        #                     use_dwc_mlps=[False, False, False, False],
+        #                     use_conv_patches=False,
+        #                     drop_rate=0.0,
+        #                     attn_drop_rate=0.0,
+        #                     drop_path_rate=0.2,
+        #                 )
 
         self.norm_3_1 = LayerNormProxy(dim=384)
         self.norm_2_1 = LayerNormProxy(dim=192)
         self.norm_1_1 = LayerNormProxy(dim=96)
 
-        self.norm_3_2 = LayerNormProxy(dim=384)
-        self.norm_2_2 = LayerNormProxy(dim=192)
-        self.norm_1_2 = LayerNormProxy(dim=96)
+        # self.norm_3_2 = LayerNormProxy(dim=384)
+        # self.norm_2_2 = LayerNormProxy(dim=192)
+        # self.norm_1_2 = LayerNormProxy(dim=96)
 
         # self.sigmoid_1 = nn.Sigmoid()
         # self.sigmoid_2 = nn.Sigmoid()
@@ -504,7 +504,7 @@ class Cross_unet(nn.Module):
         x = self.knitt(x1, x2, x3, e1, e2, e3)
 
         # y = self.tp_conv1(x)
-        # y = self.conv2(y)
+        y = self.conv2(x)
         # y = self.tp_conv2(y)
 
         return y
