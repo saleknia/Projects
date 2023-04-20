af519feac12fb3629418285770a8e31c99c13fd4
saleknia <salekniaamirhossein@gmail.com>
2023-04-20 10:57:32 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -115,7 +115,7 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up   = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels*2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        self.att  = SKAttention(channel=in_channels//2)
+        self.att  = SKAttention(channel=in_channels)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
