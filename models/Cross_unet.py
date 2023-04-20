1a695bd4a834fb8721cf303878791f9a5b620d74
saleknia <salekniaamirhossein@gmail.com>
2023-04-20 09:24:13 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -464,8 +464,13 @@ class Cross_unet(nn.Module):
         self.conv_2 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
         self.conv_3 = _make_nConv(in_channels=384, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
 
+
         # self.meta = MetaFormer()
-        self.mtc = ChannelTransformer(config=get_CTranS_config(), vis=False, img_size=224,channel_num=[96, 96, 96], patchSize=get_CTranS_config().patch_sizes)
+        # self.mtc = ChannelTransformer(config=get_CTranS_config(), vis=False, img_size=224,channel_num=[96, 96, 96], patchSize=get_CTranS_config().patch_sizes)
+
+        self.psa_1 = ParallelPolarizedSelfAttention(96)
+        self.psa_2 = ParallelPolarizedSelfAttention(96)
+        self.psa_3 = ParallelPolarizedSelfAttention(96)
 
     def forward(self, x):
         # # Question here
@@ -482,9 +487,13 @@ class Cross_unet(nn.Module):
         x2 = self.conv_2(x2)
         x1 = self.conv_1(x1)
 
+        x3 = self.psa_3(x3)
+        x2 = self.psa_2(x2)
+        x1 = self.psa_1(x1)
+
         # x1, x2, x3 = self.meta(x1, x2, x3)
 
-        x1, x2, x3 = self.mtc(x1, x2, x3)
+        # x1, x2, x3 = self.mtc(x1, x2, x3)
 
         # e1, e2, e3 = self.meta_2(e1, e2, e3)
 
@@ -615,6 +624,47 @@ class AttentionBlock(nn.Module):
         psi = self.psi(psi)
         out = skip_connection * psi
         return out 
+
+class ParallelPolarizedSelfAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*x
+        out=spatial_out+channel_out
+        return out
 
 # class Cross_unet(nn.Module):
 #     def __init__(self, n_channels=3, n_classes=1):
