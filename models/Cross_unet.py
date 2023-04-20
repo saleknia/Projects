8fa56a9894100beabb66d5afbbdb12645e5dc40a
saleknia <salekniaamirhossein@gmail.com>
2023-04-20 06:48:51 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -99,12 +99,12 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up   = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        self.att  = SKAttention(channel=in_channels//2)
+        # self.att  = SKAttention(channel=in_channels//2)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
-        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        x = self.att(x, skip_x)
+        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        # x = self.att(x, skip_x)
         x = self.conv(x)
         return x 
 
@@ -446,9 +446,9 @@ class Cross_unet(nn.Module):
 
         # self.conv2 = nn.Sequential(nn.Conv2d(96, 1, 1, 1, 0), nn.Upsample(scale_factor=4.0))
 
-        self.conv_1 = _make_nConv(in_channels=96 , out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=0)
-        self.conv_2 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=0)        
-        self.conv_3 = _make_nConv(in_channels=384, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=0)
+        self.conv_1 = _make_nConv(in_channels=96 , out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+        self.conv_2 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
+        self.conv_3 = _make_nConv(in_channels=384, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
 
     def forward(self, x):
         # # Question here
