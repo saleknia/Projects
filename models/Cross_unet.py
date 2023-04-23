4d6e0d5bd2a7d9b422ab996064e35e0748fc0d30
saleknia <salekniaamirhossein@gmail.com>
2023-04-23 14:39:03 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -116,14 +116,16 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         # self.up   = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
         self.up   = nn.Upsample(scale_factor=2.0)
-        self.conv = _make_nConv(in_channels=in_channels*2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        self.SE   = SEBlock(in_channels)
+        self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+        # self.SE   = SEBlock(in_channels)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
-        skip_x = self.SE(x, skip_x)
-        x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
+        # skip_x = self.SE(x, skip_x)
+        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+        x = x + skip_x
+        # x = self.conv(x)
         return x 
 
 class ConvBatchNorm(nn.Module):
