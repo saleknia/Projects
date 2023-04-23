1ecab469ec8e3db73fe0cd0feacd85846c36f0ff
saleknia <salekniaamirhossein@gmail.com>
2023-04-23 12:25:20 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -550,14 +550,14 @@ import ml_collections
 def get_CTranS_config():
     config = ml_collections.ConfigDict()
     config.transformer = ml_collections.ConfigDict()
-    config.KV_size = 288  # KV_size = Q1 + Q2 + Q3 + Q4
+    config.KV_size = 192  # KV_size = Q1 + Q2 + Q3 + Q4
     config.transformer.num_heads  = 4
     config.transformer.num_layers = 4
     config.expand_ratio           = 4  # MLP channel dimension expand ratio
     config.transformer.embeddings_dropout_rate = 0.1
     config.transformer.attention_dropout_rate  = 0.1
     config.transformer.dropout_rate = 0
-    config.patch_sizes = [4,2,1]
+    config.patch_sizes = [1,1]
     config.base_channel = 96 # base channel of U-Net
     config.n_classes = 1
     return config
