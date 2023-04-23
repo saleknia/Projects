04a3a17de0c4dfe8e5656daf7166a5e95283fb3c
saleknia <salekniaamirhossein@gmail.com>
2023-04-23 09:05:18 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -116,11 +116,11 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up   = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels*2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        # self.att  = SKAttention(channel=in_channels)
+        self.att  = ParallelPolarizedSelfAttention(96)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
-        # x, skip_x = self.att(x, skip_x)
+        x = self.att(x)
         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
         x = self.conv(x)
         return x 
