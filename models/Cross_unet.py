1c3f2b72cc71497774685da32c75ae2469d6d4d9
saleknia <salekniaamirhossein@gmail.com>
2023-04-23 18:10:29 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -714,7 +714,7 @@ class ParallelPolarizedSelfAttention(nn.Module):
 
 
 
-class Cross_unet(nn.Module):
+class Cross_unet_cross(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         '''
         n_channels : number of channels of the input.
@@ -733,7 +733,7 @@ class Cross_unet(nn.Module):
                                     in_chans= 3,
                                     num_classes=1000,
                                     embed_dim=96,
-                                    depths=[2, 2, 6, 2],
+                                    depths=[2, 2, 18, 2],
                                     num_heads=[3, 6, 12, 24],
                                     group_size=[7, 7, 7, 7],
                                     mlp_ratio=4.,
@@ -746,32 +746,6 @@ class Cross_unet(nn.Module):
                                     use_checkpoint=False,
                                     merge_size=[[2, 4], [2,4], [2, 4]])
 
-        self.encoder_2 = DAT(
-                            img_size=224,
-                            patch_size=4,
-                            num_classes=1000,
-                            expansion=4,
-                            dim_stem=96,
-                            dims=[96, 192, 384, 768],
-                            depths=[2, 2, 6, 2],
-                            stage_spec=[['L', 'S'], ['L', 'S'], ['L', 'D', 'L', 'D', 'L', 'D'], ['L', 'D']],
-                            heads=[3, 6, 12, 24],
-                            window_sizes=[7, 7, 7, 7] ,
-                            groups=[-1, -1, 3, 6],
-                            use_pes=[False, False, True, True],
-                            dwc_pes=[False, False, False, False],
-                            strides=[-1, -1, 1, 1],
-                            sr_ratios=[-1, -1, -1, -1],
-                            offset_range_factor=[-1, -1, 2, 2],
-                            no_offs=[False, False, False, False],
-                            fixed_pes=[False, False, False, False],
-                            use_dwc_mlps=[False, False, False, False],
-                            use_conv_patches=False,
-                            drop_rate=0.0,
-                            attn_drop_rate=0.0,
-                            drop_path_rate=0.2,
-                        )
-
 
         self.norm_3_1 = LayerNormProxy(dim=384)
         self.norm_2_1 = LayerNormProxy(dim=192)
@@ -780,18 +754,6 @@ class Cross_unet(nn.Module):
         self.conv_1_1 = _make_nConv(in_channels=96 , out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
         self.conv_2_1 = _make_nConv(in_channels=192, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
         self.conv_3_1 = _make_nConv(in_channels=384, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-
-        self.norm_3_2 = LayerNormProxy(dim=384)
-        self.norm_2_2 = LayerNormProxy(dim=192)
-        self.norm_1_2 = LayerNormProxy(dim=96)
-
-        self.conv_1_2 = _make_nConv(in_channels=96 , out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        self.conv_2_2 = _make_nConv(in_channels=192, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
-        self.conv_3_2 = _make_nConv(in_channels=384, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-
-        self.combine_1 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        self.combine_2 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
-        self.combine_3 = _make_nConv(in_channels=192, out_channels=96, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
 
         self.knitt = knitt(channel=channel)
 
@@ -806,7 +768,6 @@ class Cross_unet(nn.Module):
         # self.classifier = nn.Sequential(nn.Conv2d(channel, 1, 1, 1, 0), nn.Upsample(scale_factor=4.0))
 
         self.MetaFormer_1 = MetaFormer()
-        self.MetaFormer_2 = MetaFormer()
 
         # self.mtc  = ChannelTransformer(config=get_CTranS_config(), vis=False, img_size=224,channel_num=[96, 96, 96], patchSize=get_CTranS_config().patch_sizes)
 
@@ -817,7 +778,6 @@ class Cross_unet(nn.Module):
         B, C, H, W = x.shape
 
         outputs_1 = self.encoder_1(x_input)
-        outputs_2 = self.encoder_2(x_input)
 
         x3 = self.norm_3_1(outputs_1[2]) 
         x2 = self.norm_2_1(outputs_1[1]) 
@@ -828,20 +788,6 @@ class Cross_unet(nn.Module):
         x1 = self.conv_1_1(x1) 
 
         x1, x2, x3 = self.MetaFormer_1(x1, x2, x3)
-
-        e3 = self.norm_3_2(outputs_2[2]) 
-        e2 = self.norm_2_2(outputs_2[1]) 
-        e1 = self.norm_1_2(outputs_2[0])
-
-        e3 = self.conv_3_2(e3)
-        e2 = self.conv_2_2(e2) 
-        e1 = self.conv_1_2(e1) 
-
-        e1, e2, e3 = self.MetaFormer_2(e1, e2, e3)
-
-        x1 = self.combine_1(torch.cat([x1, e1], dim=1))
-        x2 = self.combine_2(torch.cat([x2, e2], dim=1))
-        x3 = self.combine_3(torch.cat([x3, e3], dim=1))
 
         # x1, x2, x3 = self.mtc(x1, x2, x3)
 
@@ -1454,7 +1400,7 @@ class CrossFormer(nn.Module):
                                patch_size_end=patch_size_end,
                                num_patch_size=num_patch_size)
             self.layers.append(layer)
-        checkpoint = torch.load('/content/drive/MyDrive/crossformer-s.pth', map_location='cpu')
+        checkpoint = torch.load('/content/drive/MyDrive/crossformer-b.pth', map_location='cpu')
         state_dict = checkpoint['model']
         self.load_state_dict(state_dict, strict=False)
         self.layers[3] = None
