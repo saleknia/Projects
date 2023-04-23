7aaac37a9ab91cce003f90abcbd184528af822f0
saleknia <salekniaamirhossein@gmail.com>
2023-04-23 15:54:51 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -907,7 +907,7 @@ class Cross_unet_cross(nn.Module):
         return t
 
 
-class Cross_unet_cross(nn.Module):
+class Cross_unet(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         '''
         n_channels : number of channels of the input.
