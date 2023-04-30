0129516365555769031f3fce75e8318561ba2553
saleknia <salekniaamirhossein@gmail.com>
2023-04-30 10:04:51 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -721,13 +721,31 @@ class Cross_unet(nn.Module):
 
         channel = 96
 
+        # self.encoder_1 = CrossFormer(img_size=224,
+        #                             patch_size=[4, 8, 16, 32],
+        #                             in_chans= 3,
+        #                             num_classes=1000,
+        #                             embed_dim=96,
+        #                             depths=[2, 2, 6, 2],
+        #                             num_heads=[3, 6, 12, 24],
+        #                             group_size=[7, 7, 7, 7],
+        #                             mlp_ratio=4.,
+        #                             qkv_bias=True,
+        #                             qk_scale=None,
+        #                             drop_rate=0.0,
+        #                             drop_path_rate=0.2,
+        #                             ape=False,
+        #                             patch_norm=True,
+        #                             use_checkpoint=False,
+        #                             merge_size=[[2, 4], [2,4], [2, 4]])
+
         self.encoder_1 = CrossFormer(img_size=224,
                                     patch_size=[4, 8, 16, 32],
                                     in_chans= 3,
                                     num_classes=1000,
                                     embed_dim=96,
-                                    depths=[2, 2, 6, 2],
-                                    num_heads=[3, 6, 12, 24],
+                                    depths=[1, 1, 8, 6],
+                                    num_heads=[2, 4, 8, 16],
                                     group_size=[7, 7, 7, 7],
                                     mlp_ratio=4.,
                                     qkv_bias=True,
@@ -1452,7 +1470,7 @@ class CrossFormer(nn.Module):
                                patch_size_end=patch_size_end,
                                num_patch_size=num_patch_size)
             self.layers.append(layer)
-        checkpoint = torch.load('/content/drive/MyDrive/crossformer-s.pth', map_location='cpu')
+        checkpoint = torch.load('/content/drive/MyDrive/crossformer-t.pth', map_location='cpu')
         state_dict = checkpoint['model']
         self.load_state_dict(state_dict, strict=False)
         # self.layers[3] = None
