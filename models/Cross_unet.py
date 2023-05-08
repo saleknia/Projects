6a8c123e8494a2f5b52acaf03523d52d76169b47
saleknia <salekniaamirhossein@gmail.com>
2023-05-08 20:39:12 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -111,81 +111,41 @@ class seed():
 
 seed_func = seed()
 
-class UpBlock(nn.Module):
-    """Upscaling then conv"""
-
-    def __init__(self, in_channels, out_channels, nb_Conv=2, activation='ReLU'):
-        super(UpBlock, self).__init__()
-        self.up   = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
-        self.conv = _make_nConv(in_channels=in_channels//2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-
-        # seed_func.define()
-        # self.att  = SEBlock(channel=in_channels//2)
-        # seed_func.find()
-
-    def forward(self, x, skip_x):
-        x = self.up(x) 
-        # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        # skip_x = self.att(x, skip_x)
-        x = self.conv(x+skip_x)
-        # x = self.conv(x)
-        return x 
-
-class ParallelPolarizedSelfAttention(nn.Module):
-
-    def __init__(self, channel=512):
-        super().__init__()
-        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
-        self.softmax_channel=nn.Softmax(1)
-        self.softmax_spatial=nn.Softmax(-1)
-        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
-        self.ln=nn.LayerNorm(channel)
-        self.sigmoid=nn.Sigmoid()
-        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
-        self.agp=nn.AdaptiveAvgPool2d((1,1))
-
-    def forward(self, x):
-        b, c, h, w = x.size()
-
-        #Channel-only Self-Attention
-        channel_wv=self.ch_wv(x) #bs,c//2,h,w
-        channel_wq=self.ch_wq(x) #bs,1,h,w
-        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
-        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
-        channel_wq=self.softmax_channel(channel_wq)
-        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
-        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
-        channel_out=channel_weight*x
-
-        #Spatial-only Self-Attention
-        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
-        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
-        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
-        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
-        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
-        spatial_wq=self.softmax_spatial(spatial_wq)
-        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
-        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
-        spatial_out=spatial_weight*x
-        out=spatial_out+channel_out
-        return out
-
 # class UpBlock(nn.Module):
 #     """Upscaling then conv"""
 
-#     def __init__(self, in_channels, out_channels, nb_Conv=2, activation='ReLU', img_size=224):
+#     def __init__(self, in_channels, out_channels, nb_Conv=2, activation='ReLU'):
 #         super(UpBlock, self).__init__()
-#         # self.up   = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
-#         self.up   = nn.Upsample(scale_factor=2.0)
-#         self.conv = _make_nConv(in_channels=in_channels*2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-    
+#         self.up   = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+#         self.conv = _make_nConv(in_channels=in_channels//2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+
+#         # seed_func.define()
+#         # self.att  = SEBlock(channel=in_channels//2)
+#         # seed_func.find()
+
 #     def forward(self, x, skip_x):
 #         x = self.up(x) 
-#         x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-#         x = self.conv(x)
+#         # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
+#         # skip_x = self.att(x, skip_x)
+#         x = self.conv(x+skip_x)
+#         # x = self.conv(x)
 #         return x 
+
+
+class UpBlock(nn.Module):
+    """Upscaling then conv"""
+
+    def __init__(self, in_channels, out_channels, nb_Conv=2, activation='ReLU', img_size=224):
+        super(UpBlock, self).__init__()
+        # self.up   = nn.ConvTranspose2d(in_channels, in_channels, kernel_size=2, stride=2)
+        self.up   = nn.Upsample(scale_factor=2.0)
+        self.conv = _make_nConv(in_channels=in_channels, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+    
+    def forward(self, x, skip_x):
+        x = self.up(x) 
+        x = x + skip_x
+        x = self.conv(x)
+        return x 
 
 class ConvBatchNorm(nn.Module):
     """(convolution => [BN] => ReLU)"""
@@ -227,22 +187,22 @@ class knitt(nn.Module):
     def __init__(self, channel):
         super(knitt, self).__init__()
 
-        self.fusion_x3 = UpBlock(768, 384)
-        self.fusion_x2 = UpBlock(384, 192)
-        self.fusion_x1 = UpBlock(192, 96)
+        self.fusion_x3 = UpBlock(96, 96)
+        self.fusion_x2 = UpBlock(96, 96)
+        self.fusion_x1 = UpBlock(96, 96)
 
-        seed_func.define()
-        self.SegFormerHead = SegFormerHead()
-        seed_func.find()
+        # seed_func.define()
+        # self.SegFormerHead = SegFormerHead()
+        # seed_func.find()
 
 
     def forward(self, x1, x2, x3, x4):
 
-        x3 = self.fusion_x3(x4, x3)
-        x2 = self.fusion_x2(x3, x2)
-        x1 = self.fusion_x1(x2, x1)
+        x = self.fusion_x3(x4, x3)
+        x = self.fusion_x2(x , x2)
+        x = self.fusion_x1(x , x1)
 
-        x  = self.SegFormerHead(x1, x2, x3)
+        # x  = self.SegFormerHead(x1, x2, x3)
 
         return x
 
@@ -284,10 +244,10 @@ class Cross_unet(nn.Module):
         self.norm_2 = LayerNormProxy(dim=192)
         self.norm_1 = LayerNormProxy(dim=96)
 
-        # self.conv_1_1 = _make_nConv(in_channels=96 , out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        # self.conv_2_1 = _make_nConv(in_channels=192, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
-        # self.conv_3_1 = _make_nConv(in_channels=384, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        # self.conv_4_1 = _make_nConv(in_channels=768, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+        self.conv_1 = _make_nConv(in_channels=96 , out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+        self.conv_2 = _make_nConv(in_channels=192, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)        
+        self.conv_3 = _make_nConv(in_channels=384, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
+        self.conv_4 = _make_nConv(in_channels=768, out_channels=channel, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
 
         self.knitt = knitt(channel=channel)
 
@@ -323,10 +283,10 @@ class Cross_unet(nn.Module):
         x2 = self.norm_2(outputs[1]) 
         x1 = self.norm_1(outputs[0])
 
-        # x4 = self.conv_4_1(x4)
-        # x3 = self.conv_3_1(x3)
-        # x2 = self.conv_2_1(x2) 
-        # x1 = self.conv_1_1(x1) 
+        x4 = self.conv_4(x4)
+        x3 = self.conv_3(x3)
+        x2 = self.conv_2(x2) 
+        x1 = self.conv_1(x1) 
 
         # x1, x2, x3 = self.MetaFormer(x1, x2, x3)
 
