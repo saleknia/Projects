132c04a7cc58453127298abfecd9bec1c9a3e91e
saleknia <salekniaamirhossein@gmail.com>
2023-05-08 13:08:10 +0000

    Commit Process

--- models/Cross_unet.py
+++ models/Cross_unet.py
@@ -100,15 +100,56 @@ class UpBlock(nn.Module):
         super(UpBlock, self).__init__()
         self.up   = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
         self.conv = _make_nConv(in_channels=in_channels//2, out_channels=out_channels, nb_Conv=2, activation='ReLU', dilation=1, padding=1)
-        # self.att  = SKAttention(channel=in_channels//2)
+        self.att  = ParallelPolarizedSelfAttention(channel=in_channels//2)
     
     def forward(self, x, skip_x):
         x = self.up(x) 
         # x = torch.cat([x, skip_x], dim=1)  # dim 1 is the channel dimension
-        # x = self.att(x, skip_x)
+        x = self.att(x)
         x = self.conv(x+skip_x)
         # x = self.conv(x)
         return x 
+
+class ParallelPolarizedSelfAttention(nn.Module):
+
+    def __init__(self, channel=512):
+        super().__init__()
+        self.ch_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.ch_wq=nn.Conv2d(channel,1,kernel_size=(1,1))
+        self.softmax_channel=nn.Softmax(1)
+        self.softmax_spatial=nn.Softmax(-1)
+        self.ch_wz=nn.Conv2d(channel//2,channel,kernel_size=(1,1))
+        self.ln=nn.LayerNorm(channel)
+        self.sigmoid=nn.Sigmoid()
+        self.sp_wv=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.sp_wq=nn.Conv2d(channel,channel//2,kernel_size=(1,1))
+        self.agp=nn.AdaptiveAvgPool2d((1,1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        #Channel-only Self-Attention
+        channel_wv=self.ch_wv(x) #bs,c//2,h,w
+        channel_wq=self.ch_wq(x) #bs,1,h,w
+        channel_wv=channel_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        channel_wq=channel_wq.reshape(b,-1,1) #bs,h*w,1
+        channel_wq=self.softmax_channel(channel_wq)
+        channel_wz=torch.matmul(channel_wv,channel_wq).unsqueeze(-1) #bs,c//2,1,1
+        channel_weight=self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b,c,1).permute(0,2,1))).permute(0,2,1).reshape(b,c,1,1) #bs,c,1,1
+        channel_out=channel_weight*x
+
+        #Spatial-only Self-Attention
+        spatial_wv=self.sp_wv(x) #bs,c//2,h,w
+        spatial_wq=self.sp_wq(x) #bs,c//2,h,w
+        spatial_wq=self.agp(spatial_wq) #bs,c//2,1,1
+        spatial_wv=spatial_wv.reshape(b,c//2,-1) #bs,c//2,h*w
+        spatial_wq=spatial_wq.permute(0,2,3,1).reshape(b,1,c//2) #bs,1,c//2
+        spatial_wq=self.softmax_spatial(spatial_wq)
+        spatial_wz=torch.matmul(spatial_wq,spatial_wv) #bs,1,h*w
+        spatial_weight=self.sigmoid(spatial_wz.reshape(b,1,h,w)) #bs,1,h,w
+        spatial_out=spatial_weight*x
+        out=spatial_out+channel_out
+        return out
 
 # class UpBlock(nn.Module):
 #     """Upscaling then conv"""
@@ -177,24 +218,6 @@ class knitt(nn.Module):
 
         return x
 
-class FAMBlock(nn.Module):
-    def __init__(self, channels):
-        super(FAMBlock, self).__init__()
-
-        self.conv3 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=3, padding=1)
-        self.conv1 = nn.Conv2d(in_channels=channels, out_channels=channels, kernel_size=1)
-
-        self.relu3 = nn.ReLU(inplace=True)
-        self.relu1 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x3 = self.conv3(x)
-        x3 = self.relu3(x3)
-        x1 = self.conv1(x)
-        x1 = self.relu1(x1)
-        out = x3 + x1
-
-        return out
 
 class Cross_unet(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
@@ -256,13 +279,6 @@ class Cross_unet(nn.Module):
 
         # self.DA_1, self.DA_2, self.DA_3 = get_stage()
 
-        self.FAMBlock1 = FAMBlock(channels=96)
-        self.FAMBlock2 = FAMBlock(channels=192)
-        self.FAMBlock3 = FAMBlock(channels=384)
-        self.FAM1 = nn.ModuleList([self.FAMBlock1 for i in range(4)])
-        self.FAM2 = nn.ModuleList([self.FAMBlock2 for i in range(4)])
-        self.FAM3 = nn.ModuleList([self.FAMBlock3 for i in range(4)])
-
     def forward(self, x):
         # # Question here
         x_input = x.float()
@@ -287,13 +303,6 @@ class Cross_unet(nn.Module):
         # x3 = self.DA_3(x3)[0]
 
         # x1, x2, x3, x4 = self.mtc(x1, x2, x3, x4)
-
-        for i in range(4):
-            x3 = self.FAM3[i](x3)
-        for i in range(4):
-            x2 = self.FAM2[i](x2)
-        for i in range(4):
-            x1 = self.FAM1[i](x1)
 
         x = self.knitt(x1, x2, x3, x4)
 
