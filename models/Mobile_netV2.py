9c381dcd5f963040d308777e4d9b961618dcdd86
saleknia <salekniaamirhossein@gmail.com>
2023-05-20 15:48:18 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -35,7 +35,7 @@ class Mobile_netV2(nn.Module):
 
         self.features = model.features
 
-        for param in self.features[0:4].parameters():
+        for param in self.features[0:5].parameters():
             param.requires_grad = False
 
         self.avgpool = model.avgpool
