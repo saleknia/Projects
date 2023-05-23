c1603c68d75641579ec175b37510779c2078ad71
saleknia <salekniaamirhossein@gmail.com>
2023-05-23 18:29:42 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -65,7 +65,7 @@ def label_smoothing(labels, outputs_t):
     smoothed_labels = torch.full(size=(N, C), fill_value= alpha / (C - 1)).cuda()
     smoothed_labels.scatter_(dim=1, index=torch.unsqueeze(labels, dim=1), value=1-alpha)
 
-    # smoothed_labels = (smoothed_labels + outputs_t) / 2.0
+    smoothed_labels = (smoothed_labels + outputs_t) / 2.0
 
     return smoothed_labels
 
