99e52639e7939588cb61fab4d06837fbb7b9c069
saleknia <salekniaamirhossein@gmail.com>
2023-05-23 10:57:15 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -33,9 +33,9 @@ class Mobile_netV2(nn.Module):
 
         # model = efficientnet_b1(weights=EfficientNet_B1_Weights)
 
-        model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
+        # model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
 
-        # model = efficientnet_v2_m(weights=EfficientNet_V2_M_Weights)
+        model = efficientnet_v2_m(weights=EfficientNet_V2_M_Weights)
 
         # model = efficientnet_v2_l(weights=EfficientNet_V2_L_Weights)
 
