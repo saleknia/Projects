6c3799d57931154395135c7985dae0fb1f4f535a
saleknia <salekniaamirhossein@gmail.com>
2023-05-24 09:25:49 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -62,7 +62,7 @@ class Mobile_netV2(nn.Module):
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=1408, out_features=40, bias=True))
+            nn.Linear(in_features=1408, out_features=101, bias=True))
         
     def forward(self, x0):
         b, c, w, h = x0.shape
