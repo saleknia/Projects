3aa04e0fcfe15bc801b1d81288cc2f7320ff7530
saleknia <salekniaamirhossein@gmail.com>
2023-05-25 13:07:54 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -75,11 +75,11 @@ class SEUNet(nn.Module):
         self.layer3 = model.trunk_output.block3
         self.layer4 = model.trunk_output.block4
 
-        self.up3 = DecoderBottleneckLayer(in_channels=672, out_channels=288)
-        self.up2 = DecoderBottleneckLayer(in_channels=288, out_channels=128)
-        self.up1 = DecoderBottleneckLayer(in_channels=128, out_channels=64)
+        self.up3 = DecoderBottleneckLayer(in_channels=912, out_channels=408)
+        self.up2 = DecoderBottleneckLayer(in_channels=408, out_channels=168)
+        self.up1 = DecoderBottleneckLayer(in_channels=168, out_channels=72)
 
-        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(64, 32, 3, 2, 1, 1),
+        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(72, 32, 3, 2, 1, 1),
                                       nn.BatchNorm2d(32),
                                       nn.ReLU(inplace=True),)
         self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1),
