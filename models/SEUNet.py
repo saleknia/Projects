a70ef9f9d013379f168a2cac95e7ed7731b68793
saleknia <salekniaamirhossein@gmail.com>
2023-05-25 13:32:04 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -67,7 +67,7 @@ class SEUNet(nn.Module):
         self.n_channels = n_channels
         self.n_classes = n_classes
 
-        model = torchvision.models.regnet_x_1_6gf(weights='DEFAULT')
+        model = torchvision.models.regnet_x_3_2gf(weights='DEFAULT')
 
         self.stem   = model.stem
         self.layer1 = model.trunk_output.block1
@@ -75,11 +75,11 @@ class SEUNet(nn.Module):
         self.layer3 = model.trunk_output.block3
         self.layer4 = model.trunk_output.block4
 
-        self.up3 = DecoderBottleneckLayer(in_channels=912, out_channels=408)
-        self.up2 = DecoderBottleneckLayer(in_channels=408, out_channels=168)
-        self.up1 = DecoderBottleneckLayer(in_channels=168, out_channels=72)
+        self.up3 = DecoderBottleneckLayer(in_channels=1008, out_channels=432)
+        self.up2 = DecoderBottleneckLayer(in_channels=432 , out_channels=92)
+        self.up1 = DecoderBottleneckLayer(in_channels=92  , out_channels=96)
 
-        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(72, 32, 3, 2, 1, 1),
+        self.tp_conv1 = nn.Sequential(nn.ConvTranspose2d(96, 32, 3, 2, 1, 1),
                                       nn.BatchNorm2d(32),
                                       nn.ReLU(inplace=True),)
         self.conv2 = nn.Sequential(nn.Conv2d(32, 32, 3, 1, 1),
