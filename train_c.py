fd8ef7300f013d8aadea298c5879adcdeea1b57f
saleknia <salekniaamirhossein@gmail.com>
2023-06-07 22:17:57 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -390,7 +390,8 @@ def main(args):
     elif TASK_NAME=='MIT-67':
 
         transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            # transforms.RandomResizedCrop(224),
+            transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
             transforms.RandomGrayscale(p=0.2),
