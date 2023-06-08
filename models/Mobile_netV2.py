c8346b06b461e1ec03befadb8a1ed78686201b9c
saleknia <salekniaamirhossein@gmail.com>
2023-06-08 11:39:23 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -17,11 +17,11 @@ class Mobile_netV2(nn.Module):
 
         # model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
 
-        model = efficientnet_b0(weights=EfficientNet_B0_Weights)
+        model = efficientnet_b1(weights=EfficientNet_B1_Weights)
         
         # model = torchvision.models.convnext_tiny(weights='DEFAULT')
 
-        model.features[0][0].stride = (1, 1)
+        # model.features[0][0].stride = (1, 1)
 
         self.features = model.features
 
