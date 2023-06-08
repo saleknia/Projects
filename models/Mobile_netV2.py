ec7ccf65d82c112c11ac360b10415f5ccf74025a
saleknia <salekniaamirhossein@gmail.com>
2023-06-08 15:56:42 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -47,7 +47,7 @@ class Mobile_netV2(nn.Module):
 
         self.model = model
 
-        for param in self.teacher.parameters():
+        for param in self.model.parameters():
             param.requires_grad = False
 
         self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
