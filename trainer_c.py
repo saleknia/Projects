32ba597a9d3139773fb20a2342ca69fe64346d34
saleknia <salekniaamirhossein@gmail.com>
2023-06-08 07:49:07 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -207,7 +207,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
 
         # loss_ce = ce_loss(outputs, label_smoothing(targets.long(), outputs_t))
 
-        loss_ce = loss_label_smoothing(outputs=outputs, labels=targets.long(), alpha=0.0)
+        loss_ce = loss_label_smoothing(outputs=outputs, labels=targets.long(), alpha=0.1)
 
 
 
