72908d6788f211668c5c649cd38998d76c5132cd
saleknia <salekniaamirhossein@gmail.com>
2023-06-10 12:59:38 +0000

    Commit Process

--- models/Mobile_netV2_loss.py
+++ models/Mobile_netV2_loss.py
@@ -212,7 +212,8 @@ class Mobile_netV2_loss(nn.Module):
         c58 = torch.softmax((x0 + x1 + x2 + x3 + x4 + x5) / 6.0, dim=1)
 
 
-        x  = (c4 + c5 + c6 + c7) / 4.0
+        x  = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12 + c13 + c14 + c15 + c16 + c17 + c18 + c19 + c20 
+        x  = x + c21 + c22 + c23 + c24 + c25 + c26 + c27 + c28 + c29 + c30 + c31 + c32 + c33 + c34 + c35 + c36 + c37 + c38 + c39 + c40 + c41 + c42 + c43 + c44 + c45 + c46 + c47 + c48 + c49 + c50 + c51 + c52 + c53 + c54 + c55 + c56 + c57 + c58 
 
         # x = c7
 
