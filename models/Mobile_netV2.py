0d0f3391ea07869be8ea3becb5b3a42d685c2f0a
saleknia <salekniaamirhossein@gmail.com>
2023-06-11 14:12:27 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -77,7 +77,7 @@ class Mobile_netV2(nn.Module):
         # for param in self.features[0:6].parameters():
         #     param.requires_grad = False
 
-        # self.avgpool = model.avgpool
+
 
         # self.features[0][0].stride = (1, 1)
 
@@ -105,7 +105,8 @@ class Mobile_netV2(nn.Module):
         #     param.requires_grad = True
 
         self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
-
+        self.avgpool = model.avgpool
+        
         # self.classifier = nn.Sequential(
         #     nn.Dropout(p=0.5, inplace=True),
         #     nn.Linear(in_features=1280, out_features=num_classes, bias=True))
