dcaa01172380d58d4da735de0353a07bfb67cb9c
saleknia <salekniaamirhossein@gmail.com>
2023-06-12 21:14:49 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -83,11 +83,11 @@ class Mobile_netV2(nn.Module):
 
         # model = resnet18(num_classes=365)
 
-        model_place = models.__dict__['resnet50'](num_classes=365)
+        # model_place = models.__dict__['resnet50'](num_classes=365)
 
-        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
-        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-        model_place.load_state_dict(state_dict)
+        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
+        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        # model_place.load_state_dict(state_dict)
 
         # model = models.__dict__['densenet161'](num_classes=365)
 
@@ -97,9 +97,9 @@ class Mobile_netV2(nn.Module):
         # state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
         # model.load_state_dict(state_dict)
 
-        model_seg =  ModelBuilder.build_encoder(arch='resnet50', fc_dim=2048, weights='/content/encoder_epoch_30.pth')
+        # model_seg =  ModelBuilder.build_encoder(arch='resnet50', fc_dim=2048, weights='/content/encoder_epoch_30.pth')
 
-        model_cls = torchvision.models.resnet50(weights='DEFAULT')
+        model = torchvision.models.resnet18(weights='DEFAULT')
 
         # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
         # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
@@ -113,35 +113,37 @@ class Mobile_netV2(nn.Module):
         # print(model_seg)
         # print(model_place)
 
-        self.model_place = model_place
-        self.model_seg   = model_seg
-        self.model_cls   = model_cls
+        self.model = model
+
+        # self.model_place = model_place
+        # self.model_seg   = model_seg
+        # self.model_cls   = model_cls
 
         # print(model)
 
-        for param in self.model_place.parameters():
-            param.requires_grad = False
+        # for param in self.model_place.parameters():
+        #     param.requires_grad = False
 
-        for param in self.model_seg.parameters():
-            param.requires_grad = False
+        # for param in self.model_seg.parameters():
+        #     param.requires_grad = False
 
-        for param in self.model_cls.parameters():
-            param.requires_grad = False
+        # for param in self.model_cls.parameters():
+        #     param.requires_grad = False
 
-        # for param in self.model.layer4.parameters():
-        #     param.requires_grad = True
+        for param in self.model.parameters():
+            param.requires_grad = True
 
         # for param in self.model.features.denseblock4.parameters():
         #     param.requires_grad = True
 
-        # self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=512, out_features=num_classes, bias=True))
-        # self.avgpool = model.avgpool
+        self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=512, out_features=num_classes, bias=True))
+        self.avgpool = model.avgpool
 
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+        # self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
 
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=2048, out_features=num_classes, bias=True))
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=2048, out_features=num_classes, bias=True))
 
         # self.classifier = nn.Sequential(
         #     nn.Dropout(p=0.5, inplace=True),
@@ -156,43 +158,54 @@ class Mobile_netV2(nn.Module):
     def forward(self, x0):
         b, c, w, h = x0.shape
 
-        x_seg   = self.model_seg(x0)[0]
+        x = self.model.conv1(x0)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        x = self.model.maxpool(x)
 
-        x_place = self.model_place.conv1(x0)
-        x_place = self.model_place.bn1(x_place)
-        x_place = self.model_place.relu(x_place)
-        x_place = self.model_place.maxpool(x_place)
-        x_place = self.model_place.layer1(x_place)
-        x_place = self.model_place.layer2(x_place)
-        x_place = self.model_place.layer3(x_place)
-        x_place = self.model_place.layer4(x_place)
+        x1 = self.model.layer1(x)
+        x2 = self.model.layer2(x1)
+        x3 = self.model.layer3(x2)
+        x4 = self.model.layer4(x3)
+
+        # x_seg   = self.model_seg(x0)[0]
+
+        # x_place = self.model_place.conv1(x0)
+        # x_place = self.model_place.bn1(x_place)
+        # x_place = self.model_place.relu(x_place)
+        # x_place = self.model_place.maxpool(x_place)
+        # x_place = self.model_place.layer1(x_place)
+        # x_place = self.model_place.layer2(x_place)
+        # x_place = self.model_place.layer3(x_place)
+        # x_place = self.model_place.layer4(x_place)
 
         # x1 = self.features[0:4](x0)
         # x2 = self.features[4:6](x1)
         # x3 = self.features[6:9](x2)
 
-        x_cls = self.model_cls.conv1(x0)
-        x_cls = self.model_cls.bn1(x_cls)
-        x_cls = self.model_cls.relu(x_cls)
-        x_cls = self.model_cls.maxpool(x_cls)
-        x_cls = self.model_cls.layer1(x_cls)
-        x_cls = self.model_cls.layer2(x_cls)
-        x_cls = self.model_cls.layer3(x_cls)
-        x_cls = self.model_cls.layer4(x_cls)
+        # x_cls = self.model_cls.conv1(x0)
+        # x_cls = self.model_cls.bn1(x_cls)
+        # x_cls = self.model_cls.relu(x_cls)
+        # x_cls = self.model_cls.maxpool(x_cls)
+        # x_cls = self.model_cls.layer1(x_cls)
+        # x_cls = self.model_cls.layer2(x_cls)
+        # x_cls = self.model_cls.layer3(x_cls)
+        # x_cls = self.model_cls.layer4(x_cls)
 
         # x_t, x1_t, x2_t, x3_t = self.teacher(x0)
 
         # 
         # x_place = self.model.place(x0)
 
-        x = x_seg + x_place + x_cls
+        # x = x_seg + x_place + x_cls
 
         # x = torch.cat([x_seg, x_place], dim=1)
 
-        x = self.avgpool(x)
+        x = self.avgpool(x4)
         x = x.view(x.size(0), -1)
-        # x = self.model.fc(x)
-        x = self.classifier(x)
+        x = self.model.fc(x)
+
+        # x = self.classifier(x)
 
         return x
 
