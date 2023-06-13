157f42fb5660d00d909e6080b95a2369a3c4ca94
saleknia <salekniaamirhossein@gmail.com>
2023-06-13 17:37:27 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -99,16 +99,20 @@ class Mobile_netV2(nn.Module):
 
         # model_seg =  ModelBuilder.build_encoder(arch='resnet50', fc_dim=2048, weights='/content/encoder_epoch_30.pth')
 
-        model = torchvision.models.resnet18(weights='DEFAULT')
+        # model = torchvision.models.resnet18(weights='DEFAULT')
 
-        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
-        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-        # model.load_state_dict(state_dict)
+        model = resnet18(num_classes=365)
+
+        checkpoint = torch.load('/content/wideresnet18_places365.pth.tar', map_location='cpu')
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        model.load_state_dict(state_dict)
 
         # hacky way to deal with the upgraded batchnorm2D and avgpool layers...
-        # for i, (name, module) in enumerate(model._modules.items()):
-        #     module = recursion_change_bn(model)
-        # model.avgpool = torch.nn.AvgPool2d(kernel_size=14, stride=1, padding=0)
+
+        for i, (name, module) in enumerate(model._modules.items()):
+            module = recursion_change_bn(model)
+
+        model.avgpool = torch.nn.AvgPool2d(kernel_size=14, stride=1, padding=0)
 
         # print(model_seg)
         # print(model_place)
@@ -131,10 +135,10 @@ class Mobile_netV2(nn.Module):
         #     param.requires_grad = False
 
         for param in self.model.parameters():
-            param.requires_grad = True
+            param.requires_grad = False
 
-        # for param in self.model.features.denseblock4.parameters():
-        #     param.requires_grad = True
+        for param in self.model.layer4.parameters():
+            param.requires_grad = True
 
         self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=512, out_features=num_classes, bias=True))
         self.avgpool = model.avgpool
@@ -158,61 +162,62 @@ class Mobile_netV2(nn.Module):
     def forward(self, x0):
         b, c, w, h = x0.shape
 
-        x = self.model.conv1(x0)
-        x = self.model.bn1(x)
-        x = self.model.relu(x)
-        x = self.model.maxpool(x)
+        # x = self.model.conv1(x0)
+        # x = self.model.bn1(x)
+        # x = self.model.relu(x)
+        # x = self.model.maxpool(x)
 
-        x1 = self.model.layer1(x)
-        x2 = self.model.layer2(x1)
-        x3 = self.model.layer3(x2)
-        x4 = self.model.layer4(x3)
+        # x1 = self.model.layer1(x)
+        # x2 = self.model.layer2(x1)
+        # x3 = self.model.layer3(x2)
+        # x4 = self.model.layer4(x3)
 
-        # x_seg   = self.model_seg(x0)[0]
+        # # x_seg   = self.model_seg(x0)[0]
 
-        # x_place = self.model_place.conv1(x0)
-        # x_place = self.model_place.bn1(x_place)
-        # x_place = self.model_place.relu(x_place)
-        # x_place = self.model_place.maxpool(x_place)
-        # x_place = self.model_place.layer1(x_place)
-        # x_place = self.model_place.layer2(x_place)
-        # x_place = self.model_place.layer3(x_place)
-        # x_place = self.model_place.layer4(x_place)
+        # # x_place = self.model_place.conv1(x0)
+        # # x_place = self.model_place.bn1(x_place)
+        # # x_place = self.model_place.relu(x_place)
+        # # x_place = self.model_place.maxpool(x_place)
+        # # x_place = self.model_place.layer1(x_place)
+        # # x_place = self.model_place.layer2(x_place)
+        # # x_place = self.model_place.layer3(x_place)
+        # # x_place = self.model_place.layer4(x_place)
 
-        # x1 = self.features[0:4](x0)
-        # x2 = self.features[4:6](x1)
-        # x3 = self.features[6:9](x2)
+        # # x1 = self.features[0:4](x0)
+        # # x2 = self.features[4:6](x1)
+        # # x3 = self.features[6:9](x2)
 
-        # x_cls = self.model_cls.conv1(x0)
-        # x_cls = self.model_cls.bn1(x_cls)
-        # x_cls = self.model_cls.relu(x_cls)
-        # x_cls = self.model_cls.maxpool(x_cls)
-        # x_cls = self.model_cls.layer1(x_cls)
-        # x_cls = self.model_cls.layer2(x_cls)
-        # x_cls = self.model_cls.layer3(x_cls)
-        # x_cls = self.model_cls.layer4(x_cls)
+        # # x_cls = self.model_cls.conv1(x0)
+        # # x_cls = self.model_cls.bn1(x_cls)
+        # # x_cls = self.model_cls.relu(x_cls)
+        # # x_cls = self.model_cls.maxpool(x_cls)
+        # # x_cls = self.model_cls.layer1(x_cls)
+        # # x_cls = self.model_cls.layer2(x_cls)
+        # # x_cls = self.model_cls.layer3(x_cls)
+        # # x_cls = self.model_cls.layer4(x_cls)
 
-        x1_t, x2_t, x3_t, x4_t = self.teacher(x0)
+        # x1_t, x2_t, x3_t, x4_t = self.teacher(x0)
 
-        # 
-        # x_place = self.model.place(x0)
+        # #
 
-        # x = x_seg + x_place + x_cls
+        x = self.model(x0)
 
-        # x = torch.cat([x_seg, x_place], dim=1)
+        # # x = x_seg + x_place + x_cls
 
-        x = self.avgpool(x4)
-        x = x.view(x.size(0), -1)
-        x = self.model.fc(x)
+        # # x = torch.cat([x_seg, x_place], dim=1)
+
+        # x = self.avgpool(x4)
+        # x = x.view(x.size(0), -1)
+        # x = self.model.fc(x)
 
         # x = self.classifier(x)
 
-        # return x
+        return x
 
-        if self.training:
-            return x, x1, x2, x3, x4, x1_t, x2_t, x3_t, x4_t
-        else:
-            return x
+        # if self.training:
+        #     return x, x1, x2, x3, x4, x1_t, x2_t, x3_t, x4_t
+        # else:
+        #     return x
 
 
 # class Mobile_netV2(nn.Module):
