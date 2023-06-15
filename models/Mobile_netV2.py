e28b8233809cd97dc4d4317303c3bec7275920db
saleknia <salekniaamirhossein@gmail.com>
2023-06-15 00:06:54 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -137,8 +137,8 @@ class Mobile_netV2(nn.Module):
         # for param in self.model.parameters():
         #     param.requires_grad = False
 
-        for param in self.model.layer4.parameters():
-            param.requires_grad = True
+        # for param in self.model.layer4.parameters():
+        #     param.requires_grad = True
 
         self.model.classifier = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2208, out_features=num_classes, bias=True))
 
