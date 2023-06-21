1712216581bd8c231e3f5a1963be8a2dd883bc95
saleknia <salekniaamirhossein@gmail.com>
2023-06-21 18:52:47 +0000

    Commit Process

--- models/SEUNet_lite.py
+++ models/SEUNet_lite.py
@@ -7,39 +7,6 @@ from torch.nn import Softmax
 import einops
 import timm
 
-class DecoderBottleneckLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, use_transpose=True):
-        super(DecoderBottleneckLayer, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
-        self.norm1 = nn.BatchNorm2d(in_channels // 4)
-        self.relu1 = nn.ReLU(inplace=True)
-
-        if use_transpose:
-            self.up = nn.Sequential(
-                nn.ConvTranspose2d(
-                    in_channels // 4, in_channels // 4, 3, stride=2, padding=1, output_padding=1
-                ),
-                nn.BatchNorm2d(in_channels // 4),
-                nn.ReLU(inplace=True)
-            )
-        else:
-            self.up = nn.Upsample(scale_factor=2, align_corners=True, mode="bilinear")
-
-        self.conv3 = nn.Conv2d(in_channels // 4, out_channels, 1)
-        self.norm3 = nn.BatchNorm2d(out_channels)
-        self.relu3 = nn.ReLU(inplace=True)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.norm1(x)
-        x = self.relu1(x)
-        x = self.up(x)
-        x = self.conv3(x)
-        x = self.norm3(x)
-        x = self.relu3(x)
-        return x
-
 class LayerNormProxy(nn.Module):
     
     def __init__(self, dim):
@@ -52,7 +19,53 @@ class LayerNormProxy(nn.Module):
         x = einops.rearrange(x, 'b c h w -> b h w c')
         x = self.norm(x)
         return einops.rearrange(x, 'b h w c -> b c h w')
-        
+
+class ConvBatchNorm(nn.Module):
+    """(convolution => [BN] => ReLU)"""
+
+    def __init__(self, in_channels, out_channels, activation='ReLU', kernel_size=3, padding=1):
+        super(ConvBatchNorm, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding)
+        self.norm = nn.BatchNorm2d(out_channels)
+        self.activation = get_activation(activation)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.norm(out)
+        return self.activation(out)
+
+def get_activation(activation_type):
+    activation_type = activation_type.lower()
+    if hasattr(nn, activation_type):
+        return getattr(nn, activation_type)()
+    else:
+        return nn.ReLU()
+
+def _make_nConv(in_channels, out_channels, nb_Conv, activation='ReLU'):
+    layers = []
+    layers.append(ConvBatchNorm(in_channels, out_channels, activation))
+
+    for _ in range(nb_Conv - 1):
+        layers.append(ConvBatchNorm(out_channels, out_channels, activation))
+    return nn.Sequential(*layers)
+
+
+class UpBlock(nn.Module):
+    """Upscaling then conv"""
+
+    def __init__(self, in_channels, out_channels, nb_Conv=2, activation='ReLU'):
+        super(UpBlock, self).__init__()
+
+        # self.up = nn.Upsample(scale_factor=2)
+
+        self.up     = nn.ConvTranspose2d(in_channels,in_channels//2,(2,2),2)
+        self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
+
+    def forward(self, x, skip_x):
+        out = self.up(x)
+        x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
+        return self.nConvs(x)
+
 class SEUNet_lite(nn.Module):
     def __init__(self, n_channels=1, n_classes=9):
         '''
@@ -71,6 +84,9 @@ class SEUNet_lite(nn.Module):
         pretrained_teacher = loaded_data_teacher['net']
         resnet.load_state_dict(pretrained_teacher)
 
+        for param in resnet.parameters():
+            param.requires_grad = False
+
         self.firstconv = resnet.model.conv1
         self.firstbn   = resnet.model.bn1
         self.firstrelu = resnet.model.relu
@@ -80,9 +96,9 @@ class SEUNet_lite(nn.Module):
         self.encoder3  = resnet.model.layer3
         self.encoder4  = resnet.model.layer4
 
-        self.up3 = DecoderBottleneckLayer(in_channels=512, out_channels=256)
-        self.up2 = DecoderBottleneckLayer(in_channels=256, out_channels=128)
-        self.up1 = DecoderBottleneckLayer(in_channels=128, out_channels=64 )
+        self.up3 = UpBlock(in_channels=512, out_channels=256)
+        self.up2 = UpBlock(in_channels=256, out_channels=128)
+        self.up1 = UpBlock(in_channels=128, out_channels=64 )
         
         self.final_conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
         self.final_relu1 = nn.ReLU(inplace=True)
