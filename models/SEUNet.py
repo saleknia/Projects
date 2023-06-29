06521f386aec7e3427b0c998a8970cea13b0b210
saleknia <salekniaamirhossein@gmail.com>
2023-06-29 14:29:23 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -89,7 +89,7 @@ class SEUNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
 
-        x0 = self.layer40(x)
+        x0 = self.layer4(x)
         x0 = self.avgpool_0(x0)
         x0 = x0.view(x0.size(0), -1)
         x0 = self.fc_0(x0)
