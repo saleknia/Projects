eb6442146b26124e4a6b0bb4c323867da8647bb5
saleknia <salekniaamirhossein@gmail.com>
2023-06-30 00:11:03 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -207,32 +207,13 @@ class Mobile_netV2(nn.Module):
         # state_dict = torch.load('/content/drive/MyDrive/checkpoint_1/Mobile_NetV2_MIT-67_best.pth', map_location='cpu')['net']
         # self.load_state_dict(state_dict)
 
-        # model = timm.create_model('mvitv2_base', pretrained=True)
-
-        # self.model = model 
-
-        # self.model.head = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=768, out_features=num_classes, bias=True))
-
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
-
-        # for param in self.model.stages[3].parameters():
-        #     param.requires_grad = True
-
-        # for param in self.model.head.parameters():
-        #     param.requires_grad = True
-
-        # model = timm.create_model('convnextv2_tiny', pretrained=True)
-        
-        model = timm.create_model('convnextv2_nano.fcmae_ft_in1k', pretrained=True)
+        model = timm.create_model('mvitv2_small', pretrained=True)
 
         self.model = model 
 
-        self.model.head.fc = nn.Sequential(
+        self.model.head = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=640, out_features=num_classes, bias=True))
+            nn.Linear(in_features=768, out_features=num_classes, bias=True))
 
         for param in self.model.parameters():
             param.requires_grad = False
@@ -240,11 +221,30 @@ class Mobile_netV2(nn.Module):
         for param in self.model.stages[3].parameters():
             param.requires_grad = True
 
-        # for param in self.model.stages[2].parameters():
-        #     param.requires_grad = True
-
         for param in self.model.head.parameters():
             param.requires_grad = True
+
+        # model = timm.create_model('convnextv2_tiny', pretrained=True)
+        
+        # model = timm.create_model('convnextv2_nano.fcmae_ft_in1k', pretrained=True)
+
+        # self.model = model 
+
+        # self.model.head.fc = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=640, out_features=num_classes, bias=True))
+
+        # for param in self.model.parameters():
+        #     param.requires_grad = False
+
+        # for param in self.model.stages[3].parameters():
+        #     param.requires_grad = True
+
+        # # for param in self.model.stages[2].parameters():
+        # #     param.requires_grad = True
+
+        # for param in self.model.head.parameters():
+        #     param.requires_grad = True
 
 
     def forward(self, x0):
