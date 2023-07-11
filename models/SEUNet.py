ddb3004bf3142a785795877af1da49024e8bb1f9
saleknia <salekniaamirhossein@gmail.com>
2023-07-11 10:18:33 +0000

    Commit Process

--- models/SEUNet.py
+++ models/SEUNet.py
@@ -307,48 +307,48 @@ import ttach as tta
 #         else:
 #             return x_t
 
-# class SEUNet(nn.Module):
-#     def __init__(self, num_classes=67, pretrained=True):
-#         super(SEUNet, self).__init__()
+class dense_model(nn.Module):
+    def __init__(self, num_classes=67, pretrained=True):
+        super(dense_model, self).__init__()
 
-#         model_dense = models.__dict__['densenet161'](num_classes=365)
+        model_dense = models.__dict__['densenet161'](num_classes=365)
 
-#         checkpoint = torch.load('/content/densenet161_places365.pth.tar', map_location='cpu')
-#         state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-#         state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
-#         state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
-#         model_dense.load_state_dict(state_dict)
+        checkpoint = torch.load('/content/densenet161_places365.pth.tar', map_location='cpu')
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
+        state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
+        model_dense.load_state_dict(state_dict)
 
-#         self.dense = model_dense
+        self.dense = model_dense
 
-#         for param in self.dense.parameters():
-#             param.requires_grad = False
+        for param in self.dense.parameters():
+            param.requires_grad = False
 
-#         for i, module in enumerate(self.dense.features.denseblock4):
-#             if 12 <= i: 
-#                 for param in self.dense.features.denseblock4[module].parameters():
-#                     param.requires_grad = True
+        for i, module in enumerate(self.dense.features.denseblock4):
+            if 12 <= i: 
+                for param in self.dense.features.denseblock4[module].parameters():
+                    param.requires_grad = True
 
-#         self.dense.classifier = nn.Sequential(
-#             nn.Dropout(p=0.5, inplace=True),
-#             nn.Linear(in_features=2208, out_features=num_classes, bias=True))
+        self.dense.classifier = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=2208, out_features=num_classes, bias=True))
 
-#         # checkpoint = torch.load('/content/drive/MyDrive/checkpoint_dense_ensemble/22_best.pth', map_location='cpu')
-#         # self.load_state_dict(checkpoint['net'])
+        checkpoint = torch.load('/content/drive/MyDrive/checkpoint_dense_ensemble/12_best.pth', map_location='cpu')
+        self.load_state_dict(checkpoint['net'])
 
-#         # for param in self.dense.parameters():
-#         #     param.requires_grad = False
+        for param in self.dense.parameters():
+            param.requires_grad = False
 
-#     def forward(self, x0):
-#         b, c, w, h = x0.shape
+    def forward(self, x0):
+        b, c, w, h = x0.shape
 
-#         x_dense = self.dense(x0)
+        x_dense = self.dense(x0)
         
-#         return x_dense
+        return x_dense
 
-class SEUNet(nn.Module):
+class res_model(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
-        super(SEUNet, self).__init__()
+        super(res_model, self).__init__()
 
         ###############################################################################################
         ###############################################################################################
@@ -384,11 +384,11 @@ class SEUNet(nn.Module):
 
         self.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=67, bias=True))
 
-        # checkpoint = torch.load('/content/drive/MyDrive/checkpoint_dense_ensemble/res_50.pth', map_location='cpu')
-        # self.load_state_dict(checkpoint['net'])
+        checkpoint = torch.load('/content/drive/MyDrive/checkpoint_dense_ensemble/res_50.pth', map_location='cpu')
+        self.load_state_dict(checkpoint['net'])
 
-        # for param in self.parameters():
-        #     param.requires_grad = False
+        for param in self.parameters():
+            param.requires_grad = False
 
     def forward(self, x0):
         b, c, w, h = x0.shape
@@ -409,9 +409,9 @@ class SEUNet(nn.Module):
 
         return x
 
-class res_model_distilled(nn.Module):
+class SEUNet(nn.Module):
     def __init__(self, num_classes=40, pretrained=True):
-        super(res_model_distilled, self).__init__()
+        super(SEUNet, self).__init__()
 
         ###############################################################################################
         ###############################################################################################
@@ -424,12 +424,8 @@ class res_model_distilled(nn.Module):
         for param in model.parameters():
             param.requires_grad = False
 
-        for param in model.layer4[-1].conv2.parameters():
+        for param in model.layer4[-1].parameters():
             param.requires_grad = True
-
-        for param in model.layer4[-1].conv3.parameters():
-            param.requires_grad = True
-
         ###############################################################################################
         ###############################################################################################
 
