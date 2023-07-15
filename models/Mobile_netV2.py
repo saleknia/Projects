c185fbfd5ae4021cdc42a3d1aedc59e8d34c96ee
saleknia <salekniaamirhossein@gmail.com>
2023-07-15 18:11:13 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -252,11 +252,11 @@ class Mobile_netV2(nn.Module):
 
         # model = timm.create_model('convnextv2_tiny', pretrained=True)
         
-        model = timm.create_model('convnextv2_tiny.fcmae_ft_in1k', pretrained=True)
+        model = timm.create_model('convnextv2_nano.fcmae_ft_in1k', pretrained=True)
 
         self.model = model 
 
-        self.model.head.fc     = nn.Sequential(nn.Linear(in_features=768, out_features=num_classes, bias=True))
+        self.model.head.fc     = nn.Sequential(nn.Linear(in_features=640, out_features=num_classes, bias=True))
         self.model.head.drop.p = 0.5
 
         for param in self.model.parameters():
