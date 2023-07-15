146c922033b5f6dbe1af72b70f794922e8cba002
saleknia <salekniaamirhossein@gmail.com>
2023-07-15 21:11:47 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -259,10 +259,10 @@ class Mobile_netV2(nn.Module):
         self.model.head.fc     = nn.Sequential(nn.Linear(in_features=640, out_features=num_classes, bias=True))
         self.model.head.drop.p = 0.5
 
-        for param in self.model.stages[3][1].parameters():
+        for param in self.model.stages[3].blocks[1].parameters():
             param.requires_grad = True
 
-        for param in self.model.stages[2][-1].parameters():
+        for param in self.model.stages[2].blocks[-1].parameters():
             param.requires_grad = True
 
         for param in self.model.head.parameters():
