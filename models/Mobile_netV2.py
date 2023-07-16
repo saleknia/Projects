01703a35bf257f9d35aeb2cb0a5ae1e1f595e894
saleknia <salekniaamirhossein@gmail.com>
2023-07-16 19:21:59 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -110,7 +110,7 @@ class Mobile_netV2(nn.Module):
 
         model = efficientnet_b2(weights=EfficientNet_B2_Weights)
 
-        model.features[0][0].stride = (1, 1)
+        # model.features[0][0].stride = (1, 1)
 
         self.features = model.features
 
@@ -290,7 +290,7 @@ class Mobile_netV2(nn.Module):
         # x2 = self.features[4:6](x1)
         # x3 = self.features[6:9](x2)
 
-        x_t = self.teacher(x0)
+        # x_t = self.teacher(x0)
 
         # x = self.avgpool(x3)
         # x = x.view(x.size(0), -1)
@@ -310,12 +310,12 @@ class Mobile_netV2(nn.Module):
 
         # x = self.convnext(x0)
 
-        # return x
+        return x
 
-        if self.training:
-            return x, x_t
-        else:
-            return x
+        # if self.training:
+        #     return x, x_t
+        # else:
+        #     return x
 
 
 class mvit_small(nn.Module):
