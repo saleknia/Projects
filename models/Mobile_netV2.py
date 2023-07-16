eb631d77f5a6acb4c7089aece28cdf28fbf6f8dd
saleknia <salekniaamirhossein@gmail.com>
2023-07-16 10:48:53 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -108,7 +108,7 @@ class Mobile_netV2(nn.Module):
 
         # model = efficientnet_v2_s(weights=EfficientNet_V2_S_Weights)
 
-        model = efficientnet_b2(weights=EfficientNet_B2_Weights)
+        model = efficientnet_b1(weights=EfficientNet_B1_Weights)
 
         model.features[0][0].stride = (1, 1)
 
@@ -121,7 +121,7 @@ class Mobile_netV2(nn.Module):
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=1408, out_features=num_classes, bias=True))
+            nn.Linear(in_features=1280, out_features=num_classes, bias=True))
 
         ############################################################
         ############################################################
