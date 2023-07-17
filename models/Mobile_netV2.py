49a397a8126d55a7a83ceac28e87bb46c3fa5a5c
saleknia <salekniaamirhossein@gmail.com>
2023-07-17 06:43:13 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -263,11 +263,11 @@ class Mobile_netV2(nn.Module):
         for param in self.model.parameters():
             param.requires_grad = False
 
-        for param in self.model.stages[3].blocks[0].parameters():
+        for param in self.model.stages[3].parameters():
             param.requires_grad = True
 
-        for param in self.model.stages[2].blocks[7].parameters():
-            param.requires_grad = True
+        # for param in self.model.stages[2].blocks[7].parameters():
+        #     param.requires_grad = True
 
         for param in self.model.head.parameters():
             param.requires_grad = True
--- trainer_c.py
+++ trainer_c.py
@@ -334,6 +334,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
 
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 2.0)
         optimizer.step()
 
         # scaler.scale(loss).backward()
