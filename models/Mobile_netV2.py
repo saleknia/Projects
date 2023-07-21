fb7a82cc079e9266822d7cc02f06d4696dc9252d
saleknia <salekniaamirhossein@gmail.com>
2023-07-21 07:27:52 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -72,7 +72,7 @@ class Mobile_netV2(nn.Module):
         ############################################################
         ############################################################
 
-        # model = torchvision.models.convnext_small(weights='DEFAULT')
+        # model = torchvision.models.convnext_tiny(weights='DEFAULT')
 
         # self.model = model 
 
@@ -85,11 +85,11 @@ class Mobile_netV2(nn.Module):
         # for param in self.model.parameters():
         #     param.requires_grad = False
 
-        # # for param in self.model.features[5][6:9].parameters():
-        # #     param.requires_grad = True
-
-        # for param in self.model.features[5][18:27].parameters():
+        # for param in self.model.features[5][6:9].parameters():
         #     param.requires_grad = True
+
+        # # for param in self.model.features[5][18:27].parameters():
+        # #     param.requires_grad = True
 
         # for param in self.model.features[6].parameters():
         #     param.requires_grad = True
@@ -275,14 +275,14 @@ class Mobile_netV2(nn.Module):
         for param in self.model.head.parameters():
             param.requires_grad = True
 
-        # # self.convnext = convnext_small()
-        # # self.mvit = mvit_small()
+        # self.convnext = convnext_small()
+        # self.mvit = mvit_small()
 
         # self.teacher = efficientnet_teacher()
-        # self.teacher = convnext_teacher()
+        self.teacher = convnext_teacher()
 
-        # for param in self.teacher.parameters():
-        #     param.requires_grad = False
+        for param in self.teacher.parameters():
+            param.requires_grad = False
 
     def forward(self, x0):
         b, c, w, h = x0.shape
@@ -296,7 +296,7 @@ class Mobile_netV2(nn.Module):
         # x2 = self.features[4:6](x1)
         # x3 = self.features[6:9](x2)
 
-        # x_t = self.teacher(x0)
+        x_t = self.teacher(x0)
 
         # x = self.avgpool(x3)
         # x = x.view(x.size(0), -1)
@@ -316,12 +316,12 @@ class Mobile_netV2(nn.Module):
 
         # x = self.convnext(x0)
 
-        return x
+        # return x
 
-        # if self.training:
-        #     return x, x_t
-        # else:
-        #     return x
+        if self.training:
+            return x, x_t
+        else:
+            return x
 
 
 class mvit_small(nn.Module):
