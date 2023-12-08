72136dc408ddd2d1c93997395be24e3b19b696fc
saleknia <salekniaamirhossein@gmail.com>
2023-12-08 17:57:26 +0000

    Commit Process

--- trainer_c.py
+++ trainer_c.py
@@ -251,7 +251,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
 
         # loss_ce = ce_loss(outputs, label_smoothing(targets.long(), outputs_t))
 
-        loss_ce = loss_label_smoothing(outputs=outputs, labels=targets.long(), alpha=0.1)
+        loss_ce = loss_label_smoothing(outputs=outputs, labels=targets.long(), alpha=0.0)
 
         # loss_ce = torch.nn.functional.mse_loss(outputs, outputs_t, size_average=None, reduce=None, reduction='mean')
         
@@ -320,7 +320,7 @@ def trainer(end_epoch,epoch_num,model,teacher_model,dataloader,optimizer,device,
         loss = loss_ce + loss_disparity
         ###############################################
 
-        lr_ = 0.01 * (1.0 - iter_num / max_iterations) ** 0.9     
+        lr_ = 0.0001 * (1.0 - iter_num / max_iterations) ** 0.9     
         for param_group in optimizer.param_groups:
             param_group['lr'] = lr_
         iter_num = iter_num + 1   
