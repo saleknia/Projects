f06ec3c1711212a0e940cd02abf3472892580c0e
saleknia <salekniaamirhossein@gmail.com>
2023-12-25 18:35:55 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -42,24 +42,24 @@ class Mobile_netV2(nn.Module):
 
 
 
-        # model = models.__dict__['resnet50'](num_classes=365)
-        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
-        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        model = models.__dict__['resnet50'](num_classes=365)
+        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 
-        # # state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
-        # # state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
+        # state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
+        # state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
 
-        # model.load_state_dict(state_dict)
+        model.load_state_dict(state_dict)
 
-        # self.model = model
+        self.model = model
 
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-        # for param in self.model.layer4[-1].parameters():
-        #     param.requires_grad = True
+        for param in self.model.layer4[-1].parameters():
+            param.requires_grad = True
 
-        # self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
+        self.model.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
 
         # # self.teacher.conv1.stride = (1, 1)
 
@@ -227,7 +227,7 @@ class Mobile_netV2(nn.Module):
         #     nn.Dropout(p=0.5, inplace=True),
         #     nn.Linear(in_features=768, out_features=num_classes, bias=True))
 
-        self.model = B2()
+        # self.model = B2()
 
         #################################################################################
         #################################################################################
