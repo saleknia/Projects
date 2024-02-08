24933a19d8e9f87d1f27d1d1e4aab013790c3442
saleknia <salekniaamirhossein@gmail.com>
2024-02-08 14:39:48 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -209,33 +209,9 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('mvitv2_small', pretrained=True)
-
-        self.model = model 
-
-        for param in self.model.parameters():
-            param.requires_grad = False
-
-        for param in self.model.stages[3].parameters():
-            param.requires_grad = True
-
-        self.model.head = nn.Sequential(
-            nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=768, out_features=num_classes, bias=True))
-
-        # self.model   = B2()
-        # self.teacher = B5()
-
-        #################################################################################
-        #################################################################################
-
-        # model = timm.create_model('convnext_tiny.fb_in1k', pretrained=True)
+        # model = timm.create_model('mvitv2_small', pretrained=True)
 
         # self.model = model 
-
-        # self.model.head.fc     = nn.Sequential(nn.Linear(in_features=768, out_features=num_classes, bias=True))
-
-        # self.model.head.drop.p = 0.5
 
         # for param in self.model.parameters():
         #     param.requires_grad = False
@@ -243,8 +219,32 @@ class Mobile_netV2(nn.Module):
         # for param in self.model.stages[3].parameters():
         #     param.requires_grad = True
 
-        # for param in self.model.head.parameters():
-        #     param.requires_grad = True
+        # self.model.head = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=768, out_features=num_classes, bias=True))
+
+        # self.model   = B2()
+        # self.teacher = B5()
+
+        #################################################################################
+        #################################################################################
+
+        model = timm.create_model('convnext_small.fb_in1k', pretrained=True)
+
+        self.model = model 
+
+        self.model.head.fc     = nn.Sequential(nn.Linear(in_features=768, out_features=num_classes, bias=True))
+
+        self.model.head.drop.p = 0.5
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        for param in self.model.stages[3].parameters():
+            param.requires_grad = True
+
+        for param in self.model.head.parameters():
+            param.requires_grad = True
 
         #################################################################################
         #################################################################################
