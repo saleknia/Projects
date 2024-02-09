87e1a797e71f4cc45ba45a66ee7ed4dfabbdd47d
saleknia <salekniaamirhossein@gmail.com>
2024-02-09 11:39:09 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -275,23 +275,23 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('tf_efficientnetv2_s', pretrained=True)
+        model = timm.create_model('efficientnet_b2', pretrained=True)
 
         self.model = model 
 
         self.model.classifier = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=1280, out_features=num_classes, bias=True),
+            nn.Linear(in_features=1408, out_features=num_classes, bias=True),
         )
 
-        for param in self.model.blocks[0:6].parameters():
+        for param in self.model.blocks[0:5].parameters():
             param.requires_grad = False
 
         for param in self.model.conv_stem.parameters():
             param.requires_grad = False
 
-        for param in self.model.blocks[5][10:15].parameters():
-            param.requires_grad = True
+        # for param in self.model.blocks[5][10:15].parameters():
+        #     param.requires_grad = True
 
     def forward(self, x0):
         b, c, w, h = x0.shape
