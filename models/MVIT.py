db404ca3dca4d021b31e62a4a6e763f349723821
saleknia <salekniaamirhossein@gmail.com>
2024-02-16 08:44:04 +0000

    Commit Process

--- models/MVIT.py
+++ models/MVIT.py
@@ -312,12 +312,12 @@ class UpBlock(nn.Module):
 
         self.up     = nn.Upsample(scale_factor=2)
         # self.up   = nn.ConvTranspose2d(in_channels       ,in_channels//2,(2,2),2)
-        self.nConvs = _make_nConv(in_channels, out_channels, nb_Conv, activation)
+        self.nConvs = _make_nConv(in_channels * 2, out_channels, nb_Conv, activation)
 
     def forward(self, x, skip_x):
         out = self.up(x)
-        # x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
-        return self.nConvs(out+skip_x)
+        x = torch.cat([out, skip_x], dim=1)  # dim 1 is the channel dimension
+        return self.nConvs(x)
 
 class LayerNormProxy(nn.Module):
     
