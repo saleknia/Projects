dee1a165cbac055d30d1a8883dde08ff97ec8c8f
saleknia <salekniaamirhossein@gmail.com>
2024-02-25 17:14:22 +0000

    Commit Process

--- models/DABNet.py
+++ models/DABNet.py
@@ -1,175 +1,91 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import einops
+import timm
 
-
-
-class Conv(nn.Module):
-    def __init__(self, nIn, nOut, kSize, stride, padding, dilation=(1, 1), groups=1, bn_acti=False, bias=False):
+class LayerNormProxy(nn.Module):
+    
+    def __init__(self, dim):
+        
         super().__init__()
+        self.norm = nn.LayerNorm(dim)
 
-        self.bn_acti = bn_acti
+    def forward(self, x):
 
-        self.conv = nn.Conv2d(nIn, nOut, kernel_size=kSize,
-                              stride=stride, padding=padding,
-                              dilation=dilation, groups=groups, bias=bias)
-
-        if self.bn_acti:
-            self.bn_prelu = BNPReLU(nOut)
-
-    def forward(self, input):
-        output = self.conv(input)
-
-        if self.bn_acti:
-            output = self.bn_prelu(output)
-
-        return output
-
-
-class BNPReLU(nn.Module):
-    def __init__(self, nIn):
-        super().__init__()
-        self.bn = nn.BatchNorm2d(nIn, eps=1e-3)
-        self.acti = nn.PReLU(nIn)
-
-    def forward(self, input):
-        output = self.bn(input)
-        output = self.acti(output)
-
-        return output
-
-
-class DABModule(nn.Module):
-    def __init__(self, nIn, d=1, kSize=3, dkSize=3):
-        super().__init__()
-
-        self.bn_relu_1 = BNPReLU(nIn)
-        self.conv3x3 = Conv(nIn, nIn // 2, kSize, 1, padding=1, bn_acti=True)
-
-        self.dconv3x1 = Conv(nIn // 2, nIn // 2, (dkSize, 1), 1,
-                             padding=(1, 0), groups=nIn // 2, bn_acti=True)
-        self.dconv1x3 = Conv(nIn // 2, nIn // 2, (1, dkSize), 1,
-                             padding=(0, 1), groups=nIn // 2, bn_acti=True)
-        self.ddconv3x1 = Conv(nIn // 2, nIn // 2, (dkSize, 1), 1,
-                              padding=(1 * d, 0), dilation=(d, 1), groups=nIn // 2, bn_acti=True)
-        self.ddconv1x3 = Conv(nIn // 2, nIn // 2, (1, dkSize), 1,
-                              padding=(0, 1 * d), dilation=(1, d), groups=nIn // 2, bn_acti=True)
-
-        self.bn_relu_2 = BNPReLU(nIn // 2)
-        self.conv1x1 = Conv(nIn // 2, nIn, 1, 1, padding=0, bn_acti=False)
-
-    def forward(self, input):
-        output = self.bn_relu_1(input)
-        output = self.conv3x3(output)
-
-        br1 = self.dconv3x1(output)
-        br1 = self.dconv1x3(br1)
-        br2 = self.ddconv3x1(output)
-        br2 = self.ddconv1x3(br2)
-
-        output = br1 + br2
-        output = self.bn_relu_2(output)
-        output = self.conv1x1(output)
-
-        return output + input
-
-
-class DownSamplingBlock(nn.Module):
-    def __init__(self, nIn, nOut):
-        super().__init__()
-        self.nIn = nIn
-        self.nOut = nOut
-
-        if self.nIn < self.nOut:
-            nConv = nOut - nIn
-        else:
-            nConv = nOut
-
-        self.conv3x3 = Conv(nIn, nConv, kSize=3, stride=2, padding=1)
-        self.max_pool = nn.MaxPool2d(2, stride=2)
-        self.bn_prelu = BNPReLU(nOut)
-
-    def forward(self, input):
-        output = self.conv3x3(input)
-
-        if self.nIn < self.nOut:
-            max_pool = self.max_pool(input)
-            output = torch.cat([output, max_pool], 1)
-
-        output = self.bn_prelu(output)
-
-        return output
-
-
-class InputInjection(nn.Module):
-    def __init__(self, ratio):
-        super().__init__()
-        self.pool = nn.ModuleList()
-        for i in range(0, ratio):
-            self.pool.append(nn.AvgPool2d(3, stride=2, padding=1))
-
-    def forward(self, input):
-        for pool in self.pool:
-            input = pool(input)
-
-        return input
-
+        x = einops.rearrange(x, 'b c h w -> b h w c')
+        x = self.norm(x)
+        return einops.rearrange(x, 'b h w c -> b c h w')
 
 class DABNet(nn.Module):
-    def __init__(self, classes=19, block_1=3, block_2=6):
-        super().__init__()
-        self.init_conv = nn.Sequential(
-            Conv(3, 32, 3, 2, padding=1, bn_acti=True),
-            Conv(32, 32, 3, 1, padding=1, bn_acti=True),
-            Conv(32, 32, 3, 1, padding=1, bn_acti=True),
-        )
+    def __init__(self, n_channels=3, n_classes=1):
+        super(DABNet, self).__init__()
 
-        self.down_1 = InputInjection(1)  # down-sample the image 1 times
-        self.down_2 = InputInjection(2)  # down-sample the image 2 times
-        self.down_3 = InputInjection(3)  # down-sample the image 3 times
+        self.convnext = timm.create_model('convnext_tiny', pretrained=True, features_only=True)
 
-        self.bn_prelu_1 = BNPReLU(32 + 3)
+        self.norm_3 = LayerNormProxy(dim=768)
+        self.norm_2 = LayerNormProxy(dim=384)
+        self.norm_1 = LayerNormProxy(dim=192)
+        self.norm_0 = LayerNormProxy(dim=96)
 
-        # DAB Block 1
-        self.downsample_1 = DownSamplingBlock(32 + 3, 64)
-        self.DAB_Block_1 = nn.Sequential()
-        for i in range(0, block_1):
-            self.DAB_Block_1.add_module("DAB_Module_1_" + str(i), DABModule(64, d=2))
-        self.bn_prelu_2 = BNPReLU(128 + 3)
 
-        # DAB Block 2
-        dilation_block_2 = [4, 4, 8, 8, 16, 16]
-        self.downsample_2 = DownSamplingBlock(128 + 3, 128)
-        self.DAB_Block_2 = nn.Sequential()
-        for i in range(0, block_2):
-            self.DAB_Block_2.add_module("DAB_Module_2_" + str(i),
-                                        DABModule(128, d=dilation_block_2[i]))
-        self.bn_prelu_3 = BNPReLU(256 + 3)
+        filters = [96, 192, 384, 768]
+        self.decoder3 = DecoderBottleneckLayer(filters[3], filters[2])
+        self.decoder2 = DecoderBottleneckLayer(filters[2], filters[1])
+        self.decoder1 = DecoderBottleneckLayer(filters[1], filters[0])
 
-        self.classifier = nn.Sequential(Conv(259, classes, 1, 1, padding=0))
-        self.up = nn.Upsample(4)
+        self.final_conv1 = nn.ConvTranspose2d(96, 48, 4, 2, 1)
+        self.final_relu1 = nn.ReLU(inplace=True)
+        self.final_conv2 = nn.Conv2d(48, 48, 3, padding=1)
+        self.final_relu2 = nn.ReLU(inplace=True)
+        self.final_conv3 = nn.Conv2d(48, n_classes, 3, padding=1)
+        self.final_upsample = nn.Upsample(scale_factor=2.0)
 
-    def forward(self, input):
+    def forward(self, x):
+        b, c, h, w = x.shape
 
-        output0 = self.init_conv(input)
+        c0, c1, c2, c3 = self.convnext(x)
 
-        down_1 = self.down_1(input)
-        down_2 = self.down_2(input)
-        down_3 = self.down_3(input)
+        d0 = self.decoder3(c3) + c2
+        d0 = self.decoder2(d0) + c1
+        d0 = self.decoder1(d0) + c0
 
-        output0_cat = self.bn_prelu_1(torch.cat([output0, down_1], 1))
-
-        # DAB Block 1
-        output1_0 = self.downsample_1(output0_cat)
-        output1 = self.DAB_Block_1(output1_0)
-        output1_cat = self.bn_prelu_2(torch.cat([output1, output1_0, down_2], 1))
-
-        # DAB Block 2
-        output2_0 = self.downsample_2(output1_cat)
-        output2 = self.DAB_Block_2(output2_0)
-        output2_cat = self.bn_prelu_3(torch.cat([output2, output2_0, down_3], 1))
-
-        out = self.classifier(output2_cat)
-        out = self.up(out)
+        out = self.final_conv1(d0)
+        out = self.final_relu1(out)
+        out = self.final_conv2(out)
+        out = self.final_relu2(out)
+        out = self.final_conv3(out)
+        out = self.final_upsample(out)
 
         return out
+
+class DecoderBottleneckLayer(nn.Module):
+    def __init__(self, in_channels, n_filters, use_transpose=True):
+        super(DecoderBottleneckLayer, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, in_channels // 4, 1)
+        self.norm1 = nn.BatchNorm2d(in_channels // 4)
+        self.relu1 = nn.ReLU(inplace=True)
+
+        if use_transpose:
+            self.up = nn.Sequential(
+                nn.ConvTranspose2d(
+                    in_channels // 4, in_channels // 4, 3, stride=2, padding=1, output_padding=1
+                ),
+                nn.BatchNorm2d(in_channels // 4),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            self.up = nn.Upsample(scale_factor=2, align_corners=True, mode="bilinear")
+
+        self.conv3 = nn.Conv2d(in_channels // 4, n_filters, 1)
+        self.norm3 = nn.BatchNorm2d(n_filters)
+        self.relu3 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.relu1(x)
+        x = self.up(x)
+        x = self.conv3(x)
+        x = self.norm3(x)
+        x = self.relu3(x)
+        return x
