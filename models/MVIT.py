e3ea40cccfb2e6f00c3b33bc5359fd367615d2ee
saleknia <salekniaamirhossein@gmail.com>
2024-03-10 16:50:32 +0000

    Commit Process

--- models/MVIT.py
+++ models/MVIT.py
@@ -21,7 +21,7 @@ def get_CTranS_config():
     config.transformer = ml_collections.ConfigDict()
     config.KV_size = 288  
     config.transformer.num_heads  = 4
-    config.transformer.num_layers = 2
+    config.transformer.num_layers = 4
     config.expand_ratio           = 4  # MLP channel dimension expand ratio
     config.transformer.embeddings_dropout_rate = 0.1
     config.transformer.attention_dropout_rate  = 0.1
