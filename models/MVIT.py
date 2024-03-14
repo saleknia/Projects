1f9480f51e68a5c139c3f1478b13ced406f9b4f5
saleknia <salekniaamirhossein@gmail.com>
2024-03-14 14:01:05 +0000

    Commit Process

--- models/MVIT.py
+++ models/MVIT.py
@@ -177,7 +177,7 @@ class MVIT(nn.Module):
 
         self.hybrid_decoder = hybrid_decoder()
 
-        # self.mtc = ChannelTransformer(get_CTranS_config(), vis=False, img_size=224, channel_num=[96, 96, 96], patchSize=[4, 2, 1])
+        self.mtc = ChannelTransformer(get_CTranS_config(), vis=False, img_size=224, channel_num=[96, 96, 96], patchSize=[4, 2, 1])
 
     def forward(self, x):
         b, c, h, w = x.shape
@@ -189,7 +189,7 @@ class MVIT(nn.Module):
         x1 = self.HA_1(t1, c1)        
         x2 = self.HA_2(t2, c2)
 
-        # x0, x1, x2 = self.mtc(x0, x1, x2)
+        x0, x1, x2 = self.mtc(x0, x1, x2)
 
         out = self.hybrid_decoder(x0, x1, x2)
 
