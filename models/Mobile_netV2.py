c22f501ab99f3596281ba9b9b5ca60738ec92d85
saleknia <salekniaamirhossein@gmail.com>
2024-05-11 20:51:32 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -294,25 +294,25 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('convnextv2_tiny.fcmae_ft_in1k', pretrained=True, features_only=True)
+        model = timm.create_model('convnextv2_tiny.fcmae_ft_in1k', pretrained=True)
 
         self.model = model 
 
-        self.head = nn.Sequential(
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model.head.fc = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
             nn.Linear(in_features=768, out_features=num_classes, bias=True),
         )
 
         # self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1)) 
 
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
-
-        for param in self.model.stages[3].parameters():
+        for param in self.model.stages[-1].parameters():
             param.requires_grad = True
 
-        # for param in self.head.parameters():
-        #     param.requires_grad = True
+        for param in self.head.parameters():
+            param.requires_grad = True
 
         #################################################################################
         #################################################################################
