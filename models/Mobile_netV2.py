249de14719719d368ae49e1924a3151d44890a1d
saleknia <salekniaamirhossein@gmail.com>
2024-06-02 15:21:27 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -383,7 +383,7 @@ class Mobile_netV2(nn.Module):
             x = self.model(x0)
             x = torch.softmax(x, dim=1)
 
-            if (x.max() <= 0.9):
+            if (x.max() <= 0.0):
 
                 y = self.transform(x1)
                 ncrops, bs, c, h, w = y.size()
