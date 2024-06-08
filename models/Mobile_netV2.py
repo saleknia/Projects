54a256db6a006ae0117bedd8b117479d59e52837
saleknia <salekniaamirhossein@gmail.com>
2024-06-08 17:31:27 +0000

    Commit Process

--- models/Mobile_netV2.py
+++ models/Mobile_netV2.py
@@ -314,7 +314,7 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('timm/convnext_small.fb_in1k', pretrained=True)
+        model = timm.create_model('timm/convnext_tiny.fb_in1k', pretrained=True)
 
         self.model = model 
 
