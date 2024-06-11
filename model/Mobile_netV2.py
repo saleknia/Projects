d1d8f8829f80672beaf5002605434d8386c6adde
saleknia <salekniaamirhossein@gmail.com>
2024-06-11 10:41:46 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -305,15 +305,18 @@ class Mobile_netV2(nn.Module):
         for param in self.model.parameters():
             param.requires_grad = False
 
+        for param in self.model.backbone.stages[-1].parameters():
+            param.requires_grad = True
+
         self.down1 = DownBlock(64 ,  128, nb_Conv=2)
         self.down2 = DownBlock(128,  256, nb_Conv=2)
         self.down3 = DownBlock(256,  512, nb_Conv=2)
         # self.down4 = DownBlock(512, 1024, nb_Conv=2)
 
-        self.up = nn.Upsample(scale_factor=4)
+        # self.up = nn.Upsample(scale_factor=4)
 
         self.dropout = nn.Dropout(0.5)
-        self.avgpool = nn.AvgPool2d(16, stride=1)
+        self.avgpool = nn.AvgPool2d(4, stride=1)
         self.fc_SEM  = nn.Linear(512, 67)
 
         # classifier = timm.create_model('tf_efficientnet_b0', pretrained=True)
@@ -456,11 +459,13 @@ class Mobile_netV2(nn.Module):
         # if (not self.training):
 
         x = self.model(x_in)
-        x = self.up(x)
+        # x = self.up(x)
         x = self.down1(x)
         x = self.down2(x)
         x = self.down3(x)
         # x = self.down4(x)
+
+        # print(x.shape)
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
