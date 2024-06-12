56cc2ba9182e3a4d5144e47761bdc0a371494478
saleknia <salekniaamirhossein@gmail.com>
2024-06-12 09:05:46 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -42,25 +42,25 @@ class Mobile_netV2(nn.Module):
         super(Mobile_netV2, self).__init__()
 
 
-        scene = models.__dict__['resnet50'](num_classes=365)
-        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
+        # scene = models.__dict__['resnet50'](num_classes=365)
+        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
 
-        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 
-        state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
-        state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
+        # # state_dict = {str.replace(k,'.1','1'): v for k,v in state_dict.items()}
+        # # state_dict = {str.replace(k,'.2','2'): v for k,v in state_dict.items()}
 
-        scene.load_state_dict(state_dict)
+        # scene.load_state_dict(state_dict)
 
-        self.scene = scene
+        # self.scene = scene
 
-        for param in self.scene.parameters():
-            param.requires_grad = False
+        # for param in self.scene.parameters():
+        #     param.requires_grad = False
 
-        for param in self.scene.layer4[-1].parameters():
-            param.requires_grad = True
+        # for param in self.scene.layer4[-1].parameters():
+        #     param.requires_grad = True
 
-        self.scene.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
+        # self.scene.fc = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=2048, out_features=num_classes, bias=True))
 
         ############################################################
         ############################################################
@@ -234,19 +234,19 @@ class Mobile_netV2(nn.Module):
         # #################################################################################
         # #################################################################################
 
-        # model = timm.create_model('mvitv2_tiny', pretrained=True)
+        model = timm.create_model('mvitv2_small', pretrained=True)
 
-        # self.model = model
+        self.model = model
 
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-        # for param in self.model.stages[3].parameters():
-        #     param.requires_grad = True
+        for param in self.model.stages[3][-1].parameters():
+            param.requires_grad = True
 
-        # self.model.head = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=768, out_features=num_classes, bias=True))
+        self.model.head = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=768, out_features=num_classes, bias=True))
 
         #################################################################################
         #################################################################################
@@ -398,7 +398,7 @@ class Mobile_netV2(nn.Module):
         # x = x.view(x.size(0), -1)
         # x = self.dropout(x)
         # x = self.fc_SEM(x)
-        x = self.scene(x_in)
+        x = self.model(x_in)
             # x = torch.softmax(self.model(x_in), dim=1) 
 
             # x = (self.tiny(x_in) + self.small(x_in) + self.base(x_in)) / 3.0
