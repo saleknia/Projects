386347b4e6b4ea3eae8f849b4bdff9f96246ce4c
saleknia <salekniaamirhossein@gmail.com>
2024-06-16 17:30:54 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -138,7 +138,7 @@ class Mobile_netV2(nn.Module):
         #     nn.Linear(in_features=768, out_features=256, bias=True),
         # )
 
-        seg = create_seg_model(name="b2", dataset="ade20k", weight_url="/content/drive/MyDrive/b2.pt")
+        seg = create_seg_model(name="b3", dataset="ade20k", weight_url="/content/drive/MyDrive/b3.pt")
 
         seg.backbone.input_stem.op_list[0].conv.stride  = (1, 1)
         seg.backbone.input_stem.op_list[0].conv.padding = (0, 0)
@@ -150,15 +150,15 @@ class Mobile_netV2(nn.Module):
         for param in self.seg.parameters():
             param.requires_grad = False
 
-        # for param in self.seg.head.parameters():
-        #     param.requires_grad = True
+        for param in self.seg.head.parameters():
+            param.requires_grad = True
 
-        # for param in self.seg.backbone.stages[-1].parameters():
-        #     param.requires_grad = True
+        for param in self.seg.backbone.stages[-1].op_list[-4:].parameters():
+            param.requires_grad = True
 
         self.avgpool = nn.AvgPool2d(14, stride=14)
         self.dropout = nn.Dropout(0.5)
-        self.fc_SEM  = nn.Linear(1536, num_classes)
+        self.fc_SEM  = nn.Linear(2048, num_classes)
 
         #################################################################################
         #################################################################################
