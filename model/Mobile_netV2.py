3fe8b34d88a60a54c06d9b0a1104923a3f63eedd
saleknia <64362247+saleknia@users.noreply.github.com>
2024-06-17 21:07:14 +0330

    Update Mobile_netV2.py

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -155,7 +155,7 @@ class Mobile_netV2(nn.Module):
         # for param in self.seg.head.parameters():
         #     param.requires_grad = True
 
-        for param in self.seg.stages[-1].op_list[-4:].parameters():
+        for param in self.seg.stages[-1].op_list[-2:].parameters():
             param.requires_grad = True
 
         self.avgpool = nn.AvgPool2d(14, stride=14)
