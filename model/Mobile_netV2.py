4c05e9761f59d94aa3380171241e2b4280da7d8f
saleknia <salekniaamirhossein@gmail.com>
2024-06-19 18:00:45 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -261,6 +261,27 @@ class Mobile_netV2(nn.Module):
         # for param in self.model.head.parameters():
         #     param.requires_grad = True
 
+        #################################################################################
+        #################################################################################
+
+        model = timm.create_model('timm/maxvit_tiny_tf_224.in1k', pretrained=True)
+
+        self.model = model 
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        self.model.head.fc = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=512, out_features=num_classes, bias=True),
+        )
+
+        for param in self.model.stages[-1].parameters():
+            param.requires_grad = True
+
+        for param in self.model.head.parameters():
+            param.requires_grad = True
+
         # #################################################################################
         # #################################################################################
 
