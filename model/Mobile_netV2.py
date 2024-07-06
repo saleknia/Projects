311a706b338f9c0dceca016aaa0fcad1e076ab4f
saleknia <salekniaamirhossein@gmail.com>
2024-07-06 08:21:56 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -179,7 +179,10 @@ class Mobile_netV2(nn.Module):
         for param in self.seg.head.parameters():
             param.requires_grad = True
 
-        for param in self.seg.backbone.stages[-1].op_list[-4:].parameters():
+        for param in self.seg.backbone.stages[-1].parameters():
+            param.requires_grad = True
+
+        for param in self.seg.backbone.stages[-2].parameters():
             param.requires_grad = True
 
         self.avgpool = nn.AvgPool2d(16, stride=16)
