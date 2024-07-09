cd93333b05d4557c6db3b35e0fc214f65d43e619
saleknia <salekniaamirhossein@gmail.com>
2024-07-09 10:45:34 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -347,7 +347,13 @@ class Mobile_netV2(nn.Module):
         for param in self.model.parameters():
             param.requires_grad = False
 
-        for param in self.model.stages[2:].parameters():
+        for param in self.model.stages_3.parameters():
+            param.requires_grad = True
+
+        for param in self.model.stages_2.parameters():
+            param.requires_grad = True
+
+        for param in self.head.parameters():
             param.requires_grad = True
 
         self.head = nn.Sequential(
