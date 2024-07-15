0be97fe6d2c564740aee3ffda4ffe31cf23a2693
saleknia <salekniaamirhossein@gmail.com>
2024-07-15 12:03:29 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -338,7 +338,7 @@ class Mobile_netV2(nn.Module):
         ##################################################################################
         ##################################################################################
 
-        model = timm.create_model('timm/efficientvit_b2.r224_in1k', pretrained=True)
+        model = timm.create_model('timm/efficientvit_b3.r224_in1k', pretrained=True)
 
         self.model = model 
 
@@ -347,7 +347,7 @@ class Mobile_netV2(nn.Module):
 
         self.model.head.classifier[4] = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=2560, out_features=num_classes, bias=True),
+            nn.Linear(in_features=3200, out_features=num_classes, bias=True),
         )
 
         for param in self.model.stages[-1].parameters():
