2bcf3725bf4af1ba80a85f7448de499b12f781e2
saleknia <salekniaamirhossein@gmail.com>
2024-07-15 15:17:43 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -156,24 +156,24 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        # model      = models.__dict__['resnet50'](num_classes=365)
-        # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
-        # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        model      = models.__dict__['resnet50'](num_classes=365)
+        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 
-        # model.load_state_dict(state_dict)
+        model.load_state_dict(state_dict)
 
-        # self.model = model
+        self.model = model
 
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-        # for param in self.model.layer4[-1].parameters():
-        #     param.requires_grad = True
+        for param in self.model.layer4[-1].parameters():
+            param.requires_grad = True
 
-        # self.model.fc = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=2048, out_features=67, bias=True),
-        # )
+        self.model.fc = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=2048, out_features=67, bias=True),
+        )
 
         # #################################################################################
         # #################################################################################
@@ -198,35 +198,35 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = create_seg_model(name="b2", dataset="ade20k", weight_url="/content/drive/MyDrive/b2.pt").backbone
-        # head  = create_cls_model(name="b2", weight_url="/content/drive/MyDrive/b2-r224.pt").head
+        # model = create_seg_model(name="b2", dataset="ade20k", weight_url="/content/drive/MyDrive/b2.pt").backbone
+        # # head  = create_cls_model(name="b2", weight_url="/content/drive/MyDrive/b2-r224.pt").head
 
-        model.input_stem.op_list[0].conv.stride  = (1, 1)
-        model.input_stem.op_list[0].conv.padding = (0, 0)
+        # model.input_stem.op_list[0].conv.stride  = (1, 1)
+        # model.input_stem.op_list[0].conv.padding = (0, 0)
 
-        self.model = model
-        # self.head  = head
+        # self.model = model
+        # # self.head  = head
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+        # for param in self.model.parameters():
+        #     param.requires_grad = False
 
-        # self.head.op_list[3] = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=2560, out_features=num_classes, bias=True),
-        # )
+        # # self.head.op_list[3] = nn.Sequential(
+        # #     nn.Dropout(p=0.5, inplace=True),
+        # #     nn.Linear(in_features=2560, out_features=num_classes, bias=True),
+        # # )
 
-        for param in self.model.stages[-1].parameters():
-            param.requires_grad = True
-
-        for param in self.model.stages[-2].parameters():
-            param.requires_grad = True
-
-        # for param in self.head.parameters():
+        # for param in self.model.stages[-1].parameters():
         #     param.requires_grad = True
 
-        self.dropout = nn.Dropout(0.5)
-        self.avgpool = nn.AvgPool2d(16, stride=1)
-        self.fc_SEM  = nn.Linear(384, num_classes)
+        # for param in self.model.stages[-2].parameters():
+        #     param.requires_grad = True
+
+        # # for param in self.head.parameters():
+        # #     param.requires_grad = True
+
+        # self.dropout = nn.Dropout(0.5)
+        # self.avgpool = nn.AvgPool2d(16, stride=1)
+        # self.fc_SEM  = nn.Linear(384, num_classes)
 
         #################################################################################
         #################################################################################
@@ -432,12 +432,12 @@ class Mobile_netV2(nn.Module):
 
         # b, c, w, h = x_in.shape
 
-        x = self.model(x_in)
-        x = x['stage_final']
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.dropout(x)
-        x = self.fc_SEM(x)
+        # x = self.model(x_in)
+        # x = x['stage_final']
+        # x = self.avgpool(x)
+        # x = x.view(x.size(0), -1)
+        # x = self.dropout(x)
+        # x = self.fc_SEM(x)
 
         # x0, x1, x2, x3, x4 = self.model(x_in)
 
@@ -451,7 +451,7 @@ class Mobile_netV2(nn.Module):
         # x = self.dropout(x)
         # x = self.fc_SEM(x)
 
-        # x = self.model(x_in)
+        x = self.model(x_in)
 
         # if (not self.training):
 
