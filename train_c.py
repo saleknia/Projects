223370d470ca826556169da7265b1fc9c6ae6e99
saleknia <salekniaamirhossein@gmail.com>
2024-07-15 13:15:14 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -405,7 +405,7 @@ def main(args):
 
         # class transform_test(object):
         #     def __init__(self):
-        #         self.transform_0 = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+        #         self.transform_0 = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
         #         self.transform_1 = transforms.Compose([transforms.Resize((384, 384)), transforms.ToTensor(), transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
         #     def __call__(self, sample):
                 
