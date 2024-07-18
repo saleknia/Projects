2c6a53b01bb53f034affc8c563443b4b88778710
saleknia <salekniaamirhossein@gmail.com>
2024-07-18 18:42:09 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -318,26 +318,26 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('timm/maxvit_tiny_tf_224.in1k', pretrained=True)
+        # model = timm.create_model('timm/maxvit_tiny_tf_224.in1k', pretrained=True)
 
-        self.model = model 
+        # self.model = model 
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+        # for param in self.model.parameters():
+        #     param.requires_grad = False
 
-        self.model.head.fc = nn.Sequential(
-            nn.Dropout(p=0.5, inplace=False),
-            nn.Linear(in_features=512, out_features=num_classes, bias=True),
-        )
+        # self.model.head.fc = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=False),
+        #     nn.Linear(in_features=512, out_features=num_classes, bias=True),
+        # )
 
-        for param in self.model.stages[-1].parameters():
-            param.requires_grad = True
-
-        # for param in self.model.stages[-2].parameters():
+        # for param in self.model.stages[-1].parameters():
         #     param.requires_grad = True
 
-        for param in self.model.head.parameters():
-            param.requires_grad = True
+        # # for param in self.model.stages[-2].parameters():
+        # #     param.requires_grad = True
+
+        # for param in self.model.head.parameters():
+        #     param.requires_grad = True
 
         ##################################################################################
         ##################################################################################
@@ -450,7 +450,7 @@ class Mobile_netV2(nn.Module):
         #         pretrained_teacher.pop(key)
         # self.load_state_dict(pretrained_teacher)
 
-        # self.model = teacher_ensemble()
+        self.model = teacher_ensemble()
 
         # self.count = 0.0
         # self.batch = 0.0
@@ -530,8 +530,6 @@ class teacher_ensemble(nn.Module):
         self.scene  = scene()
         self.seg    = seg()
 
-
-
     def forward(self, x0):
 
         # b, c, w, h = x0.shape
@@ -588,16 +586,17 @@ class scene(nn.Module):
             x = self.model(x0)
             x = torch.softmax(x, dim=1)
 
-            if (x.max() < 0.5):
+            if (x.max() < 0.0):
 
                 y = self.transform(x1)
                 ncrops, bs, c, h, w = y.size()
                 x = self.model(y.view(-1, c, h, w))
-                # x = torch.softmax(x, dim=1).mean(0, keepdim=True)
+                
+                x = torch.softmax(x, dim=1).mean(0, keepdim=True)
 
-                x = torch.softmax(x, dim=1)
-                a, b, c = torch.topk(x.max(dim=1).values, 3).indices
-                x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
+                # x = torch.softmax(x, dim=1)
+                # a, b, c = torch.topk(x.max(dim=1).values, 3).indices
+                # x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
         
         else:
             b, c, w, h = x_in.shape
@@ -659,12 +658,10 @@ class seg(nn.Module):
             x = self.fc_SEM(x)
             x = torch.softmax(x, dim=1)
 
-            if (x.max() < 0.5):
+            if (x.max() < 0.0):
 
                 y = self.transform(x1)
                 ncrops, bs, c, h, w = y.size()
-                # x = self.model(y.view(-1, c, h, w))
-                # x = torch.softmax(x, dim=1).mean(0, keepdim=True)
 
                 x = self.model(y.view(-1, c, h, w))
                 x = x['stage_final']
@@ -672,11 +669,12 @@ class seg(nn.Module):
                 x = x.view(x.size(0), -1)
                 x = self.dropout(x)
                 x = self.fc_SEM(x)
-                # x = torch.softmax(x, dim=1).mean(0, keepdim=True)
 
-                x = torch.softmax(x, dim=1)
-                a, b, c = torch.topk(x.max(dim=1).values, 3).indices
-                x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
+                x = torch.softmax(x, dim=1).mean(0, keepdim=True)
+
+                # x = torch.softmax(x, dim=1)
+                # a, b, c = torch.topk(x.max(dim=1).values, 3).indices
+                # x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
         
         else:
             b, c, w, h = x_in.shape
@@ -726,16 +724,17 @@ class maxvit_model(nn.Module):
             x = self.model(x0)
             x = torch.softmax(x, dim=1)
 
-            if (x.max() < 0.5):
+            if (x.max() < 0.0):
 
                 y = self.transform(x1)
                 ncrops, bs, c, h, w = y.size()
                 x = self.model(y.view(-1, c, h, w))
-                # x = torch.softmax(x, dim=1).mean(0, keepdim=True)
+                
+                x = torch.softmax(x, dim=1).mean(0, keepdim=True)
 
-                x = torch.softmax(x, dim=1)
-                a, b, c = torch.topk(x.max(dim=1).values, 3).indices
-                x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
+                # x = torch.softmax(x, dim=1)
+                # a, b, c = torch.topk(x.max(dim=1).values, 3).indices
+                # x = ((x[a] + x[b] + x[c]) / 3.0).unsqueeze(dim=0)
         
         else:
             b, c, w, h = x_in.shape
