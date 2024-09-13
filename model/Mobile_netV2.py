baee8e6adc7ce07b8a22a07fb987e53870b9a806
saleknia <salekniaamirhossein@gmail.com>
2024-09-13 09:42:57 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -318,26 +318,26 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('timm/maxvit_tiny_tf_224.in1k', pretrained=True)
+        # model = timm.create_model('timm/maxvit_tiny_tf_224.in1k', pretrained=True)
 
-        self.model = model 
+        # self.model = model 
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+        # for param in self.model.parameters():
+        #     param.requires_grad = False
 
-        self.model.head.fc = nn.Sequential(
-            nn.Dropout(p=0.5, inplace=False),
-            nn.Linear(in_features=512, out_features=num_classes, bias=True),
-        )
+        # self.model.head.fc = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=False),
+        #     nn.Linear(in_features=512, out_features=num_classes, bias=True),
+        # )
 
-        for param in self.model.stages[-1].parameters():
-            param.requires_grad = True
+        # for param in self.model.stages[-1].parameters():
+        #     param.requires_grad = True
 
         # for param in self.model.stages[-2].parameters():
         #     param.requires_grad = True
 
-        for param in self.model.head.parameters():
-            param.requires_grad = True
+        # for param in self.model.head.parameters():
+        #     param.requires_grad = True
 
         ##################################################################################
         ##################################################################################
@@ -455,6 +455,21 @@ class Mobile_netV2(nn.Module):
         # self.count = 0.0
         # self.batch = 0.0
         # self.transform = torchvision.transforms.Compose([FiveCrop(224), Lambda(lambda crops: torch.stack([crop for crop in crops]))])
+        #################################
+        #################################
+
+        self.features = timm.create_model('convnext_tiny.fb_in22k', pretrained=True, features_only=True)
+        self.head     = timm.create_model('convnext_tiny.fb_in22k', pretrained=True).head
+        self.head.fc  = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=768, out_features=num_classes, bias=True))
+
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+        for param in self.features.stages_3.parameters():
+            param.requires_grad = True
+       
+        for param in self.features.stages_2.parameters():
+            param.requires_grad = True
 
     def forward(self, x_in):
 
@@ -479,7 +494,8 @@ class Mobile_netV2(nn.Module):
         # x = self.dropout(x)
         # x = self.fc_SEM(x)
 
-        x = self.model(x_in)
+        x0, x1, x2, x3 = self.features(x_in)
+        x              = self.head(x3)
 
         # if (not self.training):
 
