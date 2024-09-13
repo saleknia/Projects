dea1dc934bbeb846a6de25a8eca98bebe2bc44c3
saleknia <salekniaamirhossein@gmail.com>
2024-09-13 22:27:56 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -39,7 +39,7 @@ from torchvision.transforms import FiveCrop, Lambda
 from efficientvit.seg_model_zoo import create_seg_model
 
 from efficientvit.cls_model_zoo import create_cls_model
-
+from timm.layers import LayerNorm2d
 
 class Mobile_netV2(nn.Module):
     def __init__(self, num_classes=67, pretrained=True):
@@ -453,21 +453,23 @@ class Mobile_netV2(nn.Module):
         #################################
         #################################
 
-        self.features = timm.create_model('timm/convnext_tiny.fb_in1k', pretrained=True, features_only=True)
+        self.features = timm.create_model('timm/convnext_tiny.fb_in1k', pretrained=True, features_only=True, out_indices=[2])
         self.head     = timm.create_model('timm/convnext_tiny.fb_in1k', pretrained=True).head
-        self.head.fc  = nn.Sequential(nn.Dropout(p=0.5, inplace=True) , nn.Linear(in_features=768, out_features=num_classes, bias=True))
+        self.head.norm = LayerNorm2d(3840)
+        self.head.fc  = nn.Sequential(nn.Dropout(p=0.5, inplace=True) , nn.Linear(in_features=3840, out_features=num_classes, bias=True))
 
-        # self.expert_w = expert_w()
-        # self.expert_s = expert_s()
-        # self.expert_p = expert_p()
-        # self.expert_l = expert_l()
-        # self.expert_h = expert_h()
+        self.expert_g = expert_g()
+        self.expert_w = expert_w()
+        self.expert_s = expert_s()
+        self.expert_p = expert_p()
+        self.expert_l = expert_l()
+        self.expert_h = expert_h()
 
         for param in self.features.parameters():
             param.requires_grad = False
 
-        for param in self.features.stages_3.parameters():
-            param.requires_grad = True
+        # for param in self.features.stages_3.parameters():
+        #     param.requires_grad = True
 
         # for param in self.features.stages_2.parameters():
         #     param.requires_grad = True
@@ -497,15 +499,18 @@ class Mobile_netV2(nn.Module):
         # x = self.dropout(x)
         # x = self.fc_SEM(x)
 
-        x0, x1, x2, x3 = self.features(x_in)
-        x  = self.head(x3)
+        # x0, x1, x2, x3 = self.features(x_in)
+        # x  = self.head(x3)
 
-        # x_in = self.features(x_in)[0]
-        # w, wi = self.expert_w(x_in)
-        # s, si = self.expert_s(x_in)
-        # p, pi = self.expert_p(x_in)
-        # l, li = self.expert_l(x_in)
-        # h, hi = self.expert_h(x_in)
+        x_in = self.features(x_in)[0]
+        # g, gi = self.expert_g(x_in)
+        w, wi = self.expert_w(x_in)
+        s, si = self.expert_s(x_in)
+        p, pi = self.expert_p(x_in)
+        l, li = self.expert_l(x_in)
+        h, hi = self.expert_h(x_in)
+
+        # index = gi
 
         # index = torch.cat([wi, si, pi, li, hi], dim=1)
         # # index = torch.softmax(index / 0.2, dim=1)
@@ -540,6 +545,8 @@ class Mobile_netV2(nn.Module):
         # print(h.shape)
 
         # x3 = w + s + p + l + h
+        x3 = torch.cat([w, s, p, l, h], dim=1)
+        x  = self.head(x3)
       
 
         # features_t = self.teacher(x_in)
@@ -584,6 +591,37 @@ class Mobile_netV2(nn.Module):
         #     return x, features_s, features_t
         # else:
         #     return x
+
+class expert_g(nn.Module):
+    def __init__(self, num_classes=5, pretrained=True):
+        super(expert_g, self).__init__()
+
+        self.features = timm.create_model('convnext_tiny.fb_in1k', pretrained=True, features_only=True)
+        self.head     = timm.create_model('convnext_tiny.fb_in1k', pretrained=True).head
+        self.head.fc  = nn.Sequential(nn.Dropout(p=0.5, inplace=True), nn.Linear(in_features=768, out_features=num_classes, bias=True))
+
+        for param in self.parameters():
+            param.requires_grad = False
+
+        loaded_data_teacher = torch.load('/content/drive/MyDrive/checkpoint/expert_g.pth', map_location='cpu')
+        pretrained_teacher  = loaded_data_teacher['net']
+        a = pretrained_teacher.copy()
+        for key in a.keys():
+            if 'teacher' in key:
+                pretrained_teacher.pop(key)
+        self.load_state_dict(pretrained_teacher)
+
+    def forward(self, x_in):
+
+        x3 = self.features.stages_3(x_in)
+
+        # x0, x1, x2, x3 = self.features(x_in)
+
+        x              = self.head(x3)
+
+        x  = x.softmax(dim=1)
+
+        return x3, x
 
 class expert_w(nn.Module):
     def __init__(self, num_classes=15, pretrained=True):
