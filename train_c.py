74de03db76baef685e8c96b51a503b4aa6107afe
saleknia <salekniaamirhossein@gmail.com>
2024-10-09 20:25:30 +0000

    Commit Process

--- train_c.py
+++ train_c.py
@@ -257,7 +257,7 @@ def main(args):
         # testset = torchvision.datasets.ImageFolder(root='/content/MIT-67-seg/test/', transform=transform_test)
         # test_loader = torch.utils.data.DataLoader(testset  , batch_size = 1         , shuffle=True, num_workers=NUM_WORKERS)
 
-        trainset     = torchvision.datasets.ImageFolder(root='/content/MIT-67-home/train/', transform=transform_train)
+        trainset     = torchvision.datasets.ImageFolder(root='/content/MIT-67-leisure/train/', transform=transform_train)
 
         # subdirectories = trainset.classes
         # class_weights = []
@@ -278,7 +278,7 @@ def main(args):
 
         train_loader = torch.utils.data.DataLoader(trainset, batch_size = BATCH_SIZE , shuffle=True, num_workers=NUM_WORKERS)
 
-        testset      = torchvision.datasets.ImageFolder(root='/content/MIT-67-home/test/' , transform=transform_test)
+        testset      = torchvision.datasets.ImageFolder(root='/content/MIT-67-leisure/test/' , transform=transform_test)
         test_loader  = torch.utils.data.DataLoader(testset , batch_size = 1          , shuffle=False, num_workers=NUM_WORKERS)
 
         NUM_CLASS = len(trainset.classes)
