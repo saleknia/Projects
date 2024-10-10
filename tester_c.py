3edd534b0bdd1223752dda6d4cc4d916e90280dd
saleknia <salekniaamirhossein@gmail.com>
2024-10-10 06:56:01 +0000

    Commit Process

--- tester_c.py
+++ tester_c.py
@@ -39,7 +39,7 @@ def tester(end_epoch,epoch_num,model,dataloader,device,ckpt,num_class,writer,log
     loss_total = utils.AverageMeter()
 
     # accuracy   = utils.AverageMeter()
-    metric = MulticlassAccuracy(average="micro", num_classes=num_class).to('cuda')
+    metric = MulticlassAccuracy(average="macro", num_classes=num_class).to('cuda')
     # accuracy = mAPMeter()
 
     ce_loss = CrossEntropyLoss()
