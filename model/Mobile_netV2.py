984f66308fbe7ec13a564766e0b894a807c02ab1
saleknia <salekniaamirhossein@gmail.com>
2024-10-12 13:46:00 +0000

    Commit Process

--- model/Mobile_netV2.py
+++ model/Mobile_netV2.py
@@ -170,28 +170,28 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        # # model      = models.__dict__['resnet50'](num_classes=365)
-        # # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
-        # # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
-
         # model      = models.__dict__['resnet50'](num_classes=365)
         # checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
         # state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 
-        # model.load_state_dict(state_dict)
+        model      = models.__dict__['resnet50'](num_classes=365)
+        checkpoint = torch.load('/content/resnet50_places365.pth.tar', map_location='cpu')
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
 
-        # self.model = model
+        model.load_state_dict(state_dict)
 
-        # for param in self.model.parameters():
-        #     param.requires_grad = False
+        self.model = model
 
-        # for param in self.model.layer4.parameters():
-        #     param.requires_grad = True
+        for param in self.model.parameters():
+            param.requires_grad = False
 
-        # self.model.fc = nn.Sequential(
-        #     nn.Dropout(p=0.5, inplace=True),
-        #     nn.Linear(in_features=2048, out_features=num_classes, bias=True),
-        # )
+        for param in self.model.layer4.parameters():
+            param.requires_grad = True
+
+        self.model.fc = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=2048, out_features=num_classes, bias=True),
+        )
 
         # #################################################################################
         # #################################################################################
@@ -312,23 +312,23 @@ class Mobile_netV2(nn.Module):
         #################################################################################
         #################################################################################
 
-        model = timm.create_model('timm/convnext_small.fb_in22k', pretrained=True)
+        # model = timm.create_model('timm/convnext_small.fb_in22k', pretrained=True)
 
-        self.model = model 
+        # self.model = model 
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+        # for param in self.model.parameters():
+        #     param.requires_grad = False
 
-        self.model.head.fc = nn.Sequential(
-            nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=768, out_features=num_classes, bias=True),
-        )
+        # self.model.head.fc = nn.Sequential(
+        #     nn.Dropout(p=0.5, inplace=True),
+        #     nn.Linear(in_features=768, out_features=num_classes, bias=True),
+        # )
 
-        for param in self.model.stages[-1].parameters():
-            param.requires_grad = True
+        # for param in self.model.stages[-1].parameters():
+        #     param.requires_grad = True
 
-        for param in self.model.head.parameters():
-            param.requires_grad = True
+        # for param in self.model.head.parameters():
+        #     param.requires_grad = True
 
         ##################################################################################
         ##################################################################################
