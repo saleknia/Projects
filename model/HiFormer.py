816c89b9c57e224b3b21f4ccccaece319ac1334b
saleknia <salekniaamirhossein@gmail.com>
2024-10-20 18:04:13 +0000

    Commit Process

--- model/HiFormer.py
+++ model/HiFormer.py
@@ -12,54 +12,6 @@ import ml_collections
 import os
 import wget
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-config = get_hiformer_s_configs()
-
-class HiFormer(nn.Module):
-    def __init__(self, config=config, img_size=224, in_chans=3, n_classes=1):
-        super().__init__()
-        self.img_size = img_size
-        self.patch_size = [4, 16]
-        self.n_classes = n_classes
-        self.All2Cross = All2Cross(config = config, img_size= img_size, in_chans=in_chans)
-        
-        self.ConvUp_s = ConvUpsample(in_chans=384, out_chans=[128,128], upsample=True)
-        self.ConvUp_l = ConvUpsample(in_chans=96, upsample=False)
-    
-        self.segmentation_head = SegmentationHead(
-            in_channels=16,
-            out_channels=n_classes,
-            kernel_size=3,
-        )    
-
-        self.conv_pred = nn.Sequential(
-            nn.Conv2d(
-                128, 16,
-                kernel_size=1, stride=1,
-                padding=0, bias=True),
-            # nn.GroupNorm(8, 16), 
-            nn.ReLU(inplace=True),
-            nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False)
-        )
-    
-    def forward(self, x):
-        xs = self.All2Cross(x)
-        embeddings = [x[:, 1:] for x in xs]
-        reshaped_embed = []
-        for i, embed in enumerate(embeddings):
-
-            embed = Rearrange('b (h w) d -> b d h w', h=(self.img_size//self.patch_size[i]), w=(self.img_size//self.patch_size[i]))(embed)
-            embed = self.ConvUp_l(embed) if i == 0 else self.ConvUp_s(embed)
-            
-            reshaped_embed.append(embed)
-
-        C = reshaped_embed[0] + reshaped_embed[1]
-        C = self.conv_pred(C)
-
-        out = self.segmentation_head(C)
-        
-        return out  
-
 ################################################################################
 
 class Attention(nn.Module):
@@ -975,3 +927,53 @@ def get_hiformer_l_configs():
     cfg.cross_pos_embed = True
 
     return cfg
+
+################################################################################
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+config = get_hiformer_s_configs()
+
+class HiFormer(nn.Module):
+    def __init__(self, config=config, img_size=224, in_chans=3, n_classes=1):
+        super().__init__()
+        self.img_size = img_size
+        self.patch_size = [4, 16]
+        self.n_classes = n_classes
+        self.All2Cross = All2Cross(config = config, img_size= img_size, in_chans=in_chans)
+        
+        self.ConvUp_s = ConvUpsample(in_chans=384, out_chans=[128,128], upsample=True)
+        self.ConvUp_l = ConvUpsample(in_chans=96, upsample=False)
+    
+        self.segmentation_head = SegmentationHead(
+            in_channels=16,
+            out_channels=n_classes,
+            kernel_size=3,
+        )    
+
+        self.conv_pred = nn.Sequential(
+            nn.Conv2d(
+                128, 16,
+                kernel_size=1, stride=1,
+                padding=0, bias=True),
+            # nn.GroupNorm(8, 16), 
+            nn.ReLU(inplace=True),
+            nn.Upsample(scale_factor=4, mode='bilinear', align_corners=False)
+        )
+    
+    def forward(self, x):
+        xs = self.All2Cross(x)
+        embeddings = [x[:, 1:] for x in xs]
+        reshaped_embed = []
+        for i, embed in enumerate(embeddings):
+
+            embed = Rearrange('b (h w) d -> b d h w', h=(self.img_size//self.patch_size[i]), w=(self.img_size//self.patch_size[i]))(embed)
+            embed = self.ConvUp_l(embed) if i == 0 else self.ConvUp_s(embed)
+            
+            reshaped_embed.append(embed)
+
+        C = reshaped_embed[0] + reshaped_embed[1]
+        C = self.conv_pred(C)
+
+        out = self.segmentation_head(C)
+        
+        return out  
